c5aa78d9c3b60b417ff9ad3214c6f07a7911c032
alexk95 <alexkuester@mail.ru>
2023-04-25 22:42:42 +0200

    Update UpdateDeploymentLibrariesOnly.bat

--- Scripts/UpdateDeploymentLibrariesOnly.bat
+++ Scripts/UpdateDeploymentLibrariesOnly.bat
@@ -4,11 +4,11 @@ REM The first argument of the shell defines whether a release or debug build sha
 REM The second argument of hte shell defines whetehr a full rebuild or just a build is performed. "BUILD" (default), "REBUILD"
 
 REM This script requires the following environment variables to be set:
-REM 1. SIM_PLAT_ROOT
+REM 1. OPENTWIN_DEV_ROOT
 REM 2. DEVENV_ROOT_2022
 
-IF "%SIM_PLAT_ROOT%"=="" (
-	ECHO Please specify the following environment variables: SIM_PLAT_ROOT
+IF "%OPENTWIN_DEV_ROOT%"=="" (
+	ECHO Please specify the following environment variables: OPENTWIN_DEV_ROOT
 	goto END
 )
 
@@ -19,133 +19,112 @@ IF "%DEVENV_ROOT_2022%"=="" (
 
 REM Setup eviroment
 
-CALL "%SIM_PLAT_ROOT%\MasterBuild\set_env.bat"
+CALL "%OPENTWIN_DEV_ROOT%\Scripts\SetupEnvironment.bat"
 
-IF "%OT_DEPLOYMENT_DIR%" == "" (
-    SET OT_DEPLOYMENT_DIR="%SIM_PLAT_ROOT%\Deployment"
+IF "%OPENTWIN_DEPLOYMENT_DIR%" == "" (
+    SET OPENTWIN_DEPLOYMENT_DIR="%OPENTWIN_DEV_ROOT%\Deployment"
 )
 
-REM Shutdown the session and authorisation services if they are still running 
-
-IF NOT DEFINED OPEN_TWIN_SERVICES_ADDRESS (
-	SET OPEN_TWIN_SERVICES_ADDRESS=127.0.0.1
-)
-
-IF NOT DEFINED OPEN_TWIN_LOG_PORT (
-	SET OPEN_TWIN_GSS_PORT=8090
-)
-
-IF NOT DEFINED OPEN_TWIN_GSS_PORT (
-	SET OPEN_TWIN_GSS_PORT=8091
-)
-
-IF NOT DEFINED OPEN_TWIN_AUTH_PORT (
-	SET OPEN_TWIN_AUTH_PORT=8092
-)
-
-IF NOT DEFINED OPEN_TWIN_LSS_PORT (
-	SET OPEN_TWIN_LSS_PORT=8093
-)
-
-REM Shutdown microservices
-taskkill /IM "open_twin.exe" /F
-
-REM Shutdown ui
-taskkill /IM "uiFrontend.exe" /F
-
-REM Shutdown Apache server
-taskkill /IM "httpd.exe" /F
+CALL "%OPENTWIN_DEV_ROOT%\Scripts\ShutodwnAll.bat"
 
 ECHO Delete libraries
 
-DEL "%OT_DEPLOYMENT_DIR%\BlockEditor.dll"
-DEL "%OT_DEPLOYMENT_DIR%\BlockEditorAPI.dll"
-DEL "%OT_DEPLOYMENT_DIR%\OpenTwinSystem.dll"
-DEL "%OT_DEPLOYMENT_DIR%\OpenTwinCore.dll"
-DEL "%OT_DEPLOYMENT_DIR%\OpenTwinCommunication.dll"
-DEL "%OT_DEPLOYMENT_DIR%\OpenTwinServiceFoundation.dll"
-DEL "%OT_DEPLOYMENT_DIR%\ModelEntities.dll"
-DEL "%OT_DEPLOYMENT_DIR%\CADModelEntities.dll"
-DEL "%OT_DEPLOYMENT_DIR%\Model.dll"
-DEL "%OT_DEPLOYMENT_DIR%\Viewer.dll"
-DEL "%OT_DEPLOYMENT_DIR%\DataStorage.dll"
-DEL "%OT_DEPLOYMENT_DIR%\UIPluginAPI.dll"
-DEL "%OT_DEPLOYMENT_DIR%\uiCore.dll"
-DEL "%OT_DEPLOYMENT_DIR%\RubberbandEngineCore.dll"	
-DEL "%OT_DEPLOYMENT_DIR%\RubberbandOsgWrapper.dll"
-DEL "%OT_DEPLOYMENT_DIR%\QwtWrapper.dll"
-DEL "%OT_DEPLOYMENT_DIR%\GlobalSessionService.dll"
-DEL "%OT_DEPLOYMENT_DIR%\SessionService.dll"
-DEL "%OT_DEPLOYMENT_DIR%\GlobalDirectoryService.dll"
-DEL "%OT_DEPLOYMENT_DIR%\LocalDirectoryService.dll"
-DEL "%OT_DEPLOYMENT_DIR%\uiFrontend.exe"
-DEL "%OT_DEPLOYMENT_DIR%\RelayService.dll"
-DEL "%OT_DEPLOYMENT_DIR%\LoggerService.dll"
-DEL "%OT_DEPLOYMENT_DIR%\AuthorisationService.dll"
-DEL "%OT_DEPLOYMENT_DIR%\PHREECService.dll"
-DEL "%OT_DEPLOYMENT_DIR%\KrigingService.dll"
-DEL "%OT_DEPLOYMENT_DIR%\ModelingService.dll"
-DEL "%OT_DEPLOYMENT_DIR%\VisualizationService.dll"
-DEL "%OT_DEPLOYMENT_DIR%\FITTDService.dll"
-DEL "%OT_DEPLOYMENT_DIR%\ImportParameterizedDataService.dll"
-DEL "%OT_DEPLOYMENT_DIR%\CartesianMeshService.dll"
-DEL "%OT_DEPLOYMENT_DIR%\TetMeshService.dll"
-DEL "%OT_DEPLOYMENT_DIR%\GetDPService.dll"
-DEL "%OT_DEPLOYMENT_DIR%\OTGui.dll"
-DEL "%OT_DEPLOYMENT_DIR%\open_twin.exe"
+DEL "%OPENTWIN_DEPLOYMENT_DIR%\BlockEditor.dll"
+DEL "%OPENTWIN_DEPLOYMENT_DIR%\BlockEditorAPI.dll"
+DEL "%OPENTWIN_DEPLOYMENT_DIR%\OpenTwinSystem.dll"
+DEL "%OPENTWIN_DEPLOYMENT_DIR%\OpenTwinCore.dll"
+DEL "%OPENTWIN_DEPLOYMENT_DIR%\OpenTwinCommunication.dll"
+DEL "%OPENTWIN_DEPLOYMENT_DIR%\OpenTwinServiceFoundation.dll"
+DEL "%OPENTWIN_DEPLOYMENT_DIR%\ModelEntities.dll"
+DEL "%OPENTWIN_DEPLOYMENT_DIR%\CADModelEntities.dll"
+DEL "%OPENTWIN_DEPLOYMENT_DIR%\Model.dll"
+DEL "%OPENTWIN_DEPLOYMENT_DIR%\Viewer.dll"
+DEL "%OPENTWIN_DEPLOYMENT_DIR%\DataStorage.dll"
+DEL "%OPENTWIN_DEPLOYMENT_DIR%\UIPluginAPI.dll"
+DEL "%OPENTWIN_DEPLOYMENT_DIR%\uiCore.dll"
+DEL "%OPENTWIN_DEPLOYMENT_DIR%\RubberbandEngineCore.dll"	
+DEL "%OPENTWIN_DEPLOYMENT_DIR%\RubberbandOsgWrapper.dll"
+DEL "%OPENTWIN_DEPLOYMENT_DIR%\QwtWrapper.dll"
+DEL "%OPENTWIN_DEPLOYMENT_DIR%\GlobalSessionService.dll"
+DEL "%OPENTWIN_DEPLOYMENT_DIR%\SessionService.dll"
+DEL "%OPENTWIN_DEPLOYMENT_DIR%\GlobalDirectoryService.dll"
+DEL "%OPENTWIN_DEPLOYMENT_DIR%\LocalDirectoryService.dll"
+DEL "%OPENTWIN_DEPLOYMENT_DIR%\uiFrontend.exe"
+DEL "%OPENTWIN_DEPLOYMENT_DIR%\RelayService.dll"
+DEL "%OPENTWIN_DEPLOYMENT_DIR%\LoggerService.dll"
+DEL "%OPENTWIN_DEPLOYMENT_DIR%\AuthorisationService.dll"
+DEL "%OPENTWIN_DEPLOYMENT_DIR%\PHREECService.dll"
+DEL "%OPENTWIN_DEPLOYMENT_DIR%\KrigingService.dll"
+DEL "%OPENTWIN_DEPLOYMENT_DIR%\ModelingService.dll"
+DEL "%OPENTWIN_DEPLOYMENT_DIR%\VisualizationService.dll"
+DEL "%OPENTWIN_DEPLOYMENT_DIR%\FITTDService.dll"
+DEL "%OPENTWIN_DEPLOYMENT_DIR%\ImportParameterizedDataService.dll"
+DEL "%OPENTWIN_DEPLOYMENT_DIR%\CartesianMeshService.dll"
+DEL "%OPENTWIN_DEPLOYMENT_DIR%\TetMeshService.dll"
+DEL "%OPENTWIN_DEPLOYMENT_DIR%\GetDPService.dll"
+DEL "%OPENTWIN_DEPLOYMENT_DIR%\OTGui.dll"
+DEL "%OPENTWIN_DEPLOYMENT_DIR%\open_twin.exe"
 
-DEL "%OT_DEPLOYMENT_DIR%\OToolkit.dll"
+DEL "%OPENTWIN_DEPLOYMENT_DIR%\OToolkit.dll"
 
 ECHO Delete Admin Service
 
-RMDIR /Q /S "%OT_DEPLOYMENT_DIR%\Apache\htdocs"
-MKDIR "%OT_DEPLOYMENT_DIR%\Apache\htdocs"
+RMDIR /Q /S "%OPENTWIN_DEPLOYMENT_DIR%\Apache\htdocs"
+MKDIR "%OPENTWIN_DEPLOYMENT_DIR%\Apache\htdocs"
 
+REM  Copy libraries
 ECHO Copy libraries
 
-COPY "%SIM_PLAT_ROOT%\Libraries\BlockEditor\x64\Release\BlockEditor.dll" "%OT_DEPLOYMENT_DIR%"
-COPY "%SIM_PLAT_ROOT%\Libraries\BlockEditorAPI\x64\Release\BlockEditorAPI.dll" "%OT_DEPLOYMENT_DIR%"
-COPY "%SIM_PLAT_ROOT%\Libraries\OpenTwinCore\x64\Release\OpenTwinCore.dll" "%OT_DEPLOYMENT_DIR%"
-COPY "%SIM_PLAT_ROOT%\Libraries\OpenTwinSystem\x64\Release\OpenTwinSystem.dll" "%OT_DEPLOYMENT_DIR%"
-COPY "%SIM_PLAT_ROOT%\Libraries\OpenTwinCommunication\x64\Release\OpenTwinCommunication.dll" "%OT_DEPLOYMENT_DIR%"
-COPY "%SIM_PLAT_ROOT%\Libraries\OpenTwinServiceFoundation\x64\Release\OpenTwinServiceFoundation.dll" "%OT_DEPLOYMENT_DIR%"
-COPY "%SIM_PLAT_ROOT%\Libraries\ModelEntities\x64\Release\ModelEntities.dll" "%OT_DEPLOYMENT_DIR%"
-COPY "%SIM_PLAT_ROOT%\Libraries\CADModelEntities\x64\Release\CADModelEntities.dll" "%OT_DEPLOYMENT_DIR%"
-COPY "%SIM_PLAT_ROOT%\Libraries\Model\x64\Release\Model.dll" "%OT_DEPLOYMENT_DIR%"
-COPY "%SIM_PLAT_ROOT%\Libraries\Viewer\x64\Release\Viewer.dll" "%OT_DEPLOYMENT_DIR%"
-COPY "%SIM_PLAT_ROOT%\Libraries\DataStorage\x64\Release\DataStorage.dll" "%OT_DEPLOYMENT_DIR%"
-COPY "%OT_UIPLUGINAPI_ROOT%\x64\Release\UIPluginAPI.dll" "%OT_DEPLOYMENT_DIR%"
-COPY "%UICORE_LIB_ROOT%\x64\Release\uiCore.dll" "%OT_DEPLOYMENT_DIR%"
-COPY "%RUBBERBAND_ENGINE_CORE%\x64\Release\RubberbandEngineCore.dll" "%OT_DEPLOYMENT_DIR%"
-COPY "%RUBBERBAND_ENGINE_OSG%\x64\Release\RubberbandOsgWrapper.dll" "%OT_DEPLOYMENT_DIR%"
-COPY "%QWT_WRAPPER%\x64\Release\QwtWrapper.dll" "%OT_DEPLOYMENT_DIR%"
-COPY "%SIM_PLAT_ROOT%\Libraries\GlobalSessionService\x64\Release\GlobalSessionService.dll" "%OT_DEPLOYMENT_DIR%"
-COPY "%SIM_PLAT_ROOT%\Libraries\SessionService\x64\Release\SessionService.dll" "%OT_DEPLOYMENT_DIR%"
-COPY "%SIM_PLAT_ROOT%\Libraries\GlobalDirectoryService\x64\Release\GlobalDirectoryService.dll" "%OT_DEPLOYMENT_DIR%"
-COPY "%SIM_PLAT_ROOT%\Libraries\LocalDirectoryService\x64\Release\LocalDirectoryService.dll" "%OT_DEPLOYMENT_DIR%"
-COPY "%UISERVICE_LIB_ROOT%\x64\Release\uiFrontend.exe" "%OT_DEPLOYMENT_DIR%"
-COPY "%RELAYSERVICE_LIB_ROOT%\x64\Release\RelayService.dll" "%OT_DEPLOYMENT_DIR%"
-COPY "%SIM_PLAT_ROOT%\Libraries\LoggerService\x64\Release\LoggerService.dll" "%OT_DEPLOYMENT_DIR%"
-COPY "%SIM_PLAT_ROOT%\Libraries\AuthorisationService\x64\Release\AuthorisationService.dll" "%OT_DEPLOYMENT_DIR%"
-COPY "%SIM_PLAT_ROOT%\Libraries\PHREECService\x64\Release\PHREECService.dll" "%OT_DEPLOYMENT_DIR%"
-COPY "%SIM_PLAT_ROOT%\Libraries\KrigingService\x64\Release\KrigingService.dll" "%OT_DEPLOYMENT_DIR%"
-COPY "%SIM_PLAT_ROOT%\Libraries\KrigingService\x64\Release\run.py" "%OT_DEPLOYMENT_DIR%\Python"
-COPY "%SIM_PLAT_ROOT%\Libraries\ModelingService\x64\Release\ModelingService.dll" "%OT_DEPLOYMENT_DIR%"
-COPY "%SIM_PLAT_ROOT%\Libraries\VisualizationService\x64\Release\VisualizationService.dll" "%OT_DEPLOYMENT_DIR%"
-COPY "%SIM_PLAT_ROOT%\Libraries\FITTDService\x64\Release\FITTDService.dll" "%OT_DEPLOYMENT_DIR%"
-COPY "%SIM_PLAT_ROOT%\Libraries\ImportParameterizedData\x64\Release\ImportParameterizedDataService.dll" "%OT_DEPLOYMENT_DIR%"
-COPY "%SIM_PLAT_ROOT%\Libraries\CartesianMeshService\x64\Release\CartesianMeshService.dll" "%OT_DEPLOYMENT_DIR%"
-COPY "%SIM_PLAT_ROOT%\Libraries\TetMeshService\x64\Release\TetMeshService.dll" "%OT_DEPLOYMENT_DIR%"
-COPY "%SIM_PLAT_ROOT%\Libraries\GetDPService\x64\Release\GetDPService.dll" "%OT_DEPLOYMENT_DIR%"
-COPY "%SIM_PLAT_ROOT%\Libraries\OTGui\x64\Release\OTGui.dll" "%OT_DEPLOYMENT_DIR%"
-COPY "%SIM_PLAT_ROOT%\Microservices\OpenTwin\target\release\open_twin.exe" "%OT_DEPLOYMENT_DIR%"
+COPY "%OT_BLOCKEDITOR_DLLR%\BlockEditor.dll" "%OPENTWIN_DEPLOYMENT_DIR%"
+COPY "%OT_BLOCKEDITORAPI_DLLR%\BlockEditorAPI.dll" "%OPENTWIN_DEPLOYMENT_DIR%"
+COPY "%OT_CORE_DLLR%\OpenTwinCore.dll" "%OPENTWIN_DEPLOYMENT_DIR%"
+COPY "%OT_SYSTEM_DLLR%\OpenTwinSystem.dll" "%OPENTWIN_DEPLOYMENT_DIR%"
+COPY "%OT_COMMUNICATION_DLLR%\OpenTwinCommunication.dll" "%OPENTWIN_DEPLOYMENT_DIR%"
+COPY "%OT_FOUNDATION_DLLR%OpenTwinServiceFoundation.dll" "%OPENTWIN_DEPLOYMENT_DIR%"
+COPY "%OT_MODELENTITIES_DLLR%\ModelEntities.dll" "%OPENTWIN_DEPLOYMENT_DIR%"
+COPY "%OT_CADMODELENTITIES_DLLR%\CADModelEntities.dll" "%OPENTWIN_DEPLOYMENT_DIR%"
+COPY "%OT_VIEWER_DLLR%\Viewer.dll" "%OPENTWIN_DEPLOYMENT_DIR%"
+COPY "%OT_DATASTORAGE_DLLR%\DataStorage.dll" "%OPENTWIN_DEPLOYMENT_DIR%"
+COPY "%OT_UIPLUGINAPI_DLLR%\UIPluginAPI.dll" "%OPENTWIN_DEPLOYMENT_DIR%"
+COPY "%OT_UICORE_DLLR%\uiCore.dll" "%OPENTWIN_DEPLOYMENT_DIR%"
+COPY "%OT_RUBBERBANDAPI_DLLR%\RubberbandEngineCore.dll" "%OPENTWIN_DEPLOYMENT_DIR%"
+COPY "%OT_RUBBERBAND_DLLR%\RubberbandOsgWrapper.dll" "%OPENTWIN_DEPLOYMENT_DIR%"
+COPY "%OT_QWTWRAPPER_DLLR%\QwtWrapper.dll" "%OPENTWIN_DEPLOYMENT_DIR%"
+COPY "%OT_GUI_DLLR%\OTGui.dll" "%OPENTWIN_DEPLOYMENT_DIR%"
 
-COPY "%SIM_PLAT_ROOT%\Tools\OToolkit\x64\Release\OToolkit.dll" "%OT_DEPLOYMENT_DIR%"
+REM  Copy Services
+ECHO Copy Services
 
+COPY "%OPENTWIN_DEV_ROOT%\Services\Model\x64\Release\Model.dll" "%OPENTWIN_DEPLOYMENT_DIR%"
+COPY "%OPENTWIN_DEV_ROOT%\Services\GlobalSessionService\x64\Release\GlobalSessionService.dll" "%OPENTWIN_DEPLOYMENT_DIR%"
+COPY "%OPENTWIN_DEV_ROOT%\Services\SessionService\x64\Release\SessionService.dll" "%OPENTWIN_DEPLOYMENT_DIR%"
+COPY "%OPENTWIN_DEV_ROOT%\Services\GlobalDirectoryService\x64\Release\GlobalDirectoryService.dll" "%OPENTWIN_DEPLOYMENT_DIR%"
+COPY "%OPENTWIN_DEV_ROOT%\Services\LocalDirectoryService\x64\Release\LocalDirectoryService.dll" "%OPENTWIN_DEPLOYMENT_DIR%"
+COPY "%UISERVICE_LIB_ROOT%\x64\Release\uiFrontend.exe" "%OPENTWIN_DEPLOYMENT_DIR%"
+COPY "%RELAYSERVICE_LIB_ROOT%\x64\Release\RelayService.dll" "%OPENTWIN_DEPLOYMENT_DIR%"
+COPY "%OPENTWIN_DEV_ROOT%\Services\LoggerService\x64\Release\LoggerService.dll" "%OPENTWIN_DEPLOYMENT_DIR%"
+COPY "%OPENTWIN_DEV_ROOT%\Services\AuthorisationService\x64\Release\AuthorisationService.dll" "%OPENTWIN_DEPLOYMENT_DIR%"
+COPY "%OPENTWIN_DEV_ROOT%\Services\PHREECService\x64\Release\PHREECService.dll" "%OPENTWIN_DEPLOYMENT_DIR%"
+COPY "%OPENTWIN_DEV_ROOT%\Services\KrigingService\x64\Release\KrigingService.dll" "%OPENTWIN_DEPLOYMENT_DIR%"
+COPY "%OPENTWIN_DEV_ROOT%\Services\KrigingService\x64\Release\run.py" "%OPENTWIN_DEPLOYMENT_DIR%\Python"
+COPY "%OPENTWIN_DEV_ROOT%\Services\ModelingService\x64\Release\ModelingService.dll" "%OPENTWIN_DEPLOYMENT_DIR%"
+COPY "%OPENTWIN_DEV_ROOT%\Services\VisualizationService\x64\Release\VisualizationService.dll" "%OPENTWIN_DEPLOYMENT_DIR%"
+COPY "%OPENTWIN_DEV_ROOT%\Services\FITTDService\x64\Release\FITTDService.dll" "%OPENTWIN_DEPLOYMENT_DIR%"
+COPY "%OPENTWIN_DEV_ROOT%\Services\ImportParameterizedData\x64\Release\ImportParameterizedDataService.dll" "%OPENTWIN_DEPLOYMENT_DIR%"
+COPY "%OPENTWIN_DEV_ROOT%\Services\CartesianMeshService\x64\Release\CartesianMeshService.dll" "%OPENTWIN_DEPLOYMENT_DIR%"
+COPY "%OPENTWIN_DEV_ROOT%\Services\TetMeshService\x64\Release\TetMeshService.dll" "%OPENTWIN_DEPLOYMENT_DIR%"
+COPY "%OPENTWIN_DEV_ROOT%\Services\GetDPService\x64\Release\GetDPService.dll" "%OPENTWIN_DEPLOYMENT_DIR%"
+
+COPY "%OPENTWIN_DEV_ROOT%\Microservices\OpenTwin\target\release\open_twin.exe" "%OPENTWIN_DEPLOYMENT_DIR%"
+
+REM  Copy Tools
+ECHO Copy Tools
+COPY "%OPENTWIN_DEV_ROOT%\Tools\OToolkit\x64\Release\OToolkit.dll" "%OPENTWIN_DEPLOYMENT_DIR%"
+
+REM  Copy Admin Panel
 ECHO Copy Admin Panel
-
-XCOPY /E /Q "%SIM_PLAT_ROOT%\Microservices\AdminPanel\build\*.*" "%OT_DEPLOYMENT_DIR%\Apache\htdocs"
-COPY "%SIM_PLAT_ROOT%\Microservices\AdminPanel\Apache_config\.htaccess" "%OT_DEPLOYMENT_DIR%\Apache\htdocs"
-COPY /Y "%SIM_PLAT_ROOT%\Microservices\AdminPanel\Apache_config\httpd.conf" "%OT_DEPLOYMENT_DIR%\Apache\conf"
-COPY /Y "%SIM_PLAT_ROOT%\Microservices\AdminPanel\Apache_config\httpd-ahssl.conf" "%OT_DEPLOYMENT_DIR%\Apache\conf\extra"
+XCOPY /E /Q "%OPENTWIN_DEV_ROOT%\Microservices\AdminPanel\build\*.*" "%OPENTWIN_DEPLOYMENT_DIR%\Apache\htdocs"
+COPY "%OPENTWIN_DEV_ROOT%\Microservices\AdminPanel\Apache_config\.htaccess" "%OPENTWIN_DEPLOYMENT_DIR%\Apache\htdocs"
+COPY /Y "%OPENTWIN_DEV_ROOT%\Microservices\AdminPanel\Apache_config\httpd.conf" "%OPENTWIN_DEPLOYMENT_DIR%\Apache\conf"
+COPY /Y "%OPENTWIN_DEV_ROOT%\Microservices\AdminPanel\Apache_config\httpd-ahssl.conf" "%OPENTWIN_DEPLOYMENT_DIR%\Apache\conf\extra"
 
