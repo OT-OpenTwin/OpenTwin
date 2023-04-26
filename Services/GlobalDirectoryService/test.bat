2d27daf35b283813dd00b6f4aad6ad551d75c4d9
Jan Wagner <janwagner23@web.de>
2023-04-26 17:07:51 +0200

    GlobalDS adjusted to new project structure.

--- Services/GlobalDirectoryService/test.bat
+++ Services/GlobalDirectoryService/test.bat
@@ -3,7 +3,7 @@
 ECHO Setting up environment
 
 rem Setup eviroment
-CALL "%SIM_PLAT_ROOT%\MasterBuild\set_env.bat"
+CALL "%OPENTWIN_DEV_ROOT%\Scripts\SetupEnvironment.bat"
 
 ECHO Testing Project : GlobalDirectoryService
 
@@ -33,20 +33,20 @@ IF "%2"=="BUILD" (
 
 IF %DEBUG%==1 (
 	ECHO %TYPE% DEBUGTEST
-	"%DEVENV_ROOT_2022%\devenv.exe" "%SIM_PLAT_ROOT%\Libraries\GlobalDirectoryService\GlobalDirectoryService.vcxproj" %TYPE% "DebugTest|x64"
+	"%DEVENV_ROOT_2022%\devenv.exe" "%OPENTWIN_DEV_ROOT%\Services\GlobalDirectoryService\GlobalDirectoryService.vcxproj" %TYPE% "DebugTest|x64"
 	ECHO %TYPE% DEBUG
-	"%SIM_PLAT_ROOT%\Libraries\GlobalDirectoryService\x64\Debug\GlobalDirectoryServiceTest.exe" /Out --gtest_output="xml:%SIM_PLAT_ROOT%\MasterBuild\Reports\GlobalDirectoryServiceDebugReport.xml"
-	CALL "%SIM_PLAT_ROOT%\Third_Party_Libraries\Python\set_paths_dev.bat"
-	python "%SIM_PLAT_ROOT%\MasterBuild\modifyXML.py" "%SIM_PLAT_ROOT%\MasterBuild\Reports\GlobalDirectoryServiceDebugReport.xml" "GlobalDirectoryService" "%SIM_PLAT_ROOT%\MasterBuild\EditReports\GlobalDirectoryServiceDebugReport.xml"
+	"%OPENTWIN_DEV_ROOT%\Services\GlobalDirectoryService\x64\Debug\GlobalDirectoryServiceTest.exe" /Out --gtest_output="xml:%OPENTWIN_DEV_ROOT%\Scripts\Reports\GlobalDirectoryServiceDebugReport.xml"
+	CALL "%OPENTWIN_THIRDPARTY_ROOT%\Python\set_paths_dev.bat"
+	python "%OPENTWIN_DEV_ROOT%\Scripts\modifyXML.py" "%OPENTWIN_DEV_ROOT%\Scripts\Reports\GlobalDirectoryServiceDebugReport.xml" "GlobalDirectoryService" "%OPENTWIN_DEV_ROOT%\Scripts\EditReports\GlobalDirectoryServiceDebugReport.xml"
 )
 
 IF %RELEASE%==1 (
 	ECHO %TYPE% RELEASETEST
-	"%DEVENV_ROOT_2022%\devenv.exe" "%SIM_PLAT_ROOT%\Libraries\GlobalDirectoryService\GlobalDirectoryService.vcxproj" %TYPE% "ReleaseTest|x64"
+	"%DEVENV_ROOT_2022%\devenv.exe" "%OPENTWIN_DEV_ROOT%\Services\GlobalDirectoryService\GlobalDirectoryService.vcxproj" %TYPE% "ReleaseTest|x64"
 	ECHO %TYPE% RELEASE
-	"%SIM_PLAT_ROOT%\Libraries\GlobalDirectoryService\x64\Release\GlobalDirectoryServiceTest.exe" /Out --gtest_output="xml:%SIM_PLAT_ROOT%\MasterBuild\Reports\GlobalDirectoryServiceReleaseReport.xml"
-	CALL "%SIM_PLAT_ROOT%\Third_Party_Libraries\Python\set_paths_dev.bat"
-	python "%SIM_PLAT_ROOT%\MasterBuild\modifyXML.py" "%SIM_PLAT_ROOT%\MasterBuild\Reports\GlobalDirectoryServiceReleaseReport.xml" "GlobalDirectoryService" "%SIM_PLAT_ROOT%\MasterBuild\EditReports\GlobalDirectoryServiceReleaseReport.xml"
+	"%OPENTWIN_DEV_ROOT%\Services\GlobalDirectoryService\x64\Release\GlobalDirectoryServiceTest.exe" /Out --gtest_output="xml:%OPENTWIN_DEV_ROOT%\Scripts\Reports\GlobalDirectoryServiceReleaseReport.xml"
+	CALL "%OPENTWIN_THIRDPARTY_ROOT%\Python\set_paths_dev.bat"
+	python "%OPENTWIN_DEV_ROOT%\Scripts\modifyXML.py" "%OPENTWIN_DEV_ROOT%\Scripts\Reports\GlobalDirectoryServiceReleaseReport.xml" "GlobalDirectoryService" "%OPENTWIN_DEV_ROOT%\Scripts\EditReports\GlobalDirectoryServiceReleaseReport.xml"
 ) 
   
 :END
