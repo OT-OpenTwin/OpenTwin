0df38f22a959c85e04d7d18445ae3af801cedd01
alexk95 <57009007+alexk95@users.noreply.github.com>
2023-04-26 13:46:09 +0200

    Switched test report path to TestReports

--- Libraries/OpenTwinCommunication/test.bat
+++ Libraries/OpenTwinCommunication/test.bat
@@ -51,18 +51,18 @@ IF %DEBUG%==1 (
 	ECHO %TYPE% DEBUGTEST
 	"%DEVENV_ROOT_2022%\devenv.exe" "%OT_COMMUNICATION_ROOT%\OpenTwinCommunication.vcxproj" %TYPE% "DebugTest|x64"  
 	ECHO %TYPE% DEBUG
-	"%OT_COMMUNICATION_ROOT%\x64\Debug\OpenTwinCommunicationTest.exe" /Out --gtest_output="xml:%OPENTWIN_DEV_ROOT%\Scripts\BuildAndTest\Reports\OpenTwinCommunicationDebugReport.xml"
+	"%OT_COMMUNICATION_ROOT%\x64\Debug\OpenTwinCommunicationTest.exe" /Out --gtest_output="xml:%OPENTWIN_DEV_ROOT%\Scripts\BuildAndTest\TestReports\OpenTwinCommunicationDebugReport.xml"
 	CALL "%OPENTWIN_THIRDPARTY_ROOT%\Python\set_paths_dev.bat"
-	python "%OPENTWIN_DEV_ROOT%\Scripts\BuildAndTest\modifyXML.py" "%OPENTWIN_DEV_ROOT%\Scripts\BuildAndTest\Reports\OpenTwinCommunicationDebugReport.xml" "OpenTwinCommunication" "%OPENTWIN_DEV_ROOT%\Scripts\BuildAndTest\EditReports\OpenTwinCommunicationDebugReport.xml"
+	python "%OPENTWIN_DEV_ROOT%\Scripts\BuildAndTest\modifyXML.py" "%OPENTWIN_DEV_ROOT%\Scripts\BuildAndTest\TestReports\OpenTwinCommunicationDebugReport.xml" "OpenTwinCommunication" "%OPENTWIN_DEV_ROOT%\Scripts\BuildAndTest\EditReports\OpenTwinCommunicationDebugReport.xml"
 )
 
 IF %RELEASE%==1 (
 	ECHO %TYPE% RELEASETEST
 	"%DEVENV_ROOT_2022%\devenv.exe" "%OT_COMMUNICATION_ROOT%\OpenTwinCommunication.vcxproj" %TYPE% "ReleaseTest|x64"
 	ECHO %TYPE% RELEASE
-	"%OT_COMMUNICATION_ROOT%\x64\Release\OpenTwinCommunicationTest.exe" /Out --gtest_output="xml:%OPENTWIN_DEV_ROOT%\Scripts\BuildAndTest\Reports\OpenTwinCommunicationReleaseReport.xml"
+	"%OT_COMMUNICATION_ROOT%\x64\Release\OpenTwinCommunicationTest.exe" /Out --gtest_output="xml:%OPENTWIN_DEV_ROOT%\Scripts\BuildAndTest\TestReports\OpenTwinCommunicationReleaseReport.xml"
 	CALL "%OPENTWIN_THIRDPARTY_ROOT%\Python\set_paths_dev.bat"
-	python "%OPENTWIN_DEV_ROOT%\Scripts\BuildAndTest\modifyXML.py" "%OPENTWIN_DEV_ROOT%\Scripts\BuildAndTest\Reports\OpenTwinCommunicationReleaseReport.xml" "OpenTwinCommunication" "%OPENTWIN_DEV_ROOT%\Scripts\BuildAndTest\EditReports\OpenTwinCommunicationReleaseReport.xml"
+	python "%OPENTWIN_DEV_ROOT%\Scripts\BuildAndTest\modifyXML.py" "%OPENTWIN_DEV_ROOT%\Scripts\BuildAndTest\TestReports\OpenTwinCommunicationReleaseReport.xml" "OpenTwinCommunication" "%OPENTWIN_DEV_ROOT%\Scripts\BuildAndTest\EditReports\OpenTwinCommunicationReleaseReport.xml"
 ) 
   
 GOTO END
