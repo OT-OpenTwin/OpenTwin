49d0818f6588aeb4c205099bac8de93ddff45428
alexk95 <57009007+alexk95@users.noreply.github.com>
2023-04-26 17:37:27 +0200

    Restructure: Rubberband API

--- Libraries/RubberbandEngineCore/test.bat
+++ Libraries/RubberbandEngineCore/test.bat
@@ -1,9 +1,31 @@
 @ECHO OFF
 
-ECHO Setting up environment
+REM This script requires the following environment variables to be set:
+REM 1. OPENTWIN_DEV_ROOT
+REM 2. OPENTWIN_THIRDPARTY_ROOT
+REM 3. DEVENV_ROOT_2022
+IF "%OPENTWIN_DEV_ROOT%" == "" (
+	ECHO Please specify the following environment variables: OPENTWIN_DEV_ROOT
+	goto PAUSE_END
+)
 
-rem Setup eviroment
-CALL "%SIM_PLAT_ROOT%\MasterBuild\set_env.bat"
+IF "%OPENTWIN_THIRDPARTY_ROOT%" == "" (
+	ECHO Please specify the following environment variables: OPENTWIN_THIRDPARTY_ROOT
+	goto PAUSE_END
+)
+
+IF "%DEVENV_ROOT_2022%" == "" (
+	ECHO Please specify the following environment variables: DEVENV_ROOT_2022
+	goto PAUSE_END
+)
+
+REM Setup eviroment
+CALL "%OPENTWIN_DEV_ROOT%\Scripts\SetupEnvironment.bat"
+
+REM Ensure that the script finished successfully
+IF NOT "%OPENTWIN_DEV_ENV_DEFINED%" == "1" (
+	goto END
+)
 
 ECHO Testing Project : RubberbandEngineCore
 
@@ -34,28 +56,26 @@ IF "%2"=="BUILD" (
 
 IF %DEBUG%==1 (
 	ECHO %TYPE% DEBUGTEST
-	"%DEVENV_ROOT_2022%\devenv.exe" "%SIM_PLAT_ROOT%\Libraries\RubberbandEngineCore\RubberbandEngineCore.vcxproj" %TYPE% "DebugTest|x64"  
+	"%DEVENV_ROOT_2022%\devenv.exe" "%OT_RUBBERBANDAPI_ROOT%\RubberbandEngineCore.vcxproj" %TYPE% "DebugTest|x64"  
 	ECHO %TYPE% DEBUG
-	"%SIM_PLAT_ROOT%\Libraries\RubberbandEngineCore\x64\Debug\RubberbandEngineCoreTest.exe" /Out --gtest_output="xml:%SIM_PLAT_ROOT%\MasterBuild\Reports\RubberbandEngineCoreDebugReport.xml"
-	CALL "%SIM_PLAT_ROOT%\Third_Party_Libraries\Python\set_paths_dev.bat"
-	python "%SIM_PLAT_ROOT%\MasterBuild\modifyXML.py" "%SIM_PLAT_ROOT%\MasterBuild\Reports\RubberbandEngineCoreDebugReport.xml" "RubberbandEngineCore" "%SIM_PLAT_ROOT%\MasterBuild\EditReports\RubberbandEngineCoreDebugReport.xml"
+	"%OT_RUBBERBANDAPI_ROOT%\x64\Debug\RubberbandEngineCoreTest.exe" /Out --gtest_output="xml:%OPENTWIN_DEV_ROOT%\Scripts\BuildAndTest\Reports\RubberbandEngineCoreDebugReport.xml"
+	CALL "%OPENTWIN_THIRDPARTY_ROOT%\Python\set_paths_dev.bat"
+	python "%OPENTWIN_DEV_ROOT%\Scripts\BuildAndTest\modifyXML.py" "%OPENTWIN_DEV_ROOT%\Scripts\BuildAndTest\Reports\RubberbandEngineCoreDebugReport.xml" "RubberbandEngineCore" "%OPENTWIN_DEV_ROOT%\Scripts\BuildAndTest\EditReports\RubberbandEngineCoreDebugReport.xml"
 )
 
 IF %RELEASE%==1 (
 	ECHO %TYPE% RELEASETEST
-	"%DEVENV_ROOT_2022%\devenv.exe" "%SIM_PLAT_ROOT%\Libraries\RubberbandEngineCore\RubberbandEngineCore.vcxproj" %TYPE% "ReleaseTest|x64"
+	"%DEVENV_ROOT_2022%\devenv.exe" "%OT_RUBBERBANDAPI_ROOT%\RubberbandEngineCore.vcxproj" %TYPE% "ReleaseTest|x64"
 	ECHO %TYPE% RELEASE
-	"%SIM_PLAT_ROOT%\Libraries\RubberbandEngineCore\x64\Release\RubberbandEngineCoreTest.exe" /Out --gtest_output="xml:%SIM_PLAT_ROOT%\MasterBuild\Reports\RubberbandEngineCoreReleaseReport.xml"
-	CALL "%SIM_PLAT_ROOT%\Third_Party_Libraries\Python\set_paths_dev.bat"
-	python "%SIM_PLAT_ROOT%\MasterBuild\modifyXML.py" "%SIM_PLAT_ROOT%\MasterBuild\Reports\RubberbandEngineCoreReleaseReport.xml" "RubberbandEngineCore" "%SIM_PLAT_ROOT%\MasterBuild\EditReports\RubberbandEngineCoreReleaseReport.xml"
+	"%OT_RUBBERBANDAPI_ROOT%\x64\Release\RubberbandEngineCoreTest.exe" /Out --gtest_output="xml:%OPENTWIN_DEV_ROOT%\Scripts\BuildAndTest\Reports\RubberbandEngineCoreReleaseReport.xml"
+	CALL "%OPENTWIN_THIRDPARTY_ROOT%\Python\set_paths_dev.bat"
+	python "%OPENTWIN_DEV_ROOT%\Scripts\BuildAndTest\modifyXML.py" "%OPENTWIN_DEV_ROOT%\Scripts\BuildAndTest\Reports\RubberbandEngineCoreReleaseReport.xml" "RubberbandEngineCore" "%OPENTWIN_DEV_ROOT%\Scripts\BuildAndTest\EditReports\RubberbandEngineCoreReleaseReport.xml"
 ) 
   
+GOTO END
+
+:PAUSE_END
+pause
+GOTO END
+
 :END
-
-
-
-
-
-
-
-
