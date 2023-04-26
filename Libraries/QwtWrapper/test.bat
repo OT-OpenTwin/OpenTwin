d6566731834865a6c439ba2f0087dda5ea37c645
alexk95 <57009007+alexk95@users.noreply.github.com>
2023-04-26 17:25:58 +0200

    Restructure: qwt wrapper

--- Libraries/QwtWrapper/test.bat
+++ Libraries/QwtWrapper/test.bat
@@ -1,9 +1,26 @@
 @ECHO OFF
 
-ECHO Setting up environment
+REM This script requires the following environment variables to be set:
+REM 1. OPENTWIN_DEV_ROOT
+REM 2. OPENTWIN_THIRDPARTY_ROOT
+REM 3. DEVENV_ROOT_2022
+IF "%OPENTWIN_DEV_ROOT%" == "" (
+	ECHO Please specify the following environment variables: OPENTWIN_DEV_ROOT
+	goto PAUSE_END
+)
 
-rem Setup eviroment
-CALL "%SIM_PLAT_ROOT%\MasterBuild\set_env.bat"
+IF "%OPENTWIN_THIRDPARTY_ROOT%" == "" (
+	ECHO Please specify the following environment variables: OPENTWIN_THIRDPARTY_ROOT
+	goto PAUSE_END
+)
+
+IF "%DEVENV_ROOT_2022%" == "" (
+	ECHO Please specify the following environment variables: DEVENV_ROOT_2022
+	goto PAUSE_END
+)
+
+REM Setup eviroment
+CALL "%OPENTWIN_DEV_ROOT%\Scripts\SetupEnvironment.bat"
 
 ECHO Testing Project : QwtWrapper
 
@@ -34,28 +51,26 @@ IF "%2"=="BUILD" (
 
 IF %DEBUG%==1 (
 	ECHO %TYPE% DEBUGTEST
-	"%DEVENV_ROOT_2022%\devenv.exe" "%SIM_PLAT_ROOT%\Libraries\QwtWrapper\QwtWrapper.vcxproj" %TYPE% "DebugTest|x64"  
+	"%DEVENV_ROOT_2022%\devenv.exe" "%OT_QWTWRAPPER_ROOT%\QwtWrapper.vcxproj" %TYPE% "DebugTest|x64"
 	ECHO %TYPE% DEBUG
-	"%SIM_PLAT_ROOT%\Libraries\QwtWrapper\x64\Debug\QwtWrapperTest.exe" /Out --gtest_output="xml:%SIM_PLAT_ROOT%\MasterBuild\Reports\QwtWrapperDebugReport.xml"
-	CALL "%SIM_PLAT_ROOT%\Third_Party_Libraries\Python\set_paths_dev.bat"
-	python "%SIM_PLAT_ROOT%\MasterBuild\modifyXML.py" "%SIM_PLAT_ROOT%\MasterBuild\Reports\QwtWrapperDebugReport.xml" "QwtWrapper" "%SIM_PLAT_ROOT%\MasterBuild\EditReports\QwtWrapperDebugReport.xml"
+	"%OT_QWTWRAPPER_ROOT%\x64\Debug\QwtWrapperTest.exe" /Out --gtest_output="xml:%OPENTWIN_DEV_ROOT%\Scripts\BuildAndTest\TestReports\QwtWrapperDebugReport.xml"
+	CALL "%OPENTWIN_THIRDPARTY_ROOT%\Python\set_paths_dev.bat"
+	python "%OPENTWIN_DEV_ROOT%\Scripts\BuildAndTest\modifyXML.py" "%OPENTWIN_DEV_ROOT%\Scripts\BuildAndTest\TestReports\QwtWrapperDebugReport.xml" "QwtWrapper" "%OPENTWIN_DEV_ROOT%\Scripts\BuildAndTest\EditReports\QwtWrapperDebugReport.xml"
 )
 
 IF %RELEASE%==1 (
 	ECHO %TYPE% RELEASETEST
-	"%DEVENV_ROOT_2022%\devenv.exe" "%SIM_PLAT_ROOT%\Libraries\QwtWrapper\QwtWrapper.vcxproj" %TYPE% "ReleaseTest|x64"
+	"%DEVENV_ROOT_2022%\devenv.exe" "%OT_QWTWRAPPER_ROOT%\QwtWrapper.vcxproj" %TYPE% "ReleaseTest|x64"
 	ECHO %TYPE% RELEASE
-	"%SIM_PLAT_ROOT%\Libraries\QwtWrapper\x64\Release\QwtWrapperTest.exe" /Out --gtest_output="xml:%SIM_PLAT_ROOT%\MasterBuild\Reports\QwtWrapperReleaseReport.xml"
-	CALL "%SIM_PLAT_ROOT%\Third_Party_Libraries\Python\set_paths_dev.bat"
-	python "%SIM_PLAT_ROOT%\MasterBuild\modifyXML.py" "%SIM_PLAT_ROOT%\MasterBuild\Reports\QwtWrapperReleaseReport.xml" "QwtWrapper" "%SIM_PLAT_ROOT%\MasterBuild\EditReports\QwtWrapperReleaseReport.xml"
+	"%OT_QWTWRAPPER_ROOT%\x64\Release\QwtWrapperTest.exe" /Out --gtest_output="xml:%OPENTWIN_DEV_ROOT%\Scripts\BuildAndTest\TestReports\QwtWrapperReleaseReport.xml"
+	CALL "%OPENTWIN_THIRDPARTY_ROOT%\Python\set_paths_dev.bat"
+	python "%OPENTWIN_DEV_ROOT%\Scripts\BuildAndTest\modifyXML.py" "%OPENTWIN_DEV_ROOT%\Scripts\BuildAndTest\TestReports\QwtWrapperReleaseReport.xml" "QwtWrapper" "%OPENTWIN_DEV_ROOT%\Scripts\BuildAndTest\EditReports\QwtWrapperReleaseReport.xml"
 ) 
   
+GOTO END
+
+:PAUSE_END
+pause
+GOTO END
+
 :END
-
-
-
-
-
-
-
-
