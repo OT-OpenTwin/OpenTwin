1f3a68e6923ad8a4458c8e36eda8c60b445be8cc
Jan Wagner <janwagner23@web.de>
2023-04-27 13:09:20 +0200

    Adjusted project batch files to new project structure.

--- Services/LocalDirectoryService/test.bat
+++ Services/LocalDirectoryService/test.bat
@@ -3,7 +3,7 @@
 ECHO Setting up environment
 
 rem Setup eviroment
-CALL "%SIM_PLAT_ROOT%\MasterBuild\set_env.bat"
+CALL "%OPENTWIN_DEV_ROOT%\Scripts\SetupEnvironment.bat"
 
 ECHO Testing Project : LocalDirectoryService
 
@@ -34,20 +34,20 @@ IF "%2"=="BUILD" (
 
 IF %DEBUG%==1 (
 	ECHO %TYPE% DEBUGTEST
-	"%DEVENV_ROOT_2022%\devenv.exe" "%SIM_PLAT_ROOT%\Libraries\LocalDirectoryService\LocalDirectoryService.vcxproj" %TYPE% "DebugTest|x64"  
+	"%DEVENV_ROOT_2022%\devenv.exe" "%OPENTWIN_DEV_ROOT%\Services\LocalDirectoryService\LocalDirectoryService.vcxproj" %TYPE% "DebugTest|x64"  
 	ECHO %TYPE% DEBUG
-	"%SIM_PLAT_ROOT%\Libraries\LocalDirectoryService\x64\Debug\LocalDirectoryServiceTest.exe" /Out --gtest_output="xml:%SIM_PLAT_ROOT%\MasterBuild\Reports\LocalDirectoryServiceDebugReport.xml"
-	CALL "%SIM_PLAT_ROOT%\Third_Party_Libraries\Python\set_paths_dev.bat"
-	python "%SIM_PLAT_ROOT%\MasterBuild\modifyXML.py" "%SIM_PLAT_ROOT%\MasterBuild\Reports\LocalDirectoryServiceDebugReport.xml" "LocalDirectoryService" "%SIM_PLAT_ROOT%\MasterBuild\EditReports\LocalDirectoryServiceDebugReport.xml"
+	"%OPENTWIN_DEV_ROOT%\Services\LocalDirectoryService\x64\Debug\LocalDirectoryServiceTest.exe" /Out --gtest_output="xml:%OPENTWIN_DEV_ROOT%\Scripts\Reports\LocalDirectoryServiceDebugReport.xml"
+	CALL "%OPENTWIN_THIRDPARTY_ROOT%\Python\set_paths_dev.bat"
+	python "%OPENTWIN_DEV_ROOT%\Scripts\modifyXML.py" "%OPENTWIN_DEV_ROOT%\Scripts\Reports\LocalDirectoryServiceDebugReport.xml" "LocalDirectoryService" "%OPENTWIN_DEV_ROOT%\Scripts\EditReports\LocalDirectoryServiceDebugReport.xml"
 )
 
 IF %RELEASE%==1 (
 	ECHO %TYPE% RELEASETEST
-	"%DEVENV_ROOT_2022%\devenv.exe" "%SIM_PLAT_ROOT%\Libraries\LocalDirectoryService\LocalDirectoryService.vcxproj" %TYPE% "ReleaseTest|x64"
+	"%DEVENV_ROOT_2022%\devenv.exe" "%OPENTWIN_DEV_ROOT%\Services\LocalDirectoryService\LocalDirectoryService.vcxproj" %TYPE% "ReleaseTest|x64"
 	ECHO %TYPE% RELEASE
-	"%SIM_PLAT_ROOT%\Libraries\LocalDirectoryService\x64\Release\LocalDirectoryServiceTest.exe" /Out --gtest_output="xml:%SIM_PLAT_ROOT%\MasterBuild\Reports\LocalDirectoryServiceReleaseReport.xml"
-	CALL "%SIM_PLAT_ROOT%\Third_Party_Libraries\Python\set_paths_dev.bat"
-	python "%SIM_PLAT_ROOT%\MasterBuild\modifyXML.py" "%SIM_PLAT_ROOT%\MasterBuild\Reports\LocalDirectoryServiceReleaseReport.xml" "LocalDirectoryService" "%SIM_PLAT_ROOT%\MasterBuild\EditReports\LocalDirectoryServiceReleaseReport.xml"
+	"%OPENTWIN_DEV_ROOT%\Services\LocalDirectoryService\x64\Release\LocalDirectoryServiceTest.exe" /Out --gtest_output="xml:%OPENTWIN_DEV_ROOT%\Scripts\Reports\LocalDirectoryServiceReleaseReport.xml"
+	CALL "%OPENTWIN_THIRDPARTY_ROOT%\Python\set_paths_dev.bat"
+	python "%OPENTWIN_DEV_ROOT%\Scripts\modifyXML.py" "%OPENTWIN_DEV_ROOT%\Scripts\Reports\LocalDirectoryServiceReleaseReport.xml" "LocalDirectoryService" "%OPENTWIN_DEV_ROOT%\Scripts\EditReports\LocalDirectoryServiceReleaseReport.xml"
 ) 
   
 :END
