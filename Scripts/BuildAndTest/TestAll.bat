18a8ae6ae3e661983920197942870e7fc3dd40cc
Jan Wagner <janwagner23@web.de>
2023-05-01 21:01:45 +0200

    Adjustedc

--- Scripts/BuildAndTest/TestAll.bat
+++ Scripts/BuildAndTest/TestAll.bat
@@ -5,100 +5,192 @@ REM The second argument of hte shell defines whether a full rebuild or just a bu
 REM Please note that the commands are case-sensitive and that they must not be enclosed in quotes.
 
 REM This script requires the following environment variables to be set:
-REM 1. SIM_PLAT_ROOT
+REM 1. OPENTWIN_DEV_ROOT
 
-IF "%SIM_PLAT_ROOT%"=="" (
-	ECHO Please specify the following environment variables: SIM_PLAT_ROOT
+CALL "%OPENTWIN_DEV_ROOT%\Scripts\SetupEnvironment.bat"
+
+IF "%OPENTWIN_DEV_ROOT%"=="" (
+	ECHO Please specify the following environment variables: OPENTWIN_DEV_ROOT
 	goto END
 )
-
+SET testBat=\test.bat
 
 ECHO ====================================================================
-ECHO Test the libraries 
+ECHO Test the services 
 ECHO ====================================================================
 
 ECHO ===============================================================
-ECHO Test Library: DataStorage
+ECHO Test Service: Authorisation
 ECHO ===============================================================
-CALL "%SIM_PLAT_ROOT%\Libraries\DataStorage\test.bat" %1 %2
+CALL "%OT_AUTHORISATION_SERVICE_ROOT%%testBat%" %1 %2
+
+ECHO ===============================================================
+ECHO Test Service: Model
+ECHO ===============================================================
+CALL "%OT_MODEL_SERVICE_ROOT%%testBat%" %1 %2
+
+ECHO ===============================================================
+ECHO Test Service: Global Session
+ECHO ===============================================================
+CALL "%OT_GLOBAL_SESSION_SERVICE_ROOT%%testBat%" %1 %2
+
+ECHO ===============================================================
+ECHO Test Service: Local Session
+ECHO ===============================================================
+CALL "%OT_LOCAL_SESSION_SERVICE_ROOT%%testBat%" %1 %2
+
+ECHO ===============================================================
+ECHO Test Service: Global Directory
+ECHO ===============================================================
+CALL "%OT_GLOBAL_DIRECTORY_SERVICE_ROOT%%testBat%" %1 %2
+
+ECHO ===============================================================
+ECHO Test Service: Local Directory
+ECHO ===============================================================
+CALL "%OT_LOCAL_DIRECTORY_SERVICE_ROOT%%testBat%" %1 %2
+
+ECHO ===============================================================
+ECHO Test Service: Local Directory
+ECHO ===============================================================
+CALL "%OT_RELAY_SERVICE_ROOT%%testBat%" %1 %2
+
+ECHO ===============================================================
+ECHO Test Service: Logger
+ECHO ===============================================================
+CALL "%OT_LOGGER_SERVICE_ROOT%%testBat%" %1 %2
+
+ECHO ===============================================================
+ECHO Test Service: PHREEC
+ECHO ===============================================================
+CALL "%OT_PHREEC_SERVICE_ROOT%%testBat%" %1 %2
+
+ECHO ===============================================================
+ECHO Test Service: KRIGING
+ECHO ===============================================================
+CALL "%OT_KRIGING_SERVICE_ROOT%%testBat%" %1 %2
+
+ECHO ===============================================================
+ECHO Test Service: Modeling
+ECHO ===============================================================
+CALL "%OT_MODELING_SERVICE_ROOT%%testBat% %1 %2
+
+ECHO ===============================================================
+ECHO Test Service: Visualization
+ECHO ===============================================================
+CALL "%OT_VISUALIZATION_SERVICE_ROOT%%testBat%" %1 %2
+
+ECHO ===============================================================
+ECHO Test Service: FITTD
+ECHO ===============================================================
+CALL "%OT_FITTD_SERVICE_ROOT%%testBat% %1 %2
+
+ECHO ===============================================================
+ECHO Test Service: Cartesian Mesh
+ECHO ===============================================================
+CALL "%OT_CARTESIAN_MESH_SERVICE_ROOT%%testBat%" %1 %2
+
+ECHO ===============================================================
+ECHO Test Service: Tet Mesh
+ECHO ===============================================================
+CALL "%OT_TET_MESH_SERVICE_ROOT%%testBat%" %1 %2
+
+ECHO ===============================================================
+ECHO Test Service: Import Parameterized Data
+ECHO ===============================================================
+CALL "%OT_IMPORT_PARAMETERIZED_DATA_SERVICE_ROOT%%testBat%" %1 %2
+
+ECHO ===============================================================
+ECHO Test Service: GetDP
+ECHO ===============================================================
+CALL "%OT_GETDP_SERVICE_ROOT%%testBat%" %1 %2
+
+ECHO ===============================================================
+ECHO Test Service: UI
+ECHO ===============================================================
+CALL "%OT_UI_SERVICE_ROOT%%testBat%" %1 %2
+
+
+ECHO ====================================================================
+ECHO Test the libraries
+ECHO ====================================================================
+
+
+ECHO ===============================================================
+ECHO Test Datastorage: DataStorage
+ECHO ===============================================================
+CALL "%OT_DATASTORAGE_ROOT%%testBat%" %1 %2
 
 ECHO ===============================================================
 ECHO Test Library: ModelEntities
 ECHO ===============================================================
-CALL "%SIM_PLAT_ROOT%\Libraries\ModelEntities\test.bat" %1 %2
+CALL "%OT_MODELENTITIES_ROOT%%testBat%" %1 %2
 
 ECHO ===============================================================
 ECHO Test Library: OpenTwinServiceFoundation
 ECHO ===============================================================
-CALL "%SIM_PLAT_ROOT%\Libraries\OpenTwinServiceFoundation\test.bat" %1 %2
+CALL "%OT_FOUNDATION_ROOT%%testBat%" %1 %2
 
 ECHO ===============================================================
 ECHO Test Library: OpenTwinCommunication
 ECHO ===============================================================
-CALL "%SIM_PLAT_ROOT%\Libraries\OpenTwinCommunication\test.bat" %1 %2
-
-ECHO ===============================================================
-ECHO Test Library: OpenTwinLogger
-ECHO ===============================================================
-CALL "%SIM_PLAT_ROOT%\Libraries\OpenTwinLogger\test.bat" %1 %2
-
-ECHO ===============================================================
-ECHO Test Library: RelayService
-ECHO ===============================================================
-CALL "%SIM_PLAT_ROOT%\Libraries\RelayService\test.bat" %1 %2
-
-ECHO ===============================================================
-ECHO Test Library: ServiceTemplate
-ECHO ===============================================================
-CALL "%SIM_PLAT_ROOT%\Libraries\ServiceTemplate\test.bat" %1 %2
-
-ECHO ===============================================================
-ECHO Test Library: ServiceTemplate
-ECHO ===============================================================
-CALL "%SIM_PLAT_ROOT%\Libraries\ServiceTemplate\test.bat" %1 %2
-
-ECHO ===============================================================
-ECHO Test Library: PHREECService
-ECHO ===============================================================
-CALL "%SIM_PLAT_ROOT%\Libraries\PHREECService\test.bat" %1 %2
-
-ECHO ===============================================================
-ECHO Test Library: AuthorisationService
-ECHO ===============================================================
-CALL "%SIM_PLAT_ROOT%\Libraries\AuthorisationService\test.bat" %1 %2
+CALL "%OT_COMMUNICATION_ROOT%%testBat%" %1 %2
 
 ECHO ===============================================================
 ECHO Test Library: CADModelEntities
 ECHO ===============================================================
-CALL "%SIM_PLAT_ROOT%\Libraries\CADModelEntities\test.bat" %1 %2
+CALL "%OT_CADMODELENTITIES_ROOT%%testBat%" %1 %2
 
 ECHO ===============================================================
-ECHO Test Library: CartesianMeshService
+ECHO Test Library: BlockEditor
 ECHO ===============================================================
-CALL "%SIM_PLAT_ROOT%\Libraries\CartesianMeshService\test.bat" %1 %2
+CALL "%OT_BLOCKEDITOR_ROOT%%testBat%" %1 %2
 
 ECHO ===============================================================
-ECHO Test Library: TetMeshService
+ECHO Test Library: BlockEditorAPI
 ECHO ===============================================================
-CALL "%SIM_PLAT_ROOT%\Libraries\TetMeshService\test.bat" %1 %2
+CALL "%OT_BLOCKEDITORAPI_ROOT%%testBat%" %1 %2
 
 ECHO ===============================================================
-ECHO Test Library: FITTDService
+ECHO Test Library: Core
 ECHO ===============================================================
-CALL "%SIM_PLAT_ROOT%\Libraries\FITTDService\test.bat" %1 %2
+CALL "%OT_CORE_ROOT%%testBat%" %1 %2
 
 ECHO ===============================================================
-ECHO Test Library: GlobalSessionService
+ECHO Test Library: System
 ECHO ===============================================================
-CALL "%SIM_PLAT_ROOT%\Libraries\GlobalSessionService\test.bat" %1 %2
+CALL "%OT_SYSTEM_ROOT%%testBat%" %1 %2
 
 ECHO ===============================================================
-ECHO Test Library: LoggerService
+ECHO Test Library: GUI
 ECHO ===============================================================
-CALL "%SIM_PLAT_ROOT%\Libraries\LoggerService\test.bat" %1 %2
+CALL "%OT_GUI_ROOT%%testBat%" %1 %2
 
 ECHO ===============================================================
-ECHO Test Library: Model
+ECHO Test Library: QWTWrapper
 ECHO ===============================================================
-CALL "%SIM_PLAT_ROOT%\Libraries\Model\test.bat" %1 %2
+CALL "%OT_QWTWRAPPER_ROOT%%testBat%" %1 %2
 
+ECHO ===============================================================
+ECHO Test Library: RubberbandAPI
+ECHO ===============================================================
+CALL "%OT_RUBBERBANDAPI_ROOT%%testBat%" %1 %2
+
+ECHO ===============================================================
+ECHO Test Library: Rubberband
+ECHO ===============================================================
+CALL "%OT_RUBBERBAND_ROOT%%testBat%" %1 %2
+
+ECHO ===============================================================
+ECHO Test Library: UICore
+ECHO ===============================================================
+CALL "%OT_UICORE_ROOT%%testBat%" %1 %2
+
+ECHO ===============================================================
+ECHO Test Library: UIPluginAPI
+ECHO ===============================================================
+CALL "%OT_UIPLUGINAPI_ROOT%%testBat%" %1 %2
+
+ECHO ===============================================================
+ECHO Test Library: Viewer
+ECHO ===============================================================
+CALL "%OT_VIEWER_ROOT%%testBat%" %1 %2
