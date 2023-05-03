dc509cbd89d12745a25ad5ec25f5c2b40061ee78
alexk95 <57009007+alexk95@users.noreply.github.com>
2023-05-03 15:52:05 +0200

    Fixed issue in the launcher batch files

--- Scripts/Launcher/OpenTwin_localUI.bat
+++ Scripts/Launcher/OpenTwin_localUI.bat
@@ -3,6 +3,9 @@
 REM It is expected that the session service and the authentification services
 REM are already running on the remote computer
 
+CALL OpenTwin_set_up_certificates.bat
+CALL OpenTwin_set_up_services.bat
+
 REM ===========================================================================
 REM Launch the UI executable 
 REM ===========================================================================
