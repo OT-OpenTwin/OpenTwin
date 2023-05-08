64fd469fe685b965e0d5d154b1bf427ff5313978
alexk95 <57009007+alexk95@users.noreply.github.com>
2023-05-08 22:56:59 +0200

    Added batch file for editing developer documentation in code

---
+++ Documentation/Developer/MakeHtml.bat
@@ -0,0 +1,22 @@
+@ECHO OFF
+
+REM This script requires the following environment variables to be set:
+REM 1. OPENTWIN_DEV_ROOT
+REM 2. OPENTWIN_THIRDPARTY_ROOT
+IF "%OPENTWIN_DEV_ROOT%" == "" (
+	ECHO Please specify the following environment variables: OPENTWIN_DEV_ROOT
+	goto PAUSE_END
+)
+
+IF "%OPENTWIN_THIRDPARTY_ROOT%" == "" (
+	ECHO Please specify the following environment variables: OPENTWIN_THIRDPARTY_ROOT
+	goto PAUSE_END
+)
+
+REM Setup eviroment
+CALL "%OPENTWIN_DEV_ROOT%\Scripts\SetupEnvironment.bat"
+
+CALL "%OPENTWIN_DEV_ROOT%\Documentation\Developer\make.bat" html
+
+:PAUSE_END
+pause
