b16aee50b9b86dc6390162ee275f24c773eb3fcb
alexk95 <57009007+alexk95@users.noreply.github.com>
2023-05-18 13:00:55 +0200

    Added nullptr assert

--- Libraries/OpenTwinCore/include/OpenTwinCore/otAssert.h
+++ Libraries/OpenTwinCore/include/OpenTwinCore/otAssert.h
@@ -7,7 +7,15 @@
 //! @brief Adds the ability to provide a custom text to the cassert macro (assertion only in debug mode)
 #define otAssert(___expression, ___message) (void)((!!(___expression)) || (_wassert(_CRT_WIDE(#___expression) L"\n\n" _CRT_WIDE(___message), _CRT_WIDE(__FILE__), (unsigned)__LINE__), 0))
 
+//! @brief Check if the provided pointer is not a nullptr
+#define otAssertNullptr(___ptr) (void)((!!(___ptr != nullptr)) || (_wassert(_CRT_WIDE(#___ptr) _CRT_WIDE("\n\n[Caution]\nnullptr provided"), _CRT_WIDE(__FILE__), (unsigned)__LINE__), 0))
+
 #else
+
 //! @brief Adds the ability to provide a custom text to the cassert macro (assertion only in debug mode)
 #define otAssert(___expression, ___message)
+
+//! @brief Check if the provided pointer is not a nullptr
+#define otAssertNullptr(___ptr)
+
 #endif // _DEBUG
