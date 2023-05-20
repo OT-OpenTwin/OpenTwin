dcd058f1173d5b6e94136f26db0b4d67290a2ed8
alexk95 <57009007+alexk95@users.noreply.github.com>
2023-05-20 20:42:39 +0200

    Create RunDevEnv2022WithEnvSet.bat

---
+++ Scripts/Other/RunDevEnv2022WithEnvSet.bat
@@ -0,0 +1,49 @@
+@ECHO OFF
+REM This script requires the following environment variables to be set:
+REM 1. OPENTWIN_DEV_ROOT
+REM 2. OPENTWIN_THIRDPARTY_ROOT
+REM 3. DEVENV_ROOT_2022
+
+IF "%OPENTWIN_DEV_ROOT%" == "" (
+	ECHO Please specify the following environment variables: OPENTWIN_DEV_ROOT
+	goto PAUSE_END
+)
+
+IF "%OPENTWIN_THIRDPARTY_ROOT%" == "" (
+	ECHO Please specify the following environment variables: OPENTWIN_THIRDPARTY_ROOT
+	goto PAUSE_END
+)
+
+IF "%DEVENV_ROOT_2022%" == "" (
+	ECHO Please specify the following environment variables: DEVENV_ROOT_2022
+	goto PAUSE_END
+)
+
+REM Call third party environment shell
+CALL "%OPENTWIN_DEV_ROOT%\Scripts\SetupEnvironment.bat"
+
+REM Ensure that the script finished successfully
+IF NOT "%OPENTWIN_DEV_ENV_DEFINED%" == "1" (
+	goto END
+)
+
+ECHO Launching development enviroment
+
+IF "%1" == "" (
+	REM Open without project
+	START "" "%DEVENV_ROOT_2022%\devenv.exe"
+)
+ELSE (
+	REM Open with project
+	START "" "%DEVENV_ROOT_2022%\devenv.exe" %1
+)
+
+GOTO END
+
+REM In case we want the user to see the messages before closing the window
+:PAUSE_END
+pause
+GOTO END
+
+:END
+
