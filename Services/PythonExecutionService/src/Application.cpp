14089a0aaf95f057a23926a75f35b3f2c0698d54
Jan Wagner <janwagner23@web.de>
2023-05-21 19:54:31 +0200

    Minor project changes

---
+++ Services/PythonExecutionService/src/Application.cpp
@@ -0,0 +1,151 @@
+/*
+ * Application.cpp
+ *
+ *  Created on:
+ *	Author:
+ *  Copyright (c)
+ */
+
+// Service header
+#include "Application.h"
+#include "ModelNotifier.h"
+#include "UiNotifier.h"
+
+// Open twin header
+#include "OpenTwinFoundation/UiComponent.h"
+#include "OpenTwinFoundation/ModelComponent.h"
+#include "OpenTwinCommunication/ActionTypes.h"
+
+#include <rapidjson/document.h>
+#include <rapidjson/rapidjson.h>
+
+Application * g_instance{ nullptr };
+
+Application * Application::instance(void) {
+	if (g_instance == nullptr) { g_instance = new Application; }
+	return g_instance;
+}
+
+void Application::deleteInstance(void) {
+	if (g_instance) { delete g_instance; }
+	g_instance = nullptr;
+}
+
+Application::Application()
+	: ot::ApplicationBase(OT_INFO_SERVICE_TYPE_PYTHON_EXECUTION_SERVICE, OT_INFO_SERVICE_TYPE_PYTHON_EXECUTION_SERVICE, new UiNotifier(), new ModelNotifier())
+{
+	
+}
+
+Application::~Application()
+{
+
+}
+
+// ##################################################################################################################################################################################################################
+
+// Custom functions
+
+
+
+// ##################################################################################################################################################################################################################
+
+// Required functions
+
+void Application::run(void)
+{
+	// Add code that should be executed when the service is started and may start its work
+}
+
+std::string Application::processAction(const std::string & _action, OT_rJSON_doc & _doc)
+{
+	try
+	{
+		std::string returnMessage = "";
+		if (_action == OT_ACTION_CMD_MODEL_ExecuteAction)
+		{
+			rapidjson::Document newDocument;
+			newDocument.AddMember(OT_ACTION_MEMBER, "Test", newDocument.GetAllocator());
+			newDocument.AddMember("TestField", 13, newDocument.GetAllocator());
+			rapidjson::Document subDocument;
+			subDocument.AddMember("TestField", 2, subDocument.GetAllocator());
+			newDocument.AddMember("TestSubDoc", subDocument, newDocument.GetAllocator());
+
+			sendMessage(true, OT_INFO_SERVICE_TYPE_ImportParameterizedDataService, newDocument);
+		}
+		return returnMessage;
+	}
+	catch (std::runtime_error& e)
+	{
+		std::string errorMessage = "Failed to execute action " + _action + " due to runtime error: " + e.what();
+		m_uiComponent->displayMessage(errorMessage);
+		return errorMessage;
+	}
+}
+
+std::string Application::processMessage(ServiceBase * _sender, const std::string & _message, OT_rJSON_doc & _doc)
+{
+	return ""; // Return empty string if the request does not expect a return
+}
+
+void Application::uiConnected(ot::components::UiComponent * _ui)
+{
+
+}
+
+void Application::uiDisconnected(const ot::components::UiComponent * _ui)
+{
+
+}
+
+void Application::uiPluginConnected(ot::components::UiPluginComponent * _uiPlugin) {
+
+}
+
+void Application::modelConnected(ot::components::ModelComponent * _model)
+{
+
+}
+
+void Application::modelDisconnected(const ot::components::ModelComponent * _model)
+{
+
+}
+
+void Application::serviceConnected(ot::ServiceBase * _service)
+{
+
+}
+
+void Application::serviceDisconnected(const ot::ServiceBase * _service)
+{
+
+}
+
+void Application::preShutdown(void) {
+
+}
+
+void Application::shuttingDown(void)
+{
+
+}
+
+bool Application::startAsRelayService(void) const
+{
+	return false;	// Do not want the service to start a relay service. Otherwise change to true
+}
+
+ot::SettingsData * Application::createSettings(void) {
+	return nullptr;
+}
+
+void Application::settingsSynchronized(ot::SettingsData * _dataset) {
+
+}
+
+bool Application::settingChanged(ot::AbstractSettingsItem * _item) {
+	return false;
+}
+
+// ##################################################################################################################################################################################################################
