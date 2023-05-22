26c909a1dcc26b56f37cb6e87f4f8e5f753da468
Jan Wagner <janwagner23@web.de>
2023-05-22 21:23:53 +0200

    Linked python lib and added classes from PythonAPI lib

---
+++ Services/PythonExecutionService/include/CPythonObjectNew.h
@@ -0,0 +1,28 @@
+/*****************************************************************//**
+ * \file   CPythonObjectNew.h
+ * \brief	Wrapper around a PyObject* that is a new reference.
+ *			This owns the reference so does not increment it on construction but 
+ *			does decrement it on destruction.
+ * 
+ * \author Wagner
+ * \date   May 2023
+ *********************************************************************/
+
+
+
+
+
+#pragma once
+#include "DecRefDecorator.h"
+
+class CPythonObjectNew : public DecRefDecorator
+{	
+public:
+	CPythonObjectNew(PyObject* newRef) : DecRefDecorator(newRef) {}
+    void reset(PyObject* ref)
+    {
+        Py_XDECREF(_ref);
+        _ref = nullptr;
+        _ref = ref;
+    }
+};
