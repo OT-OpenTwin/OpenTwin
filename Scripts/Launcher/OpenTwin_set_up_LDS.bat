e214f33b2dda3ea61bbdffc75612b82134f2bc24
alexk95 <57009007+alexk95@users.noreply.github.com>
2023-07-07 13:23:45 +0200

    Update OpenTwin_set_up_LDS.bat

--- Scripts/Launcher/OpenTwin_set_up_LDS.bat
+++ Scripts/Launcher/OpenTwin_set_up_LDS.bat
@@ -1,24 +1,22 @@
 @ECHO OFF
 
-REM This script requires the following environment variables to be set:
-REM 1. OPENTWIN_DEV_ROOT
-IF "%OPENTWIN_DEV_ROOT%" == "" (
-	ECHO Please specify the following environment variables: OPENTWIN_DEV_ROOT
-	goto PAUSE_END
-)
-
 Rem Dont set twice / dont overwrite
 IF DEFINED OT_LOCALDIRECTORYSERVICE_CONFIGURATION (
 	goto END
 )
 
-SET OT_BATCH_TMP=%OPENTWIN_DEV_ROOT%
+IF "%OPENTWIN_DEV_ROOT%" == "" (
+	SET OT_BATCH_TMP=%cd%
+)
+ELSE (
+	SET OT_BATCH_TMP=%OPENTWIN_DEV_ROOT%\Deployment
+)
 SET OT_BATCH_TMP=%OT_BATCH_TMP:\=\\%
 
 SET OT_LOCALDIRECTORYSERVICE_CONFIGURATION={"DefaultMaxCrashRestarts": 8,^
     "DefaultMaxStartupRestarts": 64,^
-    "ServicesLibraryPath": "%OT_BATCH_TMP%\\Deployment",^
-    "LauncherPath": "%OT_BATCH_TMP%\\Deployment\\open_twin.exe",^
+    "ServicesLibraryPath": "%OT_BATCH_TMP%",^
+    "LauncherPath": "%OT_BATCH_TMP%\\open_twin.exe",^
 	"SupportedServices": [^
 		"CartesianMeshService",^
 		"FITTDService",^
