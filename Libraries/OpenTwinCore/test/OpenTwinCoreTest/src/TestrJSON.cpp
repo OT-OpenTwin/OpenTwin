440288c8348dc6f973f01008b84685ab9149c7c6
JanW92 <74253705+JanW92@users.noreply.github.com>
2023-07-28 15:09:33 +0200

    Added functionality for return string containing a return code and a message in rJSON format.

---
+++ Libraries/OpenTwinCore/test/OpenTwinCoreTest/src/TestrJSON.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include "gtest/gtest.h"
+#include "OpenTwinCore/rJSON.h"
+#include "OpenTwinCore/ReturnMessage.h"
+#include <string>
+
+
+TEST(rJSONTests, ReturnMessageToJSON) 
+{
+	ot::ReturnMessage returnMessage("OK", "This was ok");
+	auto doc =	ot::rJSON::fromJSON(returnMessage);
+	EXPECT_TRUE(ot::rJSON::memberExists(doc, "Status"));
+	EXPECT_TRUE(ot::rJSON::memberExists(doc, "What"));
+}
+
+TEST(rJSONTests, JSONToReturnMessage)
+{
+	ot::ReturnMessage expected("OK", "This was ok");
+	auto doc = ot::rJSON::fromJSON(expected);
+	ot::ReturnMessage result(doc);
+	EXPECT_EQ(expected.getStatus(), result.getStatus());
+	EXPECT_EQ(expected.getWhat(), result.getWhat());
+	EXPECT_EQ(static_cast<std::string>(expected), static_cast<std::string>(result));
+}
--- Libraries/OpenTwinCore/test/OpenTwinCoreTest/src/test.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-#include "gtest/gtest.h"
-
-TEST(OpenTwinCommunication, TestName) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
-}
