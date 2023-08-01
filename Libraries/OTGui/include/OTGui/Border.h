87f27e3bf995ab313e39cac1e1aecf2c51e40f83
Alexander Küster <57009007+alexk95@users.noreply.github.com>
2023-08-01 18:15:25 +0200

    Minor adjustments to border class

--- Libraries/OTGui/include/OTGui/Border.h
+++ Libraries/OTGui/include/OTGui/Border.h
@@ -16,6 +16,7 @@ namespace ot {
 	public:
 		Border();
 		Border(const ot::Color& _color, int _width);
+		Border(const ot::Color& _color, int _topWidth, int _leftWidth, int _rightWidth, int _bottomWidth);
 		Border(const Border& _other);
 		virtual ~Border();
 
@@ -29,12 +30,26 @@ namespace ot {
 		void setColor(const ot::Color& _color) { m_color = _color; };
 		const ot::Color& color(void) const { return m_color; };
 
-		void setWidth(int _width) { m_width = _width; };
-		int width(void) const { return m_width; };
+		void setWidth(int _width);
+
+		inline void setTop(int _w) { m_top = _w; };
+		inline int top(void) const { return m_top; };
+
+		inline void setLeft(int _w) { m_left = _w; };
+		inline int left(void) const { return m_left; };
+
+		inline void setRight(int _w) { m_right = _w; };
+		inline int right(void) const { return m_right; };
+
+		inline void setBottom(int _w) { m_bottom = _w; };
+		inline int bottom(void) const { return m_bottom; };
 
 	private:
 		ot::Color m_color;
-		int m_width;
+		int m_top;
+		int m_right;
+		int m_left;
+		int m_bottom;
 	};
 
 }
--- Libraries/OTGui/src/Border.cpp
+++ Libraries/OTGui/src/Border.cpp
@@ -8,22 +8,27 @@
 #include "OpenTwinCore/rJSONHelper.h"
 #include "OpenTwinCore/otAssert.h"
 
-ot::Border::Border() : m_color(0, 0, 0), m_width(1) {}
+ot::Border::Border() : m_color(0, 0, 0), m_top(0), m_left(0), m_right(0), m_bottom(0) {}
 
-ot::Border::Border(const ot::Color& _color, int _width) : m_color(_color), m_width(_width) {}
+ot::Border::Border(const ot::Color& _color, int _width) : m_color(_color), m_top(_width), m_left(_width), m_right(_width), m_bottom(_width) {}
 
-ot::Border::Border(const Border& _other) : m_color(_other.m_color), m_width(_other.m_width) {}
+ot::Border::Border(const ot::Color& _color, int _topWidth, int _leftWidth, int _rightWidth, int _bottomWidth) : m_color(_color), m_top(_topWidth), m_left(_leftWidth), m_right(_rightWidth), m_bottom(_bottomWidth) {}
+
+ot::Border::Border(const Border& _other) : m_color(_other.m_color), m_top(_other.m_top), m_left(_other.m_left), m_right(_other.m_right), m_bottom(_other.m_bottom) {}
 
 ot::Border::~Border() {}
 
 ot::Border& ot::Border::operator = (const Border& _other) {
 	m_color = _other.m_color;
-	m_width = _other.m_width;
+	m_top = _other.m_top;
+	m_left = _other.m_left;
+	m_right = _other.m_right;
+	m_bottom = _other.m_bottom;
 	return *this;
 }
 
 bool ot::Border::operator == (const Border& _other) const {
-	return m_color == _other.m_color && m_width == _other.m_width;
+	return m_color == _other.m_color && m_top == _other.m_top && m_left == _other.m_left && m_right == _other.m_right && m_bottom == _other.m_bottom;
 }
 
 bool ot::Border::operator != (const Border& _other) const {
@@ -34,13 +39,31 @@ void ot::Border::addToJsonObject(OT_rJSON_doc& _document, OT_rJSON_val& _jsonObj
 	OT_rJSON_createValueObject(colorObj);
 	m_color.addToJsonObject(_document, colorObj);
 	ot::rJSON::add(_document, _jsonObject, "Color", colorObj);
-	ot::rJSON::add(_document, _jsonObject, "Width", m_width);
+	ot::rJSON::add(_document, _jsonObject, "Top", m_top);
+	ot::rJSON::add(_document, _jsonObject, "Left", m_left);
+	ot::rJSON::add(_document, _jsonObject, "Right", m_right);
+	ot::rJSON::add(_document, _jsonObject, "Bottom", m_bottom);
 }
 
 void ot::Border::setFromJsonObject(OT_rJSON_val& _jsonObject) {
 	OT_rJSON_checkMember(_jsonObject, "Color", Object);
-	OT_rJSON_checkMember(_jsonObject, "Width", Int);
+	OT_rJSON_checkMember(_jsonObject, "Top", Int);
+	OT_rJSON_checkMember(_jsonObject, "Left", Int);
+	OT_rJSON_checkMember(_jsonObject, "Right", Int);
+	OT_rJSON_checkMember(_jsonObject, "Bottom", Int);
+	
 	OT_rJSON_val colorObj = _jsonObject["Color"].GetObject();
 	m_color.setFromJsonObject(colorObj);
-	m_width = _jsonObject["Width"].GetInt();
+	
+	m_top = _jsonObject["Top"].GetInt();
+	m_left = _jsonObject["Left"].GetInt();
+	m_right = _jsonObject["Right"].GetInt();
+	m_bottom = _jsonObject["Bottom"].GetInt();
+}
+
+void ot::Border::setWidth(int _width) {
+	m_top = _width;
+	m_left = _width;
+	m_right = _width;
+	m_bottom = _width;
 }
