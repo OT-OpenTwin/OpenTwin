9d187b139785da61eed710812cf82926f4093830
Alexander Küster <57009007+alexk95@users.noreply.github.com>
2023-08-17 13:38:46 +0200

    Added icon manager class

---
+++ Libraries/OTWidgets/include/OTWidgets/IconManager.h
@@ -0,0 +1,86 @@
+//! @file IconManager.h
+//! @author Alexander Kuester (alexk95)
+//! @date August 2023
+// ###########################################################################################################################################################################################################################################################################################################################
+
+#pragma once
+
+// OpenTwin header
+#include "OTWidgets/OTWidgetsAPIExport.h"
+#include "OpenTwinCore/Logger.h"
+
+// Qt header
+#include <qstring.h>
+#include <qstringlist.h>
+
+// std header
+#include <map>
+#include <mutex>
+
+class QIcon;
+class QPixmap;
+class QMovie;
+
+namespace ot {
+
+	class OT_WIDGETS_API_EXPORT IconManager {
+	public:
+		static IconManager& instance(void);
+
+		void addSearchPath(const QString& _path);
+		const QStringList& searchPaths(void) const { return m_searchPaths; };
+
+		//! @brief Create and return the icon (*.png, *.jpg)
+		QIcon& getIcon(const QString& _subPath);
+
+		//! @brief Create and return the pixmap (*.png, *.jpg)
+		QPixmap& getPixmap(const QString& _subPath);
+
+		//! @brief Create and return the movie (*.gif)
+		QMovie& getMovie(const QString& _subPath);
+
+	private:
+		template <class T>
+		T& get(const QString& _subPath, const QStringList& _extensions, std::map<QString, T*>& _dataMap);
+
+		QString findFullPath(const QString& _subPath, const QStringList& _extensions);
+
+		IconManager();
+		virtual ~IconManager();
+
+		QStringList m_searchPaths;
+		std::mutex m_mutex;
+		std::map<QString, QIcon*> m_icons;
+		std::map<QString, QPixmap*> m_pixmaps;
+		std::map<QString, QMovie*> m_movies;
+
+	};
+
+}
+
+template <class T>
+T& ot::IconManager::get(const QString& _subPath, const QStringList& _extensions, std::map<QString, T*>& _dataMap) {
+	this->m_mutex.lock();
+
+	// Find existing
+	auto it = _dataMap.find(_subPath);
+	if (it != _dataMap.end()) {
+		this->m_mutex.unlock();
+		return *it->second;
+	}
+
+	// Find new
+	QString path = this->findFullPath(_subPath, _extensions);
+	if (path.isEmpty()) {
+		this->m_mutex.unlock();
+		OT_LOG_EAS("Icon \"" + _subPath.toStdString() + "\" not found");
+		throw std::exception("File not found");
+	}
+
+	// Create and store new
+	T* newEntry = new T(path);
+	_dataMap.insert_or_assign(_subPath, newEntry);
+
+	this->m_mutex.unlock();
+	return *newEntry;
+}
---
+++ Libraries/OTWidgets/src/IconManager.cpp
@@ -0,0 +1,55 @@
+//! @file IconManager.h
+//! @author Alexander Kuester (alexk95)
+//! @date August 2023
+// ###########################################################################################################################################################################################################################################################################################################################
+
+// OpenTwin header
+#include "OTWidgets/IconManager.h"
+#include "OpenTwinCore/Logger.h"
+
+// Qt header
+#include <QtCore/qfile.h>
+#include <QtGui/qicon.h>
+#include <QtGui/qpixmap.h>
+#include <QtGui/qmovie.h>
+
+ot::IconManager& ot::IconManager::instance(void) {
+	static ot::IconManager g_instance;
+	return g_instance;
+}
+
+void ot::IconManager::addSearchPath(const QString& _path) {
+	m_searchPaths.append(_path);
+}
+
+QIcon& ot::IconManager::getIcon(const QString& _subPath) {
+	return this->get<QIcon>(_subPath, { ".png", ".jpg" }, m_icons);
+}
+
+QPixmap& ot::IconManager::getPixmap(const QString& _subPath) {
+	return this->get<QPixmap>(_subPath, { ".png", ".jpg" }, m_pixmaps);
+}
+
+QMovie& ot::IconManager::getMovie(const QString& _subPath) {
+	return this->get<QMovie>(_subPath, { ".gif" }, m_movies);
+}
+
+QString ot::IconManager::findFullPath(const QString& _subPath, const QStringList& _extensions) {
+	for (auto p : m_searchPaths) {
+		for (auto ext : _extensions) {
+			if (QFile::exists(p + _subPath + ext)) return p + _subPath + ext;
+		}
+		
+	}
+	return QString();
+}
+
+ot::IconManager::IconManager() {
+
+}
+
+ot::IconManager::~IconManager() {
+	for (auto i : m_icons) delete i.second;
+	for (auto i : m_pixmaps) delete i.second;
+	for (auto i : m_movies) delete i.second;
+}
