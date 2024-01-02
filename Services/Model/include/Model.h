d77f297d07372e29d21fbf38479fbd4266dda7a9
JanW92 <74253705+JanW92@users.noreply.github.com>
2024-01-02 10:19:17 +0100

    Finalised renaming of metadata objects according to data model changes.

--- Services/Model/include/Model.h
+++ Services/Model/include/Model.h
@@ -88,8 +88,8 @@ public:
 	std::string getUnitRootName(void) { return "Units"; };
 	std::string getDataCategorizationRootName(void) { return "Data Categorization"; };
 	std::string getDatasetRootName() { return "Dataset"; };
-	std::string getRMDCategorizationName() { return getDataCategorizationRootName()+"/Research Metadata"; };
-	std::string getDatasetRMD() { return getDatasetRootName() + "/RMD"; }
+	std::string getRMDCategorizationName() { return getDataCategorizationRootName()+"/Campaign Metadata"; };
+	std::string getDatasetRMD() { return getDatasetRootName() + "/Campaign Metadata"; }
 
 	void addVisualizationNodeFromFacetData(const std::string &treeName, double surfaceColorRGB[3], double edgeColorRGB[3], ot::UID modelEntityID, const TreeIcon &treeIcons, bool backFaceCulling,
 										   double offsetFactor, bool isEditable, std::vector<Geometry::Node> &nodes, std::list<Geometry::Triangle> &triangles, std::list<Geometry::Edge> &edges, std::string &errors, bool selectChildren, bool manageParentVisibility, bool manageChildVisibility, bool showWhenSelected);
