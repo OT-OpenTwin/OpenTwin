dbec205ed68cf66bb8af80d1ff2fb169b6c7fd92
JanW92 <74253705+JanW92@users.noreply.github.com>
2024-01-12 09:10:57 +0100

    Added test for a real live 4-port example.

--- Services/ImportParameterizedData/test/ImportParameterizedDataTest/include/FixtureSParameterHandler.h
+++ Services/ImportParameterizedData/test/ImportParameterizedDataTest/include/FixtureSParameterHandler.h
@@ -7,7 +7,7 @@ class FixtureSParameterHandler: public testing::Test
 {
 public:
 		
-	const sp::OptionSettings& GetOptionSettings(const std::string& text) 
+	const sp::OptionSettings& AnalyseOptionSettings(const std::string& text) 
 	{ 
 		_handler.AnalyseLine(text);
 		return _handler.getOptionSettings(); 
@@ -42,6 +42,30 @@ public:
 	{
 		return _handler.getPortData();
 	}
+
+	const sp::OptionSettings& GetOptionSettings()
+	{
+		return _handler.getOptionSettings();
+	}
+
+	const std::string& GetFullExampleFourPorts() const { return _exampleFourPorts; };
 private:
 	SParameterHandler _handler;
+
+	const std::string _exampleFourPorts =
+		"!4 - port S - parameter data, taken at three frequency points\n"
+		"!note that data points need not be aligned\n"
+		"# Hz Y RI R 90\n"
+		"5.00000 0.60 161.24 0.40 -42.20 0.42 -66.58 0.53 -79.34 !row 1\n"
+		"0.40 -42.20 0.60 161.20 0.53 -79.34 0.42 -66.58 !row 2\n"
+		"0.42 -66.58 0.53 -79.34 0.60 161.24 0.40 -42.20 !row 3\n"
+		"0.53 -79.34 0.42 -66.58 0.40 -42.20 0.60 161.24 !row 4\n"
+		"6.00000 0.57 150.37 0.40 -44.34 0.41 -81.24 0.57 -95.77 !row 1\n"
+		"0.40 -44.34 0.57 150.37 0.57 -95.77 0.41 -81.24 !row 2\n"
+		"0.41 -81.24 0.57 -95.77 0.57 150.37 0.40 -44.34 !row 3\n"
+		"0.57 -95.77 0.41 -81.24 0.40 -44.34 0.57 150.37 !row 4\n"
+		"7.00000 0.50 136.69 0.45 -46.41 0.37 -99.09 0.62 -114.19 !row 1\n"
+		"0.45 -46.41 0.50 136.69 0.62 -114.19 0.37 -99.09 !row 2\n"
+		"0.37 -99.09 0.62 -114.19 0.50 136.69 0.45 -46.41 !row 3\n"
+		"0.62 -114.19 0.37 -99.09 0.45 -46.41 0.50 136.69 !row 4\n";
 };
--- Services/ImportParameterizedData/test/ImportParameterizedDataTest/src/TestSParameterOptionHandling.cpp
+++ Services/ImportParameterizedData/test/ImportParameterizedDataTest/src/TestSParameterOptionHandling.cpp
@@ -8,7 +8,7 @@ TEST_F(FixtureSParameterHandler, OptionDetectionSuccess)
 	const sp::OptionSettings expectedOptionSettings(sp::option::Frequency::kHz, sp::option::Format::real_imaginary, sp::option::Parameter::Admittance, ot::Variable(100));
 	
 	std::string setting = "# kHz Y RI R 100";
-	const sp::OptionSettings actualSettings	= GetOptionSettings(setting);
+	const sp::OptionSettings actualSettings	= AnalyseOptionSettings(setting);
 	
 
 	EXPECT_TRUE(expectedOptionSettings == actualSettings);
@@ -19,7 +19,7 @@ TEST_F(FixtureSParameterHandler, OptionFrequencyOmitted)
 	const sp::OptionSettings expectedOptionSettings(sp::option::Frequency::GHz, sp::option::Format::real_imaginary, sp::option::Parameter::Admittance, ot::Variable(100));
 
 	std::string setting = "# Y RI R 100";
-	const sp::OptionSettings actualSettings = GetOptionSettings(setting);
+	const sp::OptionSettings actualSettings = AnalyseOptionSettings(setting);
 
 
 	EXPECT_TRUE(expectedOptionSettings == actualSettings);
@@ -30,7 +30,7 @@ TEST_F(FixtureSParameterHandler, OptionFormatOmitted)
 	const sp::OptionSettings expectedOptionSettings(sp::option::Frequency::kHz, sp::option::Format::magnitude_angle, sp::option::Parameter::Admittance, ot::Variable(100));
 
 	std::string setting = "# kHz Y R 100";
-	const sp::OptionSettings actualSettings = GetOptionSettings(setting);
+	const sp::OptionSettings actualSettings = AnalyseOptionSettings(setting);
 
 
 	EXPECT_TRUE(expectedOptionSettings == actualSettings);
@@ -41,7 +41,7 @@ TEST_F(FixtureSParameterHandler, OptionParameterOmitted)
 	const sp::OptionSettings expectedOptionSettings(sp::option::Frequency::kHz, sp::option::Format::real_imaginary, sp::option::Parameter::Scattering, ot::Variable(100));
 
 	std::string setting = "# kHz S RI R 100";
-	const sp::OptionSettings actualSettings = GetOptionSettings(setting);
+	const sp::OptionSettings actualSettings = AnalyseOptionSettings(setting);
 
 
 	EXPECT_TRUE(expectedOptionSettings == actualSettings);
@@ -52,7 +52,7 @@ TEST_F(FixtureSParameterHandler, OptionResistanceOmitted)
 	const sp::OptionSettings expectedOptionSettings(sp::option::Frequency::kHz, sp::option::Format::real_imaginary, sp::option::Parameter::Admittance, ot::Variable(50));
 
 	std::string setting = "# kHz Y RI";
-	const sp::OptionSettings actualSettings = GetOptionSettings(setting);
+	const sp::OptionSettings actualSettings = AnalyseOptionSettings(setting);
 
 
 	EXPECT_TRUE(expectedOptionSettings == actualSettings);
@@ -63,7 +63,7 @@ TEST_F(FixtureSParameterHandler, BlankOptionLine)
 	const sp::OptionSettings expectedOptionSettings;
 
 	std::string setting = "#";
-	const sp::OptionSettings actualSettings = GetOptionSettings(setting);
+	const sp::OptionSettings actualSettings = AnalyseOptionSettings(setting);
 
 
 	EXPECT_TRUE(expectedOptionSettings == actualSettings);
@@ -74,7 +74,7 @@ TEST_F(FixtureSParameterHandler, OptionTwoEntries)
 	const sp::OptionSettings expectedOptionSettings(sp::option::Frequency::kHz, sp::option::Format::real_imaginary, sp::option::Parameter::Scattering, ot::Variable(50));
 
 	std::string setting = "# kHz RI";
-	const sp::OptionSettings actualSettings = GetOptionSettings(setting);
+	const sp::OptionSettings actualSettings = AnalyseOptionSettings(setting);
 
 
 	EXPECT_TRUE(expectedOptionSettings == actualSettings);
@@ -85,7 +85,7 @@ TEST_F(FixtureSParameterHandler, OptionOnlyResistance)
 	const sp::OptionSettings expectedOptionSettings(sp::option::Frequency::GHz, sp::option::Format::magnitude_angle, sp::option::Parameter::Scattering, ot::Variable(80));
 
 	std::string setting = "#R 80";
-	const sp::OptionSettings actualSettings = GetOptionSettings(setting);
+	const sp::OptionSettings actualSettings = AnalyseOptionSettings(setting);
 
 
 	EXPECT_TRUE(expectedOptionSettings == actualSettings);
@@ -96,7 +96,7 @@ TEST_F(FixtureSParameterHandler, OptionUnevenNumberOfWhitespaces)
 	const sp::OptionSettings expectedOptionSettings(sp::option::Frequency::kHz, sp::option::Format::real_imaginary, sp::option::Parameter::Scattering, ot::Variable(50));
 
 	std::string setting = "#          kHz      RI";
-	const sp::OptionSettings actualSettings = GetOptionSettings(setting);
+	const sp::OptionSettings actualSettings = AnalyseOptionSettings(setting);
 
 
 	EXPECT_TRUE(expectedOptionSettings == actualSettings);
@@ -107,7 +107,7 @@ TEST_F(FixtureSParameterHandler, OptionChangedOrder)
 	const sp::OptionSettings expectedOptionSettings(sp::option::Frequency::kHz, sp::option::Format::real_imaginary, sp::option::Parameter::Scattering, ot::Variable(50));
 
 	std::string setting = "# RI kHz";
-	const sp::OptionSettings actualSettings = GetOptionSettings(setting);
+	const sp::OptionSettings actualSettings = AnalyseOptionSettings(setting);
 
 
 	EXPECT_TRUE(expectedOptionSettings == actualSettings);
@@ -122,7 +122,7 @@ TEST_F(FixtureSParameterHandler, OptionDetectedAfterComments)
 		"!And a second\n"
 		"#";
 	AnalyseFile(setting);
-	const sp::OptionSettings actualSettings = GetOptionSettings(setting);
+	const sp::OptionSettings actualSettings = AnalyseOptionSettings(setting);
 
 	EXPECT_TRUE(expectedOptionSettings == actualSettings);
 }
@@ -158,4 +158,89 @@ TEST_F(FixtureSParameterHandler, V1_Example1Port)
 	AnalyseLine(line);
 	auto& portData =	GetPortData();
 	EXPECT_EQ(portData.size(), 1);
+}
+
+TEST_F(FixtureSParameterHandler, V1_Example4Ports_Comments)
+{
+	const int numberOfPorts = 4;
+	const std::string expectedComments =	"4 - port S - parameter data, taken at three frequency points\n"
+											"note that data points need not be aligned\n";
+	
+	const std::string& file = GetFullExampleFourPorts();
+	SetNumberOfPorts(numberOfPorts);
+	AnalyseFile(file);
+	
+	const std::string& actualComments = GetComments();
+	
+	EXPECT_EQ(expectedComments, actualComments);
+}
+
+TEST_F(FixtureSParameterHandler, V1_Example4Ports_Options)
+{
+	const int numberOfPorts = 4;
+
+	const sp::OptionSettings expectedOptionSettings(sp::option::Frequency::Hz, sp::option::Format::real_imaginary, sp::option::Parameter::Admittance, ot::Variable(90));
+
+	const std::string& file = GetFullExampleFourPorts();
+	SetNumberOfPorts(numberOfPorts);
+	AnalyseFile(file);
+
+	const sp::OptionSettings& actualOptionSettings = GetOptionSettings();
+
+	EXPECT_EQ(expectedOptionSettings, actualOptionSettings);
+}
+
+TEST_F(FixtureSParameterHandler, V1_Example4Ports_Data_Sizes)
+{
+	const int numberOfPorts = 4;
+	
+	const size_t expectedNumberOfEntries = 3;
+	const size_t _expectedNumberOfEntriesPerPortData = numberOfPorts * numberOfPorts;
+
+	const std::string& file = GetFullExampleFourPorts();
+	SetNumberOfPorts(numberOfPorts);
+	AnalyseFile(file);
+
+	const auto& actualPortData = GetPortData();
+
+	EXPECT_EQ(actualPortData.size(), expectedNumberOfEntries);
+	int counter(0);
+	for (const sp::PortData& portData : actualPortData)
+	{
+		counter++;
+		const auto& actualPortDataEntries = actualPortData.back().getPortDataEntries();
+		EXPECT_EQ(actualPortDataEntries.size(), _expectedNumberOfEntriesPerPortData);
+	}
+}
+
+TEST_F(FixtureSParameterHandler, V1_Example4Ports_Data_Values)
+{
+	const int numberOfPorts = 4;
+
+	const std::vector<float> expectedFrequencyValues{ 5.00000f,6.00000f,7.00000f };
+	const sp::PortDataEntry lastDataEntry = { sp::PortDataEntry(sp::PortDataSingleEntry {0.50f},sp::PortDataSingleEntry {136.69f}) };
+
+	const std::string& file = GetFullExampleFourPorts();
+	SetNumberOfPorts(numberOfPorts);
+	AnalyseFile(file);
+
+	const auto& actualPortData = GetPortData();
+
+	int counter(0);
+	for (const sp::PortData& portData : actualPortData)
+	{
+		bool typeFound = std::holds_alternative <float>(portData.getFrequency());
+		EXPECT_TRUE(typeFound);
+		float actualValue =	std::get<float>(portData.getFrequency());
+
+		EXPECT_FLOAT_EQ(actualValue, expectedFrequencyValues[counter]);
+		counter++;
+	
+		if (counter == 2)
+		{
+			const auto& actualPortDataEntries = actualPortData.back().getPortDataEntries();
+			EXPECT_EQ(std::get<float>(std::get<0>(actualPortDataEntries.back())), std::get<float>(std::get<0>(lastDataEntry)));
+			EXPECT_EQ(std::get<float>(std::get<1>(actualPortDataEntries.back())), std::get<float>(std::get<1>(lastDataEntry)));
+		}
+	}
 }
