7b9dea42b8fd99b91ab8d9be29cf7ca9fd1a9d53
Peter Thoma <pth68@users.noreply.github.com>
2024-03-04 16:21:09 +0100

    updated clean all shell for missing modules.

--- Scripts/BuildAndTest/CleanAll.bat
+++ Scripts/BuildAndTest/CleanAll.bat
@@ -31,128 +31,236 @@ IF NOT "%OPENTWIN_DEV_ENV_DEFINED%" == "1" (
 REM ################################################################################################################################
 REM LIBRARIES
 
-ECHO Clean: CADModelEntities
-CALL "%OPENTWIN_DEV_ROOT%\Libraries\CADModelEntities\clean.bat"
+ECHO ===============================================================
+ECHO Clean Library: OpenTwinSystem
+ECHO ===============================================================
+CALL "%OT_SYSTEM_ROOT%\Clean.bat" 
 
-ECHO Clean: Data Storage Library
-CALL "%OPENTWIN_DEV_ROOT%\Libraries\DataStorage\clean.bat"
+ECHO ===============================================================
+ECHO Clean Library: OpenTwinCore
+ECHO ===============================================================
+CALL "%OT_CORE_ROOT%\Clean.bat" 
 
-ECHO Clean: OTGui Library
-CALL "%OPENTWIN_DEV_ROOT%\Libraries\OTGui\clean.bat"
+ECHO ===============================================================
+ECHO Clean Library: OTGui
+ECHO ===============================================================
+CALL "%OT_GUI_ROOT%\Clean.bat" 
 
-ECHO Clean: OTWidgets Library
-CALL "%OPENTWIN_DEV_ROOT%\Libraries\OTWidgets\clean.bat"
+ECHO ===============================================================
+ECHO Clean Library: OTWidgets
+ECHO ===============================================================
+CALL "%OT_WIDGETS_ROOT%\Clean.bat" 
 
-ECHO Clean: Model Entities Library
-CALL "%OPENTWIN_DEV_ROOT%\Libraries\ModelEntities\clean.bat"
+ECHO ===============================================================
+ECHO Clean Library: OpenTwinCommunications
+ECHO ===============================================================
+CALL "%OT_COMMUNICATION_ROOT%\Clean.bat" 
 
-ECHO Clean: OpenTwin Communication Library
-CALL "%OPENTWIN_DEV_ROOT%\Libraries\OTCommunication\clean.bat"
+ECHO ===============================================================
+ECHO Clean Library: DataStorage
+ECHO ===============================================================
+CALL "%OT_DATASTORAGE_ROOT%\Clean.bat" 
 
-ECHO Clean: OpenTwin Service Foundation Library
-CALL "%OPENTWIN_DEV_ROOT%\Libraries\OTServiceFoundation\clean.bat"
+ECHO ===============================================================
+ECHO Clean Library: ModelEntities
+ECHO ===============================================================
+CALL "%OT_MODELENTITIES_ROOT%\Clean.bat" 
 
-ECHO Clean: OpenTwin Core Library
-CALL "%OPENTWIN_DEV_ROOT%\Libraries\OTCore\clean.bat"
+ECHO ===============================================================
+ECHO Clean Library: CADModelEntities
+ECHO ===============================================================
+CALL "%OT_CADMODELENTITIES_ROOT%\Clean.bat" 
 
-ECHO Clean: OpenTwin System Library
-CALL "%OPENTWIN_DEV_ROOT%\Libraries\OTSystem\clean.bat"
+ECHO ===============================================================
+ECHO Clean Library: BlockEntities
+ECHO ===============================================================
+CALL "%OT_BLOCKENTITIES_ROOT%\Clean.bat" 
 
-ECHO Clean: Qwt Wrapper
-CALL "%OPENTWIN_DEV_ROOT%\Libraries\QwtWrapper\clean.bat"
+ECHO ===============================================================
+ECHO Clean Library: ServiceFoundation
+ECHO ===============================================================
+CALL "%OT_FOUNDATION_ROOT%\Clean.bat" 
 
-ECHO Clean: Rubberband Engine Library: Core
-CALL "%OPENTWIN_DEV_ROOT%\Libraries\RubberbandEngineCore\clean.bat"
+ECHO ===============================================================
+ECHO Clean Library: StudioSuiteConnector
+ECHO ===============================================================
+CALL "%OT_STUDIO_SUITE_CONNECTOR_ROOT%\Clean.bat" 
 
-ECHO Clean: Rubberband Engine Library: OSG Wrapper
-CALL "%OPENTWIN_DEV_ROOT%\Libraries\RubberbandEngineOsgWrapper\clean.bat"
+ECHO ===============================================================
+ECHO Clean Library: uiCore
+ECHO ===============================================================
+CALL "%OT_UICORE_ROOT%\Clean.bat" 
 
-ECHO Clean: uiCore
-CALL "%OPENTWIN_DEV_ROOT%\Libraries\uiCore\clean.bat"
+ECHO ===============================================================
+ECHO Clean Library: UI Plugin API
+ECHO ===============================================================
+CALL "%OT_UIPLUGINAPI_ROOT%\Clean.bat" 
 
-ECHO Clean: UIPluginAPI Library
-CALL "%OPENTWIN_DEV_ROOT%\Libraries\UIPluginAPI\clean.bat"
+ECHO ===============================================================
+ECHO Clean Library: RubberbandEngine: Core
+ECHO ===============================================================
+CALL "%OT_RUBBERBANDAPI_ROOT%\Clean.bat" 
 
-ECHO Clean: Viewer Library
-CALL "%OPENTWIN_DEV_ROOT%\Libraries\Viewer\clean.bat"
+ECHO ===============================================================
+ECHO Clean Library: RubberbandEngine: osg Wrapper
+ECHO ===============================================================
+CALL "%OT_RUBBERBAND_ROOT%\Clean.bat" 
+
+ECHO ===============================================================
+ECHO Clean Library: QWT Wrapper
+ECHO ===============================================================
+CALL "%OT_QWTWRAPPER_ROOT%\Clean.bat" 
+
+ECHO ===============================================================
+ECHO Clean Library: Viewer
+ECHO ===============================================================
+CALL "%OT_VIEWER_ROOT%\Clean.bat" 
+
+ECHO ===============================================================
+ECHO Clean Library: Result Data Access
+ECHO ===============================================================
+CALL "%OT_RESULT_DATA_ACCESS_ROOT%\Clean.bat" 
 
 REM ################################################################################################################################
 REM SERVICES
 
-ECHO Clean: Authorisation Service
-CALL "%OPENTWIN_DEV_ROOT%\Services\AuthorisationService\clean.bat"
+ECHO ===============================================================
+ECHO Clean Service: Model
+ECHO ===============================================================
+CALL "%OT_MODEL_SERVICE_ROOT%\Clean.bat" 
 
-ECHO Clean: Authorisation Service
-CALL "%OPENTWIN_DEV_ROOT%\Services\BlockEditorService\clean.bat"
+ECHO ===============================================================
+ECHO Clean Service: GlobalSessionService
+ECHO ===============================================================
+CALL "%OT_GLOBAL_SESSION_SERVICE_ROOT%\Clean.bat" 
 
-ECHO Clean: Cartesian Mesh Service
-CALL "%OPENTWIN_DEV_ROOT%\Services\CartesianMeshService\clean.bat"
+ECHO ===============================================================
+ECHO Clean Service: LocalSessionService
+ECHO ===============================================================
+CALL "%OT_LOCAL_SESSION_SERVICE_ROOT%\Clean.bat" 
 
-ECHO Clean: Circuit Simulator Service
-CALL "%OPENTWIN_DEV_ROOT%\Services\CircuitSimulatorService\clean.bat"
+ECHO ===============================================================
+ECHO Clean Service: GlobalDirectoryService
+ECHO ===============================================================
+CALL "%OT_GLOBAL_DIRECTORY_SERVICE_ROOT%\Clean.bat" 
 
-ECHO Clean: FITTD Service
-CALL "%OPENTWIN_DEV_ROOT%\Services\FITTDService\clean.bat"
+ECHO ===============================================================
+ECHO Clean Service: LocalDirectoryService
+ECHO ===============================================================
+CALL "%OT_LOCAL_DIRECTORY_SERVICE_ROOT%\Clean.bat" 
 
-ECHO Clean: GetDP Service
-CALL "%OPENTWIN_DEV_ROOT%\Services\GetDPService\clean.bat"
+ECHO ===============================================================
+ECHO Clean Service: RelayService
+ECHO ===============================================================
+CALL "%OT_RELAY_SERVICE_ROOT%\Clean.bat" 
 
-ECHO Clean: ElmerFEM Service
-CALL "%OPENTWIN_DEV_ROOT%\Services\ElmerFEMService\clean.bat"
+ECHO ===============================================================
+ECHO Clean Service: LoggerService
+ECHO ===============================================================
+CALL "%OT_LOGGER_SERVICE_ROOT%\Clean.bat" 
 
-ECHO Clean: StudioSuite Service
-CALL "%OPENTWIN_DEV_ROOT%\Services\StudioSuiteService\clean.bat"
+ECHO ===============================================================
+ECHO Clean Service: AuthorisationService
+ECHO ===============================================================
+CALL "%OT_AUTHORISATION_SERVICE_ROOT%\Clean.bat" 
 
-ECHO Clean: Global Directory Service
-CALL "%OPENTWIN_DEV_ROOT%\Services\GlobalDirectoryService\clean.bat"
+ECHO ===============================================================
+ECHO Clean Service: PHREECService
+ECHO ===============================================================
+CALL "%OT_PHREEC_SERVICE_ROOT%\Clean.bat" 
 
-ECHO Clean: Global Session Service
-CALL "%OPENTWIN_DEV_ROOT%\Services\GlobalSessionService\clean.bat"
+REM ECHO ===============================================================
+REM ECHO Clean Service: KrigingService
+REM ECHO ===============================================================
+REM CALL "%OT_KRIGING_SERVICE_ROOT%\Clean.bat" 
 
-ECHO Clean: Import Parameterized Data Service
-CALL "%OPENTWIN_DEV_ROOT%\Services\ImportParameterizedData\clean.bat"
+ECHO ===============================================================
+ECHO Clean Service: ModelingService
+ECHO ===============================================================
+CALL "%OT_MODELING_SERVICE_ROOT%\Clean.bat" 
 
-ECHO Clean: Kriging Service
-CALL "%OPENTWIN_DEV_ROOT%\Services\KrigingService\clean.bat"
+ECHO ===============================================================
+ECHO Clean Service: VisualizationService
+ECHO ===============================================================
+CALL "%OT_VISUALIZATION_SERVICE_ROOT%\Clean.bat" 
 
-ECHO Clean: Local Directory Service
-CALL "%OPENTWIN_DEV_ROOT%\Services\LocalDirectoryService\clean.bat"
+ECHO ===============================================================
+ECHO Clean Service: FITTDService
+ECHO ===============================================================
+CALL "%OT_FITTD_SERVICE_ROOT%\Clean.bat" 
 
-ECHO Clean: Logger Service
-CALL "%OPENTWIN_DEV_ROOT%\Services\LoggerService\clean.bat"
+ECHO ===============================================================
+ECHO Clean Service: CartesianMeshService
+ECHO ===============================================================
+CALL  "%OT_CARTESIAN_MESH_SERVICE_ROOT%\Clean.bat" 
 
-ECHO Clean: Model Service
-CALL "%OPENTWIN_DEV_ROOT%\Services\Model\clean.bat"
+ECHO ===============================================================
+ECHO Clean Service: TetMeshService
+ECHO ===============================================================
+CALL  "%OT_TET_MESH_SERVICE_ROOT%\Clean.bat" 
 
-ECHO Clean: Modeling Service
-CALL "%OPENTWIN_DEV_ROOT%\Services\ModelingService\clean.bat"
+ECHO ===============================================================
+ECHO Clean Service: ImportParameterizedData
+ECHO ===============================================================
+CALL "%OT_IMPORT_PARAMETERIZED_DATA_SERVICE_ROOT%\Clean.bat" 
 
-ECHO Clean: PHREEC Service
-CALL "%OPENTWIN_DEV_ROOT%\Services\PHREECService\clean.bat"
+ECHO ===============================================================
+ECHO Clean Service: GetDPService
+ECHO ===============================================================
+CALL "%OT_GETDP_SERVICE_ROOT%\Clean.bat" 
 
-ECHO Clean: Relay Service
-CALL "%OPENTWIN_DEV_ROOT%\Services\RelayService\clean.bat"
+ECHO ===============================================================
+ECHO Clean Service: ElmerFEMService
+ECHO ===============================================================
+CALL "%OT_ELMERFEM_SERVICE_ROOT%\Clean.bat" 
 
-ECHO Clean: Local Session Service
-CALL "%OPENTWIN_DEV_ROOT%\Services\LocalSessionService\clean.bat"
+ECHO ===============================================================
+ECHO Clean Service: StudioSuiteService
+ECHO ===============================================================
+CALL "%OT_STUDIOSUITE_SERVICE_ROOT%\Clean.bat" 
 
-ECHO Clean: Tet Mesh Service
-CALL "%OPENTWIN_DEV_ROOT%\Services\TetMeshService\clean.bat"
+ECHO ===============================================================
+ECHO Clean Service: PythonExecutionService
+ECHO ===============================================================
+CALL "%OT_PYTHON_EXECUTION_SERVICE_ROOT%\Clean.bat" 
 
-ECHO Clean: UI Service
-CALL "%OPENTWIN_DEV_ROOT%\Services\uiService\clean.bat"
+ECHO ===============================================================
+ECHO Clean Service: PythonExecution
+ECHO ===============================================================
+CALL "%OT_PYTHON_EXECUTION_ROOT%\Clean.bat" 
 
-ECHO Clean: Visualization Service
-CALL "%OPENTWIN_DEV_ROOT%\Services\VisualizationService\clean.bat"
+ECHO ===============================================================
+ECHO Clean Service: BlockEditorService
+ECHO ===============================================================
+CALL "%OT_BLOCKEDITORSERVICE_ROOT%\Clean.bat" 
+
+ECHO ===============================================================
+ECHO Clean Service: DataProcessingService
+ECHO ===============================================================
+CALL "%OT_DATA_PROCESSING_SERVICE_ROOT%\Clean.bat" 
+
+ECHO ===============================================================
+ECHO Clean Service: CircuitSimulatorService
+ECHO ===============================================================
+CALL "%OT_CIRCUIT_SIMULATOR_SERVICE_ROOT%\Clean.bat" 
 
 REM ################################################################################################################################
-REM TOOLS
+REM EXECUTABLES
 
-ECHO Clean: OToolkitAPI
-CALL "%OPENTWIN_DEV_ROOT%\Tools\OToolkitAPI\clean.bat"
+ECHO ===============================================================
+ECHO Clean Executable: uiFrontend
+ECHO ===============================================================
+CALL "%OT_UI_SERVICE_ROOT%\Clean.bat" 
 
-ECHO Clean: OToolkit
-CALL "%OPENTWIN_DEV_ROOT%\Tools\OToolkit\clean.bat"
+ECHO ===============================================================
+ECHO Clean Library: OToolkitAPI
+ECHO ===============================================================
+CALL "%OPENTWIN_DEV_ROOT%\Tools\OToolkitAPI\Clean.bat" 
+
+ECHO ===============================================================
+ECHO Clean Executable: OToolkit
+ECHO ===============================================================
+CALL "%OPENTWIN_DEV_ROOT%\Tools\OToolkit\Clean.bat" 
 
 GOTO END
 
