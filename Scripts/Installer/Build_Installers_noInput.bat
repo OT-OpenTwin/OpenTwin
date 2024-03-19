38f50780a417164206b31c6e74f7fa134423d7b8
javidbavani <bavani1996@yahoo.de>
2024-03-19 02:15:29 +0100

    added unzipping funcionality via 7zip

--- Scripts/Installer/Build_Installers_noInput.bat
+++ Scripts/Installer/Build_Installers_noInput.bat
@@ -14,16 +14,24 @@ IF "%OPENTWIN_THIRDPARTY_ROOT%" == "" (
 	goto EXIT
 )
 
-set KEY=HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\NSIS
-set "REG_VALUE="
+set NSIS_REG_KEY=HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\NSIS
+set SEVENZIP_REG_KEY=HKEY_CURRENT_USER\SOFTWARE\7-Zip
+set SEVENZIP_VALUE=Path
+set "NSIS_REG_VALUE="
 
-for /f "tokens=2,*" %%a in ('reg query "%KEY%" /ve 2^>nul') do (
-    set "REG_VALUE=%%b"
+for /f "tokens=2,*" %%a in ('reg query "%NSIS_REG_KEY%" /ve 2^>nul') do (
+    set "NSIS_REG_VALUE=%%b"
 )
 
-Set MAKENSIS_PATH="!REG_VALUE!\makensis.exe"
+for /f "tokens=2*" %%a in ('reg query "%SEVENZIP_REG_KEY%" /v "%SEVENZIP_VALUE%" 2^>nul') do set SEVENZIP_REG_DATA=%%b
+
+
+Set MAKENSIS_PATH="!NSIS_REG_VALUE!\makensis.exe"
 Set ENDUSER_NSI="!OPENTWIN_DEV_ROOT!\Scripts\Installer\nsis\install_opentwin_endUser.nsi"
 Set DEVELOPER_NSI="!OPENTWIN_DEV_ROOT!\Scripts\Installer\nsis\install_opentwin_Developer.nsi"
+
+Set THIRDPARTY_UNZIP_PATH="!OPENTWIN_THIRDPARTY_ROOT!\Installer_Tools"
+Set THIRDPARTY_ZIPFILE="!OPENTWIN_THIRDPARTY_ROOT!\Installer_Tools\ThirdParty.zip"
 
 SET PYTHON_PATH="!OPENTWIN_DEV_ROOT!\Scripts\Installer\python"
 Set PY_PERMISSIONS="!OPENTWIN_DEV_ROOT!\Scripts\Installer\python\change_permissions.py"
@@ -32,8 +40,8 @@ Set PY_MONGOD_AUTH="!OPENTWIN_DEV_ROOT!\Scripts\Installer\python\mongoDB_storage
 
 
 REM Test for Python Installation
-python -V | find /v "Python" >NUL 2>NUL && (goto :PYTHON_NOT_INSTALLED)
-python -V | find "Python"    >NUL 2>NUL && (goto :PYTHON_INSTALLED)
+	python -V | find /v "Python" >NUL 2>NUL && (goto :PYTHON_NOT_INSTALLED)
+	python -V | find "Python"    >NUL 2>NUL && (goto :PYTHON_INSTALLED)
 
 :PYTHON_NOT_INSTALLED
 	echo ERROR: Python is not installed on your system.
@@ -41,20 +49,15 @@ python -V | find "Python"    >NUL 2>NUL && (goto :PYTHON_INSTALLED)
 
 	
 :PYTHON_INSTALLED
-for /f "delims=" %%V in ('python -V') do @set ver=%%V
-echo Python installation %ver% verified...
+	for /f "delims=" %%V in ('python -V') do @set ver=%%V
+	echo Python installation %ver% verified...
 
 
-if "!REG_VALUE!"=="" (
+if "!NSIS_REG_VALUE!"=="" (
     echo NSIS Installation not found!
-    set /p "NSIS_Install_Input=Do you wish to install NSIS now? (y/n): "
-    if /i "!NSIS_Install_Input!"=="y" (
-        GOTO INSTALL_NSIS
-    ) else (
-        GOTO END_FAIL
-    )
+	GOTO EXIT
 ) else (
-    echo NSIS Installation verified in '!REG_VALUE!'...
+    echo NSIS Installation verified in '!NSIS_REG_VALUE!'...
 	echo -------------------------------------------------------------
 	echo Script compilation will take a few minutes, please be patient!
 	echo Ready to compile!
@@ -64,6 +67,12 @@ if "!REG_VALUE!"=="" (
 
 :COMPILE
 echo +++ COMPILE TIME +++
+
+	echo Extracting Third Party Toolchain using 7-Zip...
+	@REM echo !SEVENZIP_REG_DATA!
+	@REM pause
+	"!SEVENZIP_REG_DATA!\7z.exe" x !THIRDPARTY_ZIPFILE! -o!THIRDPARTY_UNZIP_PATH! -y
+	pause
 	
 	echo Compiling Python Scripts...
 	cd !PYTHON_PATH!
@@ -76,18 +85,6 @@ echo +++ COMPILE TIME +++
 	!MAKENSIS_PATH! /V3 !ENDUSER_NSI!
 	!MAKENSIS_PATH! /V3 !DEVELOPER_NSI!
 	GOTO END_SUCCESS
-
-:INSTALL_NSIS
-echo Installing NSIS...
-	START "" /wait "!OPENTWIN_THIRDPARTY_ROOT!\NullsoftScriptableInstallSystem\nsis-3.09-setup.exe"
-	if %ERRORLEVEL% EQU 0 (
-		echo NSIS Installed successfully! Restarting batch process...
-		START "" "%~0"
-		exit
-	) else (
-		echo NSIS Installation failed! Aborting...
-		GOTO EXIT
-	)
 
 :END_SUCCESS
 	echo ---------------------------------------------
