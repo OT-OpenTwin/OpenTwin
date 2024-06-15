d7bfcc0dd384554d9e94bcd70c9b341a45edd2ec
Alexander Küster <alexk95@users.noreply.github.com>
2024-06-15 14:24:48 +0200

    Added split string by character to string helper

--- Libraries/OTCore/include/OTCore/StringHelper.h
+++ Libraries/OTCore/include/OTCore/StringHelper.h
@@ -17,9 +17,21 @@ namespace ot {
 
 	//! @brief Split the provided string
 	//! @param _str String to split
+	//! @param _splitBy Char to split at
+	//! @param _skipEmpty If true, all empty parts will be skipped
+	OT_CORE_API_EXPORT std::list<std::string> splitString(const std::string& _str, char _splitBy, bool _skipEmpty = false);
+
+	//! @brief Split the provided string
+	//! @param _str String to split
 	//! @param _splitBy String to split at
 	//! @param _skipEmpty If true, all empty parts will be skipped
 	OT_CORE_API_EXPORT std::list<std::string> splitString(const std::string& _str, const std::string& _splitBy, bool _skipEmpty = false);
+
+	//! @brief Split the provided string
+	//! @param _str String to split
+	//! @param _splitBy Char to split at
+	//! @param _skipEmpty If true, all empty parts will be skipped
+	OT_CORE_API_EXPORT std::list<std::wstring> splitString(const std::wstring& _str, wchar_t _splitBy, bool _skipEmpty = false);
 
 	//! @brief Split the provided string
 	//! @param _str String to split
