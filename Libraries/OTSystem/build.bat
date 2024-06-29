eec08e937160b5d04bcb46d34df170f761ebb050
Reeva30 <167049831+Reeva30@users.noreply.github.com>
2024-06-29 13:48:44 +0200

    Update build.bat

--- Libraries/OTSystem/build.bat
+++ Libraries/OTSystem/build.bat
@@ -19,11 +19,18 @@ IF "%DEVENV_ROOT_2022%" == "" (
 	goto PAUSE_END
 )
 
+REM Echo environment variables for debugging
+ECHO OPENTWIN_DEV_ROOT=%OPENTWIN_DEV_ROOT%
+ECHO OPENTWIN_THIRDPARTY_ROOT=%OPENTWIN_THIRDPARTY_ROOT%
+ECHO DEVENV_ROOT_2022=%DEVENV_ROOT_2022%
+
 REM Setup eviroment
 CALL "%OPENTWIN_DEV_ROOT%\Scripts\SetupEnvironment.bat"
 
 REM Ensure that the script finished successfully
 IF NOT "%OPENTWIN_DEV_ENV_DEFINED%" == "1" (
+    ECHO Environment setup failed
+
 	goto END
 )
 
@@ -55,11 +62,19 @@ IF "%2"=="BUILD" (
 IF %DEBUG%==1 (
 	ECHO %TYPE_NAME% DEBUG
 	"%DEVENV_ROOT_2022%\devenv.exe" "%OT_SYSTEM_ROOT%\OTSystem.vcxproj" %TYPE% "Debug|x64" /Out buildLog_Debug.txt
+ 	IF ERRORLEVEL 1 (
+        ECHO Debug build failed
+        GOTO END
+    )
 )
 
 IF %RELEASE%==1 (
 	ECHO %TYPE_NAME% RELEASE
 	"%DEVENV_ROOT_2022%\devenv.exe" "%OT_SYSTEM_ROOT%\OTSystem.vcxproj" %TYPE% "Release|x64" /Out buildLog_Release.txt
+	IF ERRORLEVEL 1 (
+        ECHO Release build failed
+        GOTO END
+    )
 ) 
   
 GOTO END
