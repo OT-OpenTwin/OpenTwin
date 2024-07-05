a0fde728710873d1147b692c459ba3106051edf6
Reeva30 <167049831+Reeva30@users.noreply.github.com>
2024-07-05 23:40:26 +0200

    Update build.bat

--- Libraries/OTRandom/build.bat
+++ Libraries/OTRandom/build.bat
@@ -54,12 +54,12 @@ IF "%2"=="BUILD" (
 
 IF %DEBUG%==1 (
 	ECHO %TYPE_NAME% DEBUG
-	"%DEVENV_ROOT_2022%\devenv.com" "%OT_RANDOM_ROOT%\OTRandom.sln" %TYPE% "Debug|x64" /Out buildLog_Debug.txt
+	"%DEVENV_ROOT_2022%\devenv.exe" "%OT_RANDOM_ROOT%\OTRandom.vcxproj" %TYPE% "debug" 
 )
 
 IF %RELEASE%==1 (
 	ECHO %TYPE_NAME% RELEASE
-	"%DEVENV_ROOT_2022%\devenv.exe" "%OT_RANDOM_ROOT%\OTRandom.sln" %TYPE% "Release|x64" /Out buildLog_Release.txt
+	"%DEVENV_ROOT_2022%\devenv.exe" "%OT_RANDOM_ROOT%\OTRandom.sln" %TYPE% "release" 
 ) 
   
 GOTO END
