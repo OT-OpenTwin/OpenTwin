62945c537b555729ee6fbb9daa10261094503370
Reeva30 <167049831+Reeva30@users.noreply.github.com>
2024-07-06 23:51:46 +0200

    Update build.bat

--- Libraries/ResultDataAccess/build.bat
+++ Libraries/ResultDataAccess/build.bat
@@ -54,12 +54,12 @@ IF "%2"=="BUILD" (
 
 IF %DEBUG%==1 (
 	ECHO %TYPE_NAME% DEBUG
-	"%DEVENV_ROOT_2022%\devenv.exe" "%OT_RESULT_DATA_ACCESS_ROOT%\ResultDataAccess.vcxproj" %TYPE% "Debug|x64" /Out buildLog_Debug.txt
+	"%DEVENV_ROOT_2022%\devenv.com" "%OT_RESULT_DATA_ACCESS_ROOT%\ResultDataAccess.vcxproj" %TYPE% "Debug|x64" /Out buildLog_Debug.txt
 )
 
 IF %RELEASE%==1 (
 	ECHO %TYPE_NAME% RELEASE
-	"%DEVENV_ROOT_2022%\devenv.exe" "%OT_RESULT_DATA_ACCESS_ROOT%\ResultDataAccess.vcxproj" %TYPE% "Release|x64" /Out buildLog_Release.txt
+	"%DEVENV_ROOT_2022%\devenv.com" "%OT_RESULT_DATA_ACCESS_ROOT%\ResultDataAccess.vcxproj" %TYPE% "Release|x64" /Out buildLog_Release.txt
 ) 
   
 GOTO END
