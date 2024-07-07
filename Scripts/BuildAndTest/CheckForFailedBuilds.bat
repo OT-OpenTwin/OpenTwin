11abe588a47a067f47eedd4cb9e0c6cfaecfc797
Reeva30 <167049831+Reeva30@users.noreply.github.com>
2024-07-07 21:23:57 +0200

    Update CheckForFailedBuilds.bat

--- Scripts/BuildAndTest/CheckForFailedBuilds.bat
+++ Scripts/BuildAndTest/CheckForFailedBuilds.bat
@@ -1,4 +1,4 @@
-@echo off
+@echo on
 findstr /c:"1 failed" < buildLog_Summary.txt
 if %errorlevel%==0 (
 PowerShell -Command "Add-Type -AssemblyName PresentationFramework;[System.Windows.MessageBox]::Show('Project(s) have failed in build process')"
