7d6184331ba3cb326ea333290bb75ea54fc68a1c
JWagner <jan.wagner@fb2.fra-uas.de>
2024-10-22 15:42:05 +0200

    Fixed coding style

--- Libraries/OTCore/src/EncodingGuesser.cpp
+++ Libraries/OTCore/src/EncodingGuesser.cpp
@@ -42,8 +42,14 @@ ot::TextEncoding::EncodingStandard ot::EncodingGuesser::operator()(const char* f
 ot::TextEncoding::EncodingStandard ot::EncodingGuesser::operator()(const std::vector<char>& fileContent)
 {
 	uint64_t numberOfBytes = fileContent.size();
-	if (numberOfBytes == 0) return TextEncoding::UTF8;
-	else return operator()(&fileContent[0], numberOfBytes);
+	if (numberOfBytes == 0)
+	{
+		return TextEncoding::UTF8;
+	}
+	else
+	{
+		return operator()(&fileContent[0], numberOfBytes);
+	}
 }
 
 bool ot::EncodingGuesser::CheckIfISO(byte* fileContent, int64_t numberOfBytes)
