1f505f0be3b1504f5b2ba373eff1a5f93f66bc1a
Alexander Küster <alexk95@users.noreply.github.com>
2024-11-22 16:51:23 +0100

    Improved styled text builder to support more controls and ebaled multiline support

--- Libraries/OTGui/include/OTGui/StyledTextStyle.h
+++ Libraries/OTGui/include/OTGui/StyledTextStyle.h
@@ -14,11 +14,24 @@ namespace ot {
 
 	class OT_GUI_API_EXPORT StyledTextStyle : public Serializable {
 	public:
+		enum class TextSize {
+			Regular,
+			Header1,
+			Header2,
+			Header3
+		};
+
+		static std::string toString(TextSize _size);
+		static TextSize stringToTextSize(const std::string& _size);
+
 		StyledTextStyle();
 		StyledTextStyle(const ConstJsonObject& _jsonObject);
 		StyledTextStyle(const StyledTextStyle&) = default;
 		virtual ~StyledTextStyle() = default;
 		StyledTextStyle& operator = (const StyledTextStyle&) = default;
+
+		bool operator == (const StyledTextStyle& _other) const;
+		bool operator != (const StyledTextStyle& _other) const;
 
 		//! \brief Add the object contents to the provided JSON object.
 		//! \param _object Json object reference to write the data to.
@@ -39,14 +52,27 @@ namespace ot {
 		void setItalic(bool _isItalic) { m_isItalic = _isItalic; };
 		bool getItalic(void) const { return m_isItalic; };
 
+		void setUnderline(bool _isUnderline) { m_isUnderline = _isUnderline; };
+		bool getUnderline(void) const { return m_isUnderline; };
+
+		void setLineTrough(bool _isLineTrough) { m_isLineTrough = _isLineTrough; };
+		bool getLineTrough(void) const { return m_isLineTrough; };
+
+		void setTextSize(TextSize _size) { m_size = _size; };
+		TextSize getTextSize(void) const { return m_size; };
+
 		//! \brief Returns true if any style changes are set.
 		//! If any style is set the generated text requires styling otherwise the default text editor style will be used.
-		bool hasStyleSet(void) const { return m_colorReference != StyledText::ColorReference::Default || m_isBold || m_isItalic; };
+		bool hasStyleSet(void) const { return m_colorReference != StyledText::ColorReference::Default || m_isBold || m_isItalic || m_isUnderline || m_isLineTrough || m_size != TextSize::Regular; };
 
 	private:
 		StyledText::ColorReference m_colorReference;
 		bool m_isBold;
 		bool m_isItalic;
+		bool m_isUnderline;
+		bool m_isLineTrough;
+		TextSize m_size;
+
 	};
 
 }
