adf02c29e46784c00757040e02666b30bf466950
Alexander Küster <alexk95@users.noreply.github.com>
2024-12-23 17:18:11 +0100

    Added SignalBlockWrapper

---
+++ Libraries/OTWidgets/include/OTWidgets/SignalBlockWrapper.h
@@ -0,0 +1,43 @@
+//! @file SignalBlockWrapper.h
+//! @author Alexander Kuester (alexk95)
+//! @date December 2024
+// ###########################################################################################################################################################################################################################################################################################################################
+
+#pragma once
+
+// OpenTwin header
+#include "OTCore/OTClassHelper.h"
+#include "OTWidgets/OTWidgetsAPIExport.h"
+
+// Qt header
+#include <QtCore/qobject.h>
+
+namespace ot {
+
+	//! \brief The SignalBlockWrapper is a RAII wrapper for blocking signals of a given QObject.
+	class OT_WIDGETS_API_EXPORT SignalBlockWrapper {
+		OT_DECL_NOCOPY(SignalBlockWrapper)
+		OT_DECL_NODEFAULT(SignalBlockWrapper)
+	public:
+		//! \brief Constructor.
+		//! The currently set "block signals" state of the given object will be stored and the signals will be blocked.
+		SignalBlockWrapper(QObject* _object) : m_object(_object), m_initialState(false) {
+			if (m_object) {
+				m_initialState = m_object->signalsBlocked();
+				m_object->blockSignals(true);
+			}
+		}
+
+		//! \brief Destructor.
+		//! The "blocked signals" state of the provided object will be set back to the initial value, any previous changes will be ignored.
+		~SignalBlockWrapper() {
+			if (m_object) {
+				m_object->blockSignals(m_initialState);
+			}
+		}
+	private:
+		QObject* m_object;
+		bool m_initialState;
+	};
+
+}
