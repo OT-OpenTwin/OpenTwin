648500aecac8f57b1af641eb52791a22715e47c2
JWagner <jan.wagner@fb2.fra-uas.de>
2025-01-02 11:15:08 +0100

    Improved progress update wrapper

--- Libraries/OTServiceFoundation/include/OTServiceFoundation/ProgressUpdater.h
+++ Libraries/OTServiceFoundation/include/OTServiceFoundation/ProgressUpdater.h
@@ -1,6 +1,8 @@
 #pragma once
 #include "OTServiceFoundation/UiComponent.h"
 #include "OTCore/DefensiveProgramming.h"
+#include <chrono>
+
 //! @brief RAII wrapper for the processbar, including  an API to trigger an update with set frequency.
 class __declspec(dllexport) ProgressUpdater
 {
@@ -16,12 +18,18 @@ public:
 	void setTriggerFrequency(uint64_t  _triggerFrequency);
 	void setTotalNumberOfSteps(uint64_t  _totalNumberofSteps);
 	void setTotalNumberOfUpdates(uint32_t  _numberOfUpdates, uint64_t _totalNumberofSteps);
+	void setTimeTrigger(std::chrono::seconds _timeTrigger);
 	void triggerUpdate(int32_t _currentStep);
+
+	uint64_t getTriggerFrequency() const { return m_triggerFrequency; }
+	uint64_t getTotalNumberOfSteps() const { return m_totalNumberOfSteps; }
 private:
 	ProgressUpdater() {};
 	inline bool invariant() { return m_uiComponent != nullptr; }
-
+	std::chrono::steady_clock::time_point m_priorTimestamp;
 	ot::components::UiComponent* m_uiComponent = nullptr;
 	uint64_t m_triggerFrequency = 0;
 	uint64_t m_totalNumberOfSteps = 0;
+	std::chrono::seconds m_timeTrigger = std::chrono::seconds(2);
+
 };
--- Libraries/OTServiceFoundation/src/ProgressUpdater.cpp
+++ Libraries/OTServiceFoundation/src/ProgressUpdater.cpp
@@ -1,12 +1,13 @@
 #include "OTServiceFoundation/ProgressUpdater.h"
 #include <cmath>
+#include <thread>
 
 ProgressUpdater::ProgressUpdater(ot::components::UiComponent* _uiComponent, const std::string& _progressbarMessage)
 	: m_uiComponent(_uiComponent)
 {
 	INVARIANT;
 	m_uiComponent->setProgressInformation(_progressbarMessage, false);
-	m_uiComponent->setProgress(0);
+	m_uiComponent->setProgress(0);	
 }
 
 ProgressUpdater::~ProgressUpdater()
@@ -40,17 +41,26 @@ void ProgressUpdater::setTotalNumberOfUpdates(uint32_t _numberOfUpdates, uint64_
 	POST(m_totalNumberOfSteps > 0 && m_triggerFrequency > 0);
 }
 
+void ProgressUpdater::setTimeTrigger(std::chrono::seconds _timeTrigger)
+{
+	m_timeTrigger = _timeTrigger;
+}
+
 void ProgressUpdater::triggerUpdate(int32_t _currentStep)
 {
 	PRE(m_totalNumberOfSteps > 0 && m_triggerFrequency > 0);
 
+	std::chrono::steady_clock::time_point currentTime = std::chrono::steady_clock::now();
+	std::chrono::seconds duration = std::chrono::duration_cast<std::chrono::seconds>(currentTime - m_priorTimestamp);
+	m_priorTimestamp = currentTime;
+
 	const bool isFirstStep = _currentStep == 0;
 	const bool isLastStep = _currentStep == m_totalNumberOfSteps;
 	const bool currentStepShouldTrigger = _currentStep% m_triggerFrequency == 0;
-
-	if (!isFirstStep && !isLastStep && currentStepShouldTrigger)
+	
+	if (!isFirstStep && !isLastStep && (duration >= m_timeTrigger || currentStepShouldTrigger))
 	{
-		const uint32_t percentage =  static_cast<uint32_t>(_currentStep / m_totalNumberOfSteps * 100);
+		const uint32_t percentage = static_cast<uint32_t>(std::lround(static_cast<float>(_currentStep) / m_totalNumberOfSteps * 100));
 		m_uiComponent->setProgress(percentage);
 	}
 }
