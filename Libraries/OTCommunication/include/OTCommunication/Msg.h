b120e18b7cf123dc2eca84c6aeb8039227d8b92c
Alexander Küster <alexk95@users.noreply.github.com>
2025-01-11 19:58:44 +0100

    Error log is now generated if the curl send request fails

--- Libraries/OTCommunication/include/OTCommunication/Msg.h
+++ Libraries/OTCommunication/include/OTCommunication/Msg.h
@@ -22,28 +22,13 @@ namespace ot {
 		//! @param _receiverIP The receiver IP address
 		//! @param _message The message to send
 		//! @param _response Here the response will be written to
-		OT_COMMUNICATION_API_EXPORT bool send(
-			const std::string &				_senderIP,
-			const std::string &				_receiverIP,
-			ot::MessageType					_type,
-			const std::string &				_message,
-			std::string &					_response,
-			int								_timeout = 3000,
-			bool							_shutdownMessage = false,
-			bool							_createLogMessage = true
-		);
+		OT_COMMUNICATION_API_EXPORT bool send(const std::string& _senderIP, const std::string& _receiverIP, ot::MessageType _type, const std::string& _message, std::string& _response, int _timeout = 3000, bool _shutdownMessage = false, bool _createLogMessage = true);
 
 		//! @brief Will send the provided message to the microservice framework. The massage will be send asynchronously
 		//! @param _senderIP The sender IP address
 		//! @param _receiverIP The receiver IP address
 		//! @param _message The message to send
 		//! @param _response Here the response will be written to
-		OT_COMMUNICATION_API_EXPORT void sendAsync(
-			const std::string &				_senderIP,
-			const std::string &				_receiverIP,
-			ot::MessageType					_type,
-			const std::string &				_message,
-			int								_timeout = 3000
-		);
+		OT_COMMUNICATION_API_EXPORT void sendAsync(const std::string& _senderIP, const std::string& _receiverIP, ot::MessageType _type, const std::string& _message, int _timeout = 3000, bool _createLogMessage = true);
 	}
 }
--- Libraries/OTCommunication/src/msg.cpp
+++ Libraries/OTCommunication/src/msg.cpp
@@ -7,6 +7,7 @@
  */
 
 // OpenTwin header
+#include "OTSystem/ArchitectureInfo.h"
 #include "OTCore/Logger.h"
 #include "OTCore/OTAssert.h"
 #include "OTCommunication/Msg.h"			// Corresponding header
@@ -24,7 +25,7 @@
 
 namespace ot {
 	namespace intern {
-		void sendAsyncWorker(const std::string& _senderURL, const std::string& _receiverURL, ot::MessageType _type, const std::string& _message, int _timeout);
+		void sendAsyncWorker(const std::string& _senderURL, const std::string& _receiverURL, ot::MessageType _type, const std::string& _message, int _timeout, bool _createLogMessage);
 	}
 }
 
@@ -44,35 +45,23 @@ std::string get_env_var(std::string const& key)
 	return std::string(buffer);
 }
 
-bool ot::msg::send(
-	const std::string &				_senderIP,
-	const std::string &				_receiverIP,
-	MessageType						_type,
-	const std::string &				_message,
-	std::string &					_response,
-	int								_timeout,
-	bool							_shutdownMessage,
-	bool							_createLogMessage
-) {
+bool ot::msg::send(const std::string& _senderIP, const std::string& _receiverIP, MessageType _type, const std::string& _message, std::string& _response, int _timeout, bool _shutdownMessage, bool _createLogMessage) {
 	if (_senderIP == _receiverIP) {
-		if (_createLogMessage) { OT_LOG_W("Receiver is the same as sender: \"" + _receiverIP + "\". Ignoring message..."); }
+		if (_createLogMessage) {
+			OT_LOG_W("Receiver is the same as sender: \"" + _receiverIP + "\". Ignoring message...");
+		}
 		return true;
 	}
 
-	if (_createLogMessage) { OT_LOG("Sending message to (Receiver = \"" + _receiverIP + "\"; Endpoint = " + (_type == ot::EXECUTE ? "Execute" : (_type == ot::QUEUE ? "Queue" : "Execute one way TLS")) + "). Message = \"" + _message + "\"", ot::OUTGOING_MESSAGE_LOG); }
-
-	//using namespace std::chrono_literals;
-	//thread_local static bool lock = false;
-	//while (lock) std::this_thread::sleep_for(1ms);
-	//lock = true;
+	if (_createLogMessage) {
+		OT_LOG("Sending message to (Receiver = \"" + _receiverIP + "\"; Endpoint = " + (_type == ot::EXECUTE ? "Execute" : (_type == ot::QUEUE ? "Queue" : "Execute one way TLS")) + "). Message = \"" + _message + "\"", ot::OUTGOING_MESSAGE_LOG);
+	}
 
 	// Setting up the shared data between handles.
 	thread_local static CURLSH* share = nullptr;
 
-	if (!_shutdownMessage)
-	{
-		if (share == nullptr)
-		{
+	if (!_shutdownMessage) {
+		if (share == nullptr) {
 			share = curl_share_init();
 			curl_share_setopt(share, CURLSHOPT_SHARE, CURL_LOCK_DATA_COOKIE);
 			curl_share_setopt(share, CURLSHOPT_SHARE, CURL_LOCK_DATA_DNS);
@@ -80,37 +69,28 @@ bool ot::msg::send(
 			curl_share_setopt(share, CURLSHOPT_SHARE, CURL_LOCK_DATA_CONNECT); // Share Connection Cache
 		}
 	}
-	else
-	{
-		if (share != nullptr)
-		{
+	else {
+		if (share != nullptr) {
 			curl_share_cleanup(share);
 			share = nullptr;
 		}
 	}
 
 	auto curl = curl_easy_init();
-	if (!curl)
-	{
-		//lock = false;
-		OTAssert(0, "Failed to initialize curl");
-		OT_LOG_E("Failed to initialize curl");
+	if (!curl) {
+		OT_LOG_EA("Failed to initialize curl");
 		return false;
 	}
 
-	//NOTE, is the url the receiver url?
 	std::string curlURL("https://");
 	curlURL.append(_receiverIP).append("/");
 
-	switch (_type)
-	{
+	switch (_type) {
 	case QUEUE: curlURL.append("queue"); break;
 	case EXECUTE: curlURL.append("execute"); break;
 	case EXECUTE_ONE_WAY_TLS: curlURL.append("execute-one-way-tls"); break;
 	default:
-		//lock = false;
-		OTAssert(0, "Unknown message type");
-		OT_LOG_E("Unknown message type");
+		OT_LOG_EA("Unknown message type");
 		return false;
 	}
 
@@ -134,21 +114,17 @@ bool ot::msg::send(
 	curl_easy_setopt(curl, CURLOPT_HEADERDATA, &header_string);
 
 	// SSL/TLS configuration
-
 	static std::string ca_cert_file;
 	static std::string ca_client_cert;
 	static std::string ca_client_key;
 
-	if (_type == EXECUTE_ONE_WAY_TLS)
-	{
-		if (ca_cert_file.empty())
-		{
+	if (_type == EXECUTE_ONE_WAY_TLS) {
+		if (ca_cert_file.empty()) {
 			ca_cert_file = get_env_var("OPEN_TWIN_CA_CERT");
 
-			if (!std::filesystem::exists(ca_cert_file))
-			{
+			if (!std::filesystem::exists(ca_cert_file)) {
 				// Get the path of the executable 
-#ifdef _WIN32
+#ifdef OT_OS_WINDOWS
 				char path[MAX_PATH] = { 0 };
 				GetModuleFileNameA(NULL, path, MAX_PATH);
 				ca_cert_file = path;
@@ -161,8 +137,7 @@ bool ot::msg::send(
 				ca_cert_file += "\\Certificates\\ca.pem";
 
 				// Check whether local cert file ca.pem exists
-				if (!std::filesystem::exists(ca_cert_file))
-				{
+				if (!std::filesystem::exists(ca_cert_file)) {
 					// Get the development root environment variable and build the path to the deployment cert file
 					std::string dev_root = get_env_var("OPENTWIN_DEV_ROOT");
 					ca_cert_file = dev_root + "\\Deployment\\Certificates\\ca.pem";
@@ -170,24 +145,17 @@ bool ot::msg::send(
 			}
 		}
 
-		// cannot log here: will deadlock... find a better solution to add this to log
-		//OT_LOG_D("Using CA certificate file: " + ca_cert_file);
-
 		curl_easy_setopt(curl, CURLOPT_CAINFO, ca_cert_file.c_str());
 	}
-	else
-	{
+	else {
 		// If the environment variables are set, then we perform a call through mTLS
-
-		if (ca_client_cert.empty() || ca_client_key.empty() || ca_cert_file.empty())
-		{
+		if (ca_client_cert.empty() || ca_client_key.empty() || ca_cert_file.empty()) {
 			ca_cert_file = get_env_var("OPEN_TWIN_CA_CERT");
 			ca_client_cert = get_env_var("OPEN_TWIN_SERVER_CERT");
 			ca_client_key = get_env_var("OPEN_TWIN_SERVER_CERT_KEY");
 		}
 
-		if (ca_client_cert != "" && ca_client_key != "" && ca_cert_file != "")
-		{
+		if (ca_client_cert != "" && ca_client_key != "" && ca_cert_file != "") {
 			curl_easy_setopt(curl, CURLOPT_SSLCERT, ca_client_cert.c_str());
 			curl_easy_setopt(curl, CURLOPT_SSLKEY, ca_client_key.c_str());
 			curl_easy_setopt(curl, CURLOPT_CAINFO, ca_cert_file.c_str());
@@ -197,8 +165,7 @@ bool ot::msg::send(
 	curl_easy_setopt(curl, CURLOPT_HTTP_VERSION, (long)CURL_HTTP_VERSION_2TLS);
 	curl_easy_setopt(curl, CURLOPT_VERBOSE, 0); // For testing purposes set to 1
 
-	if (!_shutdownMessage)
-	{
+	if (!_shutdownMessage) {
 		curl_easy_setopt(curl, CURLOPT_TCP_KEEPALIVE, 1L);
 
 		// SHARED CACHES CONFIG
@@ -212,34 +179,23 @@ bool ot::msg::send(
 
 	// Send the request
 	CURLcode errorCode = curl_easy_perform(curl);
-	//std::cout << "URL Code received: " << res << std::endl;
-
+	
 	curl_easy_cleanup(curl);
-	//curl_share_cleanup(share);
 	curl = NULL;
 
-	//lock = false;
-	if (errorCode == CURLE_OK)
-	{
-		if (_createLogMessage) 
-		{ 
+	if (errorCode == CURLE_OK) {
+		if (_createLogMessage)  {
 			OT_LOG(".. Message sent successful (Receiver = \"" + _receiverIP + "\"; Endpoint = " + (_type == ot::EXECUTE ? "Execute" : (_type == ot::QUEUE ? "Queue" : "Execute one way TLS")) + "). Response = \"" + _response + "\"", ot::OUTGOING_MESSAGE_LOG); 
 		}
 		return true;
 	}
-	else
-	{
-		std::string errorString = curl_easy_strerror(errorCode);
-		if (_createLogMessage)
-		{
-			OT_LOG_D(
-				".. Message sent failed "
-				"Error message: " + errorString + "; "
-				"Error buffer: " + errbuf + "; "
-				"(Receiver = \"" + _receiverIP + "\"; "
-				"Endpoint = " + (_type == ot::EXECUTE ? "Execute" : (_type == ot::QUEUE ? "Queue" : "Execute one way TLS")) + "). );"
-				
-			);
+	else {
+		if (_createLogMessage) {
+			OT_LOG_E("Message sent failed: { \"Error message\": \"" + std::string(curl_easy_strerror(errorCode)) +
+				"\", \"Error buffer\": \"" + errbuf + 
+				"\", \"Receiver\": \"" + _receiverIP +
+				"\", \"Endpoint\": " + (_type == ot::EXECUTE ? "\"Execute\"" : (_type == ot::QUEUE ? "\"Queue\"" : "\"Execute one way TLS\"")) +
+				" }");
 		}
 		return false;
 	}
@@ -250,15 +206,16 @@ void ot::msg::sendAsync(
 	const std::string &				_receiverIP,
 	MessageType						_type,
 	const std::string &				_message,
-	int								_timeout
+	int								_timeout,
+	bool							_createLogMessage
 ) {
-	std::thread t(ot::intern::sendAsyncWorker, _senderIP, _receiverIP, _type, _message, _timeout);
+	std::thread t(ot::intern::sendAsyncWorker, _senderIP, _receiverIP, _type, _message, _timeout, _createLogMessage);
 	t.detach();
 }
 
-void ot::intern::sendAsyncWorker(const std::string& _senderIP, const std::string& _receiverIP, ot::MessageType _type, const std::string& _message, int _timeout) {
+void ot::intern::sendAsyncWorker(const std::string& _senderIP, const std::string& _receiverIP, ot::MessageType _type, const std::string& _message, int _timeout, bool _createLogMessage) {
 	std::string response;
-	if (!ot::msg::send(_senderIP, _receiverIP, _type, _message, response, _timeout)) {
+	if (!ot::msg::send(_senderIP, _receiverIP, _type, _message, response, _timeout, false, _createLogMessage)) {
 		OT_LOG_E("[ASYNC] Failed to send message to \"" + _receiverIP + "\"");
 	}
 	OT_ACTION_IF_RESPONSE_ERROR(response) {
