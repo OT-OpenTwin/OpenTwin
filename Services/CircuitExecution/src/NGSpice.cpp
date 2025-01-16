de4d4f76bf0b2008df8d5fca624798d5018af3a7
Sebastian Urmann <sebastian.urmann@stud.fra-uas.de>
2025-01-16 16:01:28 +0100

    Commented out the forwarding of NgSpice callback due to performance.

--- Services/CircuitExecution/src/NGSpice.cpp
+++ Services/CircuitExecution/src/NGSpice.cpp
@@ -17,7 +17,7 @@ NGSpice::NGSpice() {
 //Callback Functions for NGSpice
 int NGSpice::MySendCharFunction(char* output, int ident, void* userData) {
 
-	SimulationResults::getInstance()->triggerCallback("Message", std::string(output));
+	//SimulationResults::getInstance()->triggerCallback("Message", std::string(output));
 	
 	OT_LOG_D(std::string(output));
 	
@@ -26,7 +26,7 @@ int NGSpice::MySendCharFunction(char* output, int ident, void* userData) {
 
 int NGSpice::MySendStat(char* outputReturn, int ident, void* userData) {
 	
-	SimulationResults::getInstance()->triggerCallback("Message", std::string(outputReturn));
+	//SimulationResults::getInstance()->triggerCallback("Message", std::string(outputReturn));
 	OT_LOG_D(std::string(outputReturn));
 
 	return 0;
