093d2d958943e2e239a6d5c3ae47a966565941e7
Alexander Küster <alexk95@users.noreply.github.com>
2025-02-01 19:45:47 +0100

    Adjusted build documentation scripts fixing setup issue

--- Documentation/Doxygen/Generate.bat
+++ Documentation/Doxygen/Generate.bat
@@ -1,9 +1,9 @@
 @echo off
 
-:: This script requires the following environment variables to be set:
-:: 1. OPENTWIN_DEV_ROOT
-:: 2. OPENTWIN_THIRDPARTY_ROOT
-:: 3. DEVENV_ROOT_2022
+rem This script requires the following environment variables to be set:
+rem 1. OPENTWIN_DEV_ROOT
+rem 2. OPENTWIN_THIRDPARTY_ROOT
+rem 3. DEVENV_ROOT_2022
 if "%OPENTWIN_DEV_ROOT%" == "" (
 	echo Please specify the following environment variables: OPENTWIN_DEV_ROOT
 	goto PAUSE_END
@@ -14,35 +14,39 @@ if "%OPENTWIN_THIRDPARTY_ROOT%" == "" (
 	goto PAUSE_END
 )
 
-:: ############################################################################################################################
+rem ############################################################################################################################
 
-:: Setup eviroment
+rem Setup eviroment
 call "%OPENTWIN_DEV_ROOT%\Scripts\SetupEnvironment.bat"
 
-:: Ensure that the script finished successfully
+rem Ensure that the script finished successfully
 if not "%OPENTWIN_DEV_ENV_DEFINED%" == "1" (
 	goto END
 )
 
-:: ############################################################################################################################
+rem ############################################################################################################################
 
-:: Setup code documentation build path
+REM Setup code documentation build path to developer documentation output.
 if "%OPENTWIN_CODEDOC_BUILD_OUTPUT_ROOT%" == "" (
-	set OPENTWIN_CODEDOC_BUILD_OUTPUT_ROOT=%OPENTWIN_DEV_ROOT%\Documentation\Doxygen
-	goto PREPARE_MATHJAX_REL_PATH
+	set "OPENTWIN_CODEDOC_BUILD_OUTPUT_ROOT=%OPENTWIN_DEV_ROOT%\Documentation\Developer\_build\html\_static"
 )
+
+if "%MATHJAX_REL_PATH%" == "" (
+	goto GET_MATHJAX_RELATIVE_PATH
+)
+
 goto START_BUILD
 
-:PREPARE_MATHJAX_REL_PATH
+:GET_MATHJAX_RELATIVE_PATH
 
 REM This only works if the dev root and the third party root have the common parent directory
-SET ot_lcl_batch_tmp=cd
+SET "ot_lcl_batch_tmp=cd"
 cd %OPENTWIN_DEV_ROOT%
 cd ..
-SET ot_lcl_batch_tmp_dev=cd
+SET "ot_lcl_batch_tmp_dev=cd"
 cd %OPENTWIN_THIRDPARTY_ROOT%
 cd ..
-SET ot_lcl_batch_tmp_thi=cd
+SET "ot_lcl_batch_tmp_thi=cd"
 cd ot_lcl_batch_tmp
 
 if "%ot_lcl_batch_tmp_dev%" == "%ot_lcl_batch_tmp_thi%" (
@@ -55,11 +59,19 @@ goto PAUSE_END
 :SET_MATHJAX_REL_PATH
 for %%A in ("%OPENTWIN_THIRDPARTY_ROOT%") do set "ot_lcl_third_party_name=%%~nxA"
 echo Directory name: %ot_lcl_third_party_name%
-set MATHJAX_REL_PATH=..\..\..\%ot_lcl_third_party_name%\%MATHJAX_REL_PATH_SUFFIX%
+set "MATHJAX_REL_PATH=..\..\..\%ot_lcl_third_party_name%\%MATHJAX_REL_PATH_SUFFIX%"
 
-:: ############################################################################################################################
+if "%MATHJAX_REL_PATH%" == "" (
+	echo Relative path not generated. Please provide MATHJAX_REL_PATH > "%OPENTWIN_DEV_ROOT%\Scripts\BuildAndTest\Documentation_buildLog.txt"
+	goto PAUSE_END
+)
+
+rem ############################################################################################################################
 
 :START_BUILD
+
+rem Get the current timestamp (capture echo call from the batch)
+for /f "delims=" %%a in ('call "%OPENTWIN_DEV_ROOT%\Scripts\Other\PrintCurrentTimestamp.bat" "Code doc generation finished at: " ""') do set OT_LCL_BuildStart=%%a
 
 pushd "%OPENTWIN_DEV_ROOT%"
 
@@ -70,6 +82,13 @@ echo - MATHJAX_REL_PATH = "%MATHJAX_REL_PATH%"
 doxygen "%OPENTWIN_DEV_ROOT%\Documentation\Doxygen\Doxyfile"
 
 popd
+
+rem Get the current timestamp (capture echo call from the batch)
+ Get the current timestamp (capture echo call from the batch)
+for /f "delims=" %%a in ('call "%OPENTWIN_DEV_ROOT%\Scripts\Other\PrintCurrentTimestamp.bat" "Code doc generation finished at: " ""') do set OT_LCL_BuildEnd=%%a
+
+echo %OT_LCL_BuildStart%
+echo %OT_LCL_BuildEnd%
 
 goto END
 
--- Scripts/BuildAndTest/BuildDocumentation.bat
+++ Scripts/BuildAndTest/BuildDocumentation.bat
@@ -30,23 +30,29 @@ IF NOT "%OPENTWIN_DEV_ENV_DEFINED%" == "1" (
 	goto END
 )
 
+rem ############################################################################################################################
+
 REM Setup code documentation build path to developer documentation output.
 if "%OPENTWIN_CODEDOC_BUILD_OUTPUT_ROOT%" == "" (
-	set OPENTWIN_CODEDOC_BUILD_OUTPUT_ROOT=%OPENTWIN_DEV_ROOT%\Documentation\Developer\_build\html\_static
-	goto PREPARE_MATHJAX_REL_PATH
+	set "OPENTWIN_CODEDOC_BUILD_OUTPUT_ROOT=%OPENTWIN_DEV_ROOT%\Documentation\Developer\_build\html\_static"
 )
+
+if "%MATHJAX_REL_PATH%" == "" (
+	goto GET_MATHJAX_RELATIVE_PATH
+)
+
 goto START_BUILD
 
-:PREPARE_MATHJAX_REL_PATH
+:GET_MATHJAX_RELATIVE_PATH
 
 REM This only works if the dev root and the third party root have the common parent directory
-SET ot_lcl_batch_tmp=cd
+SET "ot_lcl_batch_tmp=cd"
 cd %OPENTWIN_DEV_ROOT%
 cd ..
-SET ot_lcl_batch_tmp_dev=cd
+SET "ot_lcl_batch_tmp_dev=cd"
 cd %OPENTWIN_THIRDPARTY_ROOT%
 cd ..
-SET ot_lcl_batch_tmp_thi=cd
+SET "ot_lcl_batch_tmp_thi=cd"
 cd ot_lcl_batch_tmp
 
 if "%ot_lcl_batch_tmp_dev%" == "%ot_lcl_batch_tmp_thi%" (
@@ -59,7 +65,14 @@ goto PAUSE_END
 :SET_MATHJAX_REL_PATH
 for %%A in ("%OPENTWIN_THIRDPARTY_ROOT%") do set "ot_lcl_third_party_name=%%~nxA"
 echo Directory name: %ot_lcl_third_party_name%
-set MATHJAX_REL_PATH=..\..\..\..\..\..\%ot_lcl_third_party_name%\%MATHJAX_REL_PATH_SUFFIX%
+set "MATHJAX_REL_PATH=..\..\..\..\..\..\%ot_lcl_third_party_name%\%MATHJAX_REL_PATH_SUFFIX%"
+
+if "%MATHJAX_REL_PATH%" == "" (
+	echo Relative path not generated. Please provide MATHJAX_REL_PATH > "%OPENTWIN_DEV_ROOT%\Scripts\BuildAndTest\Documentation_buildLog.txt"
+	goto PAUSE_END
+)
+
+rem ############################################################################################################################
 
 :START_BUILD
 
@@ -75,8 +88,29 @@ RMDIR /S /Q "%OPENTWIN_DEV_ROOT%\Documentation\Developer\_build\html\_static\cod
 ECHO ====================================================================
 ECHO Build Doxygen Documentation
 ECHO ====================================================================
+
+echo Building Doxygen documentation with following config: > "%OPENTWIN_DEV_ROOT%\Scripts\BuildAndTest\Documentation_buildLog.txt"
+echo - Output Path      = "%OPENTWIN_CODEDOC_BUILD_OUTPUT_ROOT%" > "%OPENTWIN_DEV_ROOT%\Scripts\BuildAndTest\Documentation_buildLog.txt"
+echo - MATHJAX_REL_PATH = "%MATHJAX_REL_PATH%" > "%OPENTWIN_DEV_ROOT%\Scripts\BuildAndTest\Documentation_buildLog.txt"
+
+rem ############################################################################################################################
+
+REM Get the current timestamp (capture echo call from the batch)
+for /f "delims=" %%a in ('call "%OPENTWIN_DEV_ROOT%\Scripts\Other\PrintCurrentTimestamp.bat" "Started at: " ""') do set OT_LCL_BuildCodeStart=%%a
+echo %OT_LCL_BuildCodeStart%
+echo %OT_LCL_BuildCodeStart% > "%OPENTWIN_DEV_ROOT%\Scripts\BuildAndTest\Documentation_buildLog.txt"
+
 CALL "%OPENTWIN_DEV_ROOT%\Documentation\Doxygen\Generate.bat" > "%OPENTWIN_DEV_ROOT%\Scripts\BuildAndTest\DoxygenDocumentation_buildLog.txt" 2>&1
 
 RENAME "%OPENTWIN_DEV_ROOT%\Documentation\Developer\_build\html\_static\codedochtml\index.xhtml" "code_doc_index.xhtml"
+
+echo Ranamed code documentation index file: %OPENTWIN_DEV_ROOT%\Documentation\Developer\_build\html\_static\codedochtml\code_doc_index.xhtml
+
+REM Get the current timestamp (capture echo call from the batch)
+for /f "delims=" %%a in ('call "%OPENTWIN_DEV_ROOT%\Scripts\Other\PrintCurrentTimestamp.bat" "Finished at: " ""') do set OT_LCL_BuildCodeEnd=%%a
+echo %OT_LCL_BuildCodeEnd%
+echo %OT_LCL_BuildCodeEnd% > "%OPENTWIN_DEV_ROOT%\Scripts\BuildAndTest\Documentation_buildLog.txt"
+
+:PAUSE_END
 
 :END
