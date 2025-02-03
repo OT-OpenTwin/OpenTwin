cc26b1879f469ae1f96c5379a324b5a78a5d728e
JWagner <jan.wagner@fb2.fra-uas.de>
2025-02-03 15:39:05 +0100

    Adjusted the paste functionality on model side for pasting general entities.

--- Services/Model/src/BlockHandler.cpp
+++ Services/Model/src/BlockHandler.cpp
@@ -11,11 +11,41 @@
 
 #pragma warning(disable:4996)
 
-void BlockHandler::updateIdentifier(std::list<std::unique_ptr<EntityBase>>& _newEntities) {
-	//Rename
-	//Set obs and parent zu nullptr
-	//Set name
+void BlockHandler::updateIdentifier(std::list<std::unique_ptr<EntityBase>>& _newEntities) 
+{
+	Model* model = Application::instance()->getModel();
+	OTAssertNullptr(model);
+	std::list<std::string> newEntityNames;
+	for (auto& newEntity : _newEntities)
+	{
+		newEntity->setEntityID(model->createEntityUID());
+		
+		const std::string name = newEntity->getName();
+		const std::string folderName = name.substr(name.find_last_of("/") + 1);
+		std::list<std::string> folderContent = model->getListOfFolderItems(folderName,false);
+		folderContent.insert(folderContent.end(), newEntityNames.begin(), newEntityNames.end()); //Names from the other new entities are not known to the model yet
+		const std::string newName =	CreateNewUniqueTopologyName(folderContent, folderName, name);
+		newEntityNames.push_back(newName);
+		newEntity->setName(newName);
+	}
+}
 
+void BlockHandler::storeEntities(std::list<std::unique_ptr<EntityBase>>& _newEntities)
+{
+	ot::UIDList topoEntID, topoEntVers, dataEntID, dataEntVers, dataEntParent;
+	std::list<bool> forceVis;
+
+	for (auto& newEntity : _newEntities)
+	{
+		newEntity->StoreToDataBase();
+		topoEntID.push_back(newEntity->getEntityID());
+		topoEntVers.push_back(newEntity->getEntityStorageVersion());
+		forceVis.push_back(false);
+	}
+	
+	Model* model = Application::instance()->getModel();
+	OTAssertNullptr(model);
+	model->addEntitiesToModel(topoEntID, topoEntVers, forceVis, dataEntID, dataEntVers, dataEntParent, "Copy+Paste Entities", true,false);
 }
 
 std::string BlockHandler::selectedEntitiesSerialiseAction(ot::JsonDocument& _document) {
@@ -68,9 +98,10 @@ std::string BlockHandler::pasteEntitiesAction(ot::JsonDocument& _document) {
 	std::map<ot::UID, EntityBase*> entityMap;
 
 	// Setup info for graphics paste
-	ot::Point2DD graphicsTopLeft(std::numeric_limits<double>::max(), std::numeric_limits<double>::max());
-	std::list<EntityCoordinates2D*> blockCoordinateEntities;
+	/*ot::Point2DD graphicsTopLeft(std::numeric_limits<double>::max(), std::numeric_limits<double>::max());
+	std::list<EntityCoordinates2D*> blockCoordinateEntities;*/
 
+	std::list<std::unique_ptr<EntityBase>> newEntities;
 	// Deserialize entities
 	for (const ot::CopyEntityInformation& entityInfo : info.getEntities()) {
 		OTAssert(entityInfo.getRawData().empty(), "Empty entity data stored");
@@ -86,55 +117,56 @@ std::string BlockHandler::pasteEntitiesAction(ot::JsonDocument& _document) {
 		std::unique_ptr<EntityBase>entity(classFactory.CreateEntity(entityType));
 
 		// ########################################################################################################################
-
-		// !!!!!   If ithis is a block, the UID of the coordinate entity is pointing to the origin coordinate   !!!!!
 		entity->restoreFromDataBase(nullptr, model, model->getStateManager(), serialisedEntityBSONView, entityMap);
+		newEntities.push_back(std::move(entity));
+		//EntityBlock* blockEntity = dynamic_cast<EntityBlock*>(entity.get());
+		//
+		//if (blockEntity != nullptr) {
+		//	// ########################################################################################################################
 
-		EntityBlock* blockEntity = dynamic_cast<EntityBlock*>(entity.get());
-		
-		if (blockEntity != nullptr) {
-			// ########################################################################################################################
+		//	// !!!!!   note that curently the coordinate entity id of the copy origin is stored   !!!!!
+		//	ot::UID coordinateEntityId = blockEntity->getCoordinateEntityID();
+		//	EntityCoordinates2D* coordinateEntity = dynamic_cast<EntityCoordinates2D*>(model->getEntityByID(coordinateEntityId));
+		//	if (!coordinateEntity) {
+		//		OT_LOG_E("Block entity has an invalid coordinate entity");
+		//		return ot::ReturnMessage(ot::ReturnMessage::Failed).toJson();;
+		//	}
 
-			// !!!!!   note that curently the coordinate entity id of the copy origin is stored   !!!!!
-			ot::UID coordinateEntityId = blockEntity->getCoordinateEntityID();
-			EntityCoordinates2D* coordinateEntity = dynamic_cast<EntityCoordinates2D*>(model->getEntityByID(coordinateEntityId));
-			if (!coordinateEntity) {
-				OT_LOG_E("Block entity has an invalid coordinate entity");
-				return ot::ReturnMessage(ot::ReturnMessage::Failed).toJson();;
-			}
-
-			blockCoordinateEntities.push_back(coordinateEntity);
-			OTAssert(coordinateEntity->getCoordinates().x() != std::numeric_limits<double>::max(), "Invalid x coordinate value stored");
-			OTAssert(coordinateEntity->getCoordinates().y() != std::numeric_limits<double>::max(), "Invalid y coordinate value stored");
-			graphicsTopLeft.moveTop(coordinateEntity->getCoordinates().y()).moveLeft(coordinateEntity->getCoordinates().x());
-		}
+		//	blockCoordinateEntities.push_back(coordinateEntity);
+		//	OTAssert(coordinateEntity->getCoordinates().x() != std::numeric_limits<double>::max(), "Invalid x coordinate value stored");
+		//	OTAssert(coordinateEntity->getCoordinates().y() != std::numeric_limits<double>::max(), "Invalid y coordinate value stored");
+		//	graphicsTopLeft.moveTop(coordinateEntity->getCoordinates().y()).moveLeft(coordinateEntity->getCoordinates().x());
+		//}
 	}
 
-	// Check move distance
-	if (!blockCoordinateEntities.empty()) {
-		// Calculate distance 
-		ot::Point2DD moveAdder(0., 0.);
-		if (info.getDestinationScenePosSet()) {
-			moveAdder = info.getDestinationScenePos() - graphicsTopLeft;
-		}
-		else {
-			// If no scene pos is provided we move all block by 20
-			moveAdder.set(20., 20.);
-		}
+	updateIdentifier(newEntities);
 
-		// Adjust coordinates
-		for (EntityCoordinates2D* coord : blockCoordinateEntities) {
-			// Update coordinate
-			coord->setCoordinates(coord->getCoordinates() + moveAdder);
+	//// Check move distance
+	//if (!blockCoordinateEntities.empty()) {
+	//	// Calculate distance 
+	//	ot::Point2DD moveAdder(0., 0.);
+	//	if (info.getDestinationScenePosSet()) {
+	//		moveAdder = info.getDestinationScenePos() - graphicsTopLeft;
+	//	}
+	//	else {
+	//		// If no scene pos is provided we move all block by 20
+	//		moveAdder.set(20., 20.);
+	//	}
 
-			//coord->StoreToDataBase();
-		}
-	}
+	//	// Adjust coordinates
+	//	for (EntityCoordinates2D* coord : blockCoordinateEntities) {
+	//		// Update coordinate
+	//		coord->setCoordinates(coord->getCoordinates() + moveAdder);
+
+	//		//coord->StoreToDataBase();
+	//	}
+	//}
 
 	//model->addEntitiesToModel(entityIDsTopo, entityVersionsTopo, topoForceVisible, entityIDsData, entityVersionsData, entityIDsTopo, "Copied block entities", true, false);
 
-	ot::JsonDocument responseDocument;
+	/*ot::JsonDocument responseDocument;
 	info.addToJsonObject(responseDocument, responseDocument.GetAllocator());
 
-	return ot::ReturnMessage(ot::ReturnMessage::Ok, responseDocument.toJson()).toJson();
+	return ot::ReturnMessage(ot::ReturnMessage::Ok, responseDocument.toJson()).toJson();*/
+	return ot::ReturnMessage().toJson();
 }
