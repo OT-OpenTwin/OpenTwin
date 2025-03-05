7a7f7c8bed45edc2c7ca37d0ff3676c4f51e5690
Alexander Küster <alexk95@users.noreply.github.com>
2025-03-05 02:45:11 +0100

    Made ContainerHelper a static class

--- Libraries/OTCore/include/OTCore/ContainerHelper.h
+++ Libraries/OTCore/include/OTCore/ContainerHelper.h
@@ -12,7 +12,8 @@
 
 namespace ot {
 
-	namespace ContainerHelper {
+	class ContainerHelper {
+	public:
 		enum DiffMode {
 			MissingLeft, //! @brief Items missing in the left list that exist in the right list.
 			MissingRight, //! @brief Items missing in the right list that exist in the left list.
@@ -24,35 +25,34 @@ namespace ot {
 		//! @tparam V Map value type.
 		//! @param _map Data.
 		//! @return List of values.
-		template <typename K, typename V> std::list<K> listFromMapKeys(const std::map<K, V>& _map);
+		template <typename K, typename V> static std::list<K> listFromMapKeys(const std::map<K, V>& _map);
 
 		//! @brief Creates a list containing all the values from the provided map.
 		//! @tparam K Map key type.
 		//! @tparam V Map value type.
 		//! @param _map Data.
 		//! @return List of values.
-		template <typename K, typename V> std::list<V> listFromMapValues(const std::map<K, V>& _map);
+		template <typename K, typename V> static std::list<V> listFromMapValues(const std::map<K, V>& _map);
 
 		//! @brief Removes the value from the provided map.
 		//! @tparam K Map key type.
 		//! @tparam V Map value type.
 		//! @param _map 
 		//! @param _value 
-		template <class K, class V> void removeFromMapByValue(std::map<K, V>& _map, const V& _value);
+		template <class K, class V> static void removeFromMapByValue(std::map<K, V>& _map, const V& _value);
 
 		//! @brief Returns true if the provided map stores an entry with the given value.
 		//! @tparam K Map key type.
 		//! @tparam V Map value type.
 		//! @param _map 
 		//! @param _value 
-		template <class K, class V> bool hasValue(const std::map<K, V>& _map, const V& _value);
+		template <class K, class V> static bool hasValue(const std::map<K, V>& _map, const V& _value);
 
-		template <typename T> std::vector<T> vectorFromList(const std::list<T>& _list);
-		template <typename T> std::list<T> listFromVector(const std::vector<T>& _vector);
+		template <typename T> static std::vector<T> vectorFromList(const std::list<T>& _list);
+		template <typename T> static std::list<T> listFromVector(const std::vector<T>& _vector);
 
-		template <typename T> std::list<T> createDiff(const std::list<T>& _left, const std::list<T>& _right, DiffMode _diffMode);
-		template <typename T> std::vector<T> createDiff(const std::vector<T>& _left, const std::vector<T>& _right, DiffMode _diffMode);
-
+		template <typename T> static std::list<T> createDiff(const std::list<T>& _left, const std::list<T>& _right, DiffMode _diffMode);
+		template <typename T> static std::vector<T> createDiff(const std::vector<T>& _left, const std::vector<T>& _right, DiffMode _diffMode);
 
 	};	
 }
