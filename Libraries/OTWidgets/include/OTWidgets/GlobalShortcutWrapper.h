797ede40dd67fccd966ee1a2b907517e1200431c
Alexander Küster <alexk95@users.noreply.github.com>
2025-03-06 17:43:56 +0100

    Prepared global shortcut wrapper for discussion

---
+++ Libraries/OTWidgets/include/OTWidgets/GlobalShortcutWrapper.h
@@ -0,0 +1,59 @@
+//! @file GlobalShortcutWrapper.h
+//! @author Alexander Kuester (alexk95)
+//! @date March 2025
+// ###########################################################################################################################################################################################################################################################################################################################
+
+#pragma once
+
+// OpenTwin header
+#include "OTCore/OTClassHelper.h"
+#include "OTWidgets/OTWidgetsAPIExport.h"
+
+// Qt header
+#include <QtCore/qobject.h>
+#include <QtCore/qstring.h>
+#include <QtGui/qkeysequence.h>
+
+// std header
+#include <map>
+
+class QShortcut;
+
+namespace ot {
+
+	class OT_WIDGETS_API_EXPORT GlobalShortcutWrapper : public QObject {
+		Q_OBJECT
+		OT_DECL_NOCOPY(GlobalShortcutWrapper)
+		OT_DECL_NOMOVE(GlobalShortcutWrapper)
+		OT_DECL_NODEFAULT(GlobalShortcutWrapper)
+	public:
+		GlobalShortcutWrapper(const QString& _keySequence);
+		~GlobalShortcutWrapper();
+
+		// ###########################################################################################################################################################################################################################################################################################################################
+
+		// Setter / Getter
+
+		QString keySequenceFromShortcut(const QShortcut* _shortcut) const;
+
+		QShortcut* getShortcut(void) const { return m_shortcut; };
+		const QKeySequence& getKeySequence(void) const { return m_keySequence; };
+
+	Q_SIGNALS:
+		void shortcutActivated();
+		void shortcutActivatedAmbiguously();
+
+	protected:
+		bool eventFilter(QObject* _object, QEvent* _event) override;
+
+	private Q_SLOTS:
+		void slotShortcutActivated(void);
+		void slotShortcutActivatedAmbiguously(void);
+
+	private:
+		QShortcut* m_shortcut;
+		QKeySequence m_keySequence;
+
+	};
+
+}
---
+++ Libraries/OTWidgets/src/GlobalShortcutWrapper.cpp
@@ -0,0 +1,75 @@
+//! @file GlobalShortcutWrapper.cpp
+//! @author Alexander Kuester (alexk95)
+//! @date March 2025
+// ###########################################################################################################################################################################################################################################################################################################################
+
+// OpenTwin header
+#include "OTCore/Logger.h"
+#include "OTWidgets/GlobalShortcutWrapper.h"
+
+// Qt header
+#include <QtGui/qevent.h>
+#include <QtWidgets/qwidget.h>
+#include <QtWidgets/qshortcut.h>
+#include <QtWidgets/qapplication.h>
+
+ot::GlobalShortcutWrapper::GlobalShortcutWrapper(const QString& _keySequence) :
+	QObject(QApplication::instance()), m_keySequence(_keySequence)
+{
+	QObject* par = this->parent();
+	OTAssertNullptr(par);
+	par->installEventFilter(this);
+
+	OTAssert(!_keySequence.isEmpty(), "Empty key sequence set");
+	m_shortcut = new QShortcut(m_keySequence, par);
+	
+	m_shortcut->setContext(Qt::ApplicationShortcut);
+
+	this->connect(m_shortcut, &QShortcut::activated, this, &GlobalShortcutWrapper::slotShortcutActivated);
+	this->connect(m_shortcut, &QShortcut::activatedAmbiguously, this, &GlobalShortcutWrapper::slotShortcutActivatedAmbiguously);
+
+}
+
+ot::GlobalShortcutWrapper::~GlobalShortcutWrapper() {
+	OTAssertNullptr(m_shortcut);
+
+	this->disconnect(m_shortcut, &QShortcut::activated, this, &GlobalShortcutWrapper::slotShortcutActivated);
+	this->disconnect(m_shortcut, &QShortcut::activatedAmbiguously, this, &GlobalShortcutWrapper::slotShortcutActivatedAmbiguously);
+	delete m_shortcut;
+}
+
+// ###########################################################################################################################################################################################################################################################################################################################
+
+// Setter / Getter
+
+
+QString ot::GlobalShortcutWrapper::keySequenceFromShortcut(const QShortcut* _shortcut) const {
+	QString str;
+	for (const QKeySequence& seq : _shortcut->keys()) {
+		if (!str.isEmpty()) {
+			str.push_back('+');
+		}
+
+		str.append(seq.toString());
+	}
+	return str;
+}
+
+// ###########################################################################################################################################################################################################################################################################################################################
+
+// Private slots
+
+void ot::GlobalShortcutWrapper::slotShortcutActivated(void) {
+	Q_EMIT shortcutActivated();
+}
+
+void ot::GlobalShortcutWrapper::slotShortcutActivatedAmbiguously(void) {
+	Q_EMIT shortcutActivatedAmbiguously();
+}
+
+bool ot::GlobalShortcutWrapper::eventFilter(QObject* _object, QEvent* _event) {
+	if (_event->type() == QEvent::KeyPress) {
+		
+	}
+	return QObject::eventFilter(_object, _event);
+}
