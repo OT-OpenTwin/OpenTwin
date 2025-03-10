e861e3fb485c497b0af90f9d60b75ed77a1a8d4c
JWagner <jan.wagner@fb2.fra-uas.de>
2025-03-10 19:32:31 +0100

    Fixed warning about not supported character

--- Libraries/Viewer/src/CurveVisualiser.cpp
+++ Libraries/Viewer/src/CurveVisualiser.cpp
@@ -20,42 +20,4 @@ void CurveVisualiser::visualise(bool _setFocus) {
 	doc.AddMember(OT_ACTION_PARAM_VIEW_SetActiveView, _setFocus, doc.GetAllocator());
 
 	FrontendAPI::instance()->messageModelService(doc.toJson());
-
-	//BsonViewOrValue m_query;
-	//BsonViewOrValue m_projection;
-	//int m_limit;
-	//ot::Plot1DCfg curveConfig;
-
-	//const std::string _xAxisEntryName;
-	//const std::string _yAxisEntryName;
-
-	//DataStorageAPI::ResultDataStorageAPI access("collection");
-	//DataStorageAPI::DataStorageResponse response = access.SearchInResultCollection(m_query, m_projection, m_limit);
-	//auto queryResult =	response.getBsonResult();
-	//size_t numberOfEntries = queryResult.value().length();
-	//std::vector<double> xAxisValues, yAxisValues;
-	//xAxisValues.reserve(numberOfEntries);
-	//yAxisValues.reserve(numberOfEntries);
-
-	////Hier gehe ich davon aus, dass keine Kurvenschar zurückgegeben wird.
-	//if (queryResult.has_value())
-	//{
-	//	for (auto& entry : queryResult.value())
-	//	{
-	//		xAxisValues.push_back(entry[_xAxisEntryName].get_double().value);
-	//		yAxisValues.push_back(entry[_yAxisEntryName].get_double().value);
-	//	}
-	//}
-	//
-	//ot::Plot* ownerPlot;
-	//QString title = "";
-	//int id = 0;
-	//ot::PlotDataset m_dataset(ownerPlot,id,title , xAxisValues.data(), yAxisValues.data(), numberOfEntries);
-	//ownerPlot->addDataset()
-
-	//ot::Plot plot;
-	//plot.setConfig(curveConfig); 
-
-
-
 }
