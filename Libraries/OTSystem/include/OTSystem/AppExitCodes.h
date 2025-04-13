dd1d92a1845c4bd0512ee6f09eb051b334badbb6
Alexander Küster <alexk95@users.noreply.github.com>
2025-04-13 16:37:39 +0200

    Added application exit codes header

---
+++ Libraries/OTSystem/include/OTSystem/AppExitCodes.h
@@ -0,0 +1,134 @@
+//! @file AppExitCodes.h
+//! @author Alexander Kuester (alexk95)
+//! @date April 2025
+// ###########################################################################################################################################################################################################################################################################################################################
+
+#pragma once
+
+// OpenTwin header
+#include "OTSystem/SystemAPIExport.h"
+
+// std header
+#include <string>
+
+namespace ot {
+
+	//! @brief Exit codes used by OpenTwin applications.
+	class OT_SYS_API_EXPORT AppExitCode {
+	public:
+		// ###########################################################################################################################################################################################################################################################################################################################
+
+		// Framework
+
+		//! @brief Framework exit codes (100 - 999).
+		//! Exit codes used by the OpenTwin framework.
+		//! These codes are used to indicate specific errors or conditions that occur within the framework itself.
+		//! They are not intended for use by individual applications or modules.
+		enum FrameworkExitCode : int {
+			GeneralFrameworkError      = 100
+		};
+
+		// ###########################################################################################################################################################################################################################################################################################################################
+
+		// IO
+
+		//! @brief IO exit codes (1000 - 1099).
+		enum IOExitCode : int {
+			GeneralIOError             = 1000,
+			FileNotFound               = 1001,
+			InvalidFileFormat          = 1002,
+			PermissionDenied           = 1003,
+			OutOfMemory                = 1004,
+			InvalidPath                = 1005,
+			SendFailed                 = 1006
+		};
+
+		// ###########################################################################################################################################################################################################################################################################################################################
+
+		// Service Data
+
+		//! @brief Service data exit codes (1100 - 1199).
+		//! Exit codes generated related to service data conflicts/errors.
+		enum ServiceDataExitCode : int {
+			GeneralServiceDataError    = 1100,
+			ServiceUrlMissing          = 1101,
+			ServiceUrlInvalid          = 1102,
+
+			AuthUrlMissing             = 1111,
+			AuthUrlInvalid             = 1112,
+			GSSUrlMissing              = 1121,
+			GSSUrlInvalid              = 1122,
+			LSSUrlMissing              = 1131,
+			LSSUrlInvalid              = 1132,
+			GDSUrlMissing              = 1141,
+			GDSUrlInvalid              = 1142,
+			LDSUrlMissing              = 1151,
+			LDSUrlInvalid              = 1152
+
+		};
+		
+		// ###########################################################################################################################################################################################################################################################################################################################
+
+		// Service State
+
+		//! @brief Service state exit codes (1200 - 1299).
+		//! Exit codes generated related to service state errors.
+		enum ServiceStateExitCode : int {
+			GeneralServiceStateError   = 1200,
+			AuthRegistrationFailed     = 1201,
+			GSSRegistrationFailed      = 1202,
+			LSSRegistrationFailed      = 1203,
+			GDSRegistrationFailed      = 1204,
+			LDSRegistrationFailed      = 1205,
+
+			AuthNotRunning             = 1206,
+			GSSNotRunning              = 1207,
+			LSSNotRunning              = 1208,
+			GDSNotRunning              = 1209,
+			LDSNotRunning              = 1210
+		};
+
+		// ###########################################################################################################################################################################################################################################################################################################################
+
+		// Data Base
+
+		//! @brief Data Base related exit codes (1800 - 1899).
+		enum DataBaseExitCode : int {
+			GeneralDataBaseError       = 1800,
+			DataBaseUrlMissing         = 1801,
+			DataBaseUrlInvalid         = 1802,
+			DataBaseConnectionFailed   = 1803,
+			DataBaseInvalidCredentials = 1804
+		};
+
+		// ###########################################################################################################################################################################################################################################################################################################################
+
+		// General
+
+		//! @brief General exit codes (7000+).
+		enum GeneralExitCode : int {
+			//! @brief General success exit code (0).
+			Success                    = 0,
+
+			GeneralError               = 7700,
+			UnknownError               = 7701,
+			EmergencyShutdown          = 7702,
+			EnvironmentError           = 7703,
+			ConfigurationBroken        = 7704,
+			OpenGLError                = 7705,
+			AssetError                 = 7706,
+			ComponentError             = 7707,
+
+			LogInError                 = 7800,
+			FailedToConvertPort        = 7801,
+			KeepAliveFailed            = 7802
+		};
+
+		//! @brief Returns a humanly readable string representation of the given exit code.
+		static std::string toString(int _exitCode);
+
+	private:
+		AppExitCode() = delete;
+	};
+
+}
---
+++ Libraries/OTSystem/src/AppExitCodes.cpp
@@ -0,0 +1,101 @@
+//! @file AppExitCodes.cpp
+//! @author Alexander Kuester (alexk95)
+//! @date April 2025
+// ###########################################################################################################################################################################################################################################################################################################################
+
+// OpenTwin header
+#include "OTSystem/AppExitCodes.h"
+
+// std header
+#include <cassert>
+
+std::string ot::AppExitCode::toString(int _exitCode) {
+	switch (_exitCode) {
+	case GeneralExitCode::Success: return "Success";
+
+	// ###########################################################################################################################################################################################################################################################################################################################
+
+	// Framework
+
+	case FrameworkExitCode::GeneralFrameworkError: return "General framework error";
+
+	// ###########################################################################################################################################################################################################################################################################################################################
+
+	// IO
+
+	case IOExitCode::GeneralIOError: return "General IO error";
+	case IOExitCode::FileNotFound: return "File not found";
+	case IOExitCode::InvalidFileFormat: return "Invalid file format";
+	case IOExitCode::PermissionDenied: return "Permission denied";
+	case IOExitCode::OutOfMemory: return "Out of memory";
+	case IOExitCode::InvalidPath: return "Invalid path";
+	case IOExitCode::SendFailed: return "Send failed";
+
+	// ###########################################################################################################################################################################################################################################################################################################################
+
+	// Service Data
+
+	case ServiceDataExitCode::GeneralServiceDataError: return "General service data error";
+	case ServiceDataExitCode::ServiceUrlMissing: return "Service URL is missing";
+	case ServiceDataExitCode::ServiceUrlInvalid: return "Service URL is invalid";
+
+	case ServiceDataExitCode::AuthUrlMissing: return "Authentication URL is missing";
+	case ServiceDataExitCode::AuthUrlInvalid: return "Authentication URL is invalid";
+	case ServiceDataExitCode::GSSUrlMissing: return "Global Session Service URL is missing";
+	case ServiceDataExitCode::GSSUrlInvalid: return "Global Session Service URL is invalid";
+	case ServiceDataExitCode::LSSUrlMissing: return "Local Session Service URL is missing";
+	case ServiceDataExitCode::LSSUrlInvalid: return "Local Session Service URL is invalid";
+	case ServiceDataExitCode::GDSUrlMissing: return "Global Directory Service URL is missing";
+	case ServiceDataExitCode::GDSUrlInvalid: return "Global Directory Service URL is invalid";
+	case ServiceDataExitCode::LDSUrlMissing: return "Local Directory Service URL is missing";
+	case ServiceDataExitCode::LDSUrlInvalid: return "Local Directory Service URL is invalid";
+
+	// ###########################################################################################################################################################################################################################################################################################################################
+
+	// Service State
+
+	case ServiceStateExitCode::GeneralServiceStateError: return "General service state error";
+	case ServiceStateExitCode::AuthRegistrationFailed: return "Authentication Service registration failed";
+	case ServiceStateExitCode::GSSRegistrationFailed: return "Global Session Service registration failed";
+	case ServiceStateExitCode::LSSRegistrationFailed: return "Local Session Service registration failed";
+	case ServiceStateExitCode::GDSRegistrationFailed: return "Global Directory Service registration failed";
+	case ServiceStateExitCode::LDSRegistrationFailed: return "Local Directory Service registration failed";
+
+	case ServiceStateExitCode::AuthNotRunning: return "Authentication Service is not running";
+	case ServiceStateExitCode::GSSNotRunning: return "Global Session Service is not running";
+	case ServiceStateExitCode::LSSNotRunning: return "Local Session Service is not running";
+	case ServiceStateExitCode::GDSNotRunning: return "Global Directory Service is not running";
+	case ServiceStateExitCode::LDSNotRunning: return "Local Directory Service is not running";
+
+	// ###########################################################################################################################################################################################################################################################################################################################
+
+	// Data Base
+
+	case DataBaseExitCode::GeneralDataBaseError: return "General database error";
+	case DataBaseExitCode::DataBaseUrlMissing: return "Database URL is missing";
+	case DataBaseExitCode::DataBaseUrlInvalid: return "Database URL is invalid";
+	case DataBaseExitCode::DataBaseConnectionFailed: return "Database connection failed";
+	case DataBaseExitCode::DataBaseInvalidCredentials: return "Invalid database credentials";
+
+	// ###########################################################################################################################################################################################################################################################################################################################
+
+	// General
+
+	case GeneralExitCode::GeneralError: return "General error";
+	case GeneralExitCode::UnknownError: return "Unknown error";
+	case GeneralExitCode::EmergencyShutdown: return "Emergency shutdown";
+	case GeneralExitCode::EnvironmentError: return "Environment error";
+	case GeneralExitCode::ConfigurationBroken: return "Configuration is broken";
+	case GeneralExitCode::OpenGLError: return "OpenGL error";
+	case GeneralExitCode::AssetError: return "Asset error";
+	case GeneralExitCode::ComponentError: return "Component error";
+
+	case GeneralExitCode::LogInError: return "Log in error";
+	case GeneralExitCode::FailedToConvertPort: return "Failed to convert port";
+	case GeneralExitCode::KeepAliveFailed: return "Keep alive failed";
+
+	default:
+		assert(0);
+		return "<Unknown exit code>";
+	}
+}
