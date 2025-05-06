1d95c48c0218993b3829f13438db7e42e87e90d4
Alexander Küster <alexk95@users.noreply.github.com>
2025-05-06 10:27:08 +0200

    Added closeable as default to plot config and added missing initializer

--- Libraries/OTGui/src/Plot1DCfg.cpp
+++ Libraries/OTGui/src/Plot1DCfg.cpp
@@ -56,12 +56,12 @@ ot::Plot1DCfg::AxisQuantity ot::Plot1DCfg::stringToAxisQuantity(const std::strin
 // ###########################################################################################################################################################################################################################################################################################################################
 
 ot::Plot1DCfg::Plot1DCfg() : 
-	WidgetViewBase(WidgetViewBase::View1D), m_type(Plot1DCfg::Cartesian), m_axisQuantity(Plot1DCfg::Real),
-	m_gridVisible(true), m_gridWidth(1.), m_isHidden(false), m_legendVisible(true)
+	WidgetViewBase(WidgetViewBase::View1D, WidgetViewBase::ViewIsCentral | WidgetViewBase::ViewIsCloseable), m_type(Plot1DCfg::Cartesian), m_axisQuantity(Plot1DCfg::Real),
+	m_gridVisible(true), m_gridWidth(1.), m_isHidden(false), m_legendVisible(true), m_curveLimit(0), m_useLimit(false)
 {}
 
 ot::Plot1DCfg::Plot1DCfg(const Plot1DCfg& _other)
-	: WidgetViewBase(WidgetViewBase::View1D)
+	: WidgetViewBase(WidgetViewBase::View1D, _other.getViewFlags())
 {
 	*this = _other;
 }
