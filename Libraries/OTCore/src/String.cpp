b67e44fbaafc09fc5b53f5a208112ea5ecaf8b48
Alexander Küster <alexk95@users.noreply.github.com>
2025-06-15 12:22:00 +0200

    Added toUpper method to String helper class

--- Libraries/OTCore/src/String.cpp
+++ Libraries/OTCore/src/String.cpp
@@ -54,10 +54,18 @@ std::string ot::String::toLower(const std::string& _string)
 	return result;
 }
 
+std::string ot::String::toUpper(const std::string& _string) {
+	std::string result = _string;
+	std::transform(result.begin(), result.end(), result.begin(),
+		[](unsigned char c) { return std::toupper(c); });
+
+	return result;
+}
+
 size_t ot::String::findOccurance(const std::string& _string, char _character, int _occurance) {
 	const size_t size = _string.size();
 	size_t count = 0;
-	for (size_t i = 0; i < size; ++i) {
+	for (size_t i = 0; i < size; i++) {
 		if (_string[i] == _character) {
 			if (++count == _occurance) {
 				return i;
@@ -65,80 +73,6 @@ size_t ot::String::findOccurance(const std::string& _string, char _character, in
 		}
 	}
 	return std::string::npos;
-}
-
-std::string ot::String::evaluateEscapeCharacters(const std::string& _string) {
-	std::string result;
-	size_t len = _string.length() - 1;
-	result.reserve(len + 1);
-
-	for (size_t ix = 0; ix < (len + 1); ix++) {
-		if (_string[ix] == '\\') {
-			if (ix == len) {
-				result.push_back(_string[ix]);
-				return result;
-			}
-			char type = _string[ix + 1];
-			switch (type) {
-			case 'n': 
-				result.push_back('\n');
-				ix++;
-				break;
-
-			case 't':
-				result.push_back('\t');
-				ix++;
-				break;
-
-			case 'r':
-				result.push_back('\r');
-				ix++;
-				break;
-
-			case '\\':
-				result.push_back('\\');
-				ix++;
-				break;
-
-			case '\"':
-				result.push_back('\"');
-				ix++;
-				break;
-
-			case '\'':
-				result.push_back('\'');
-				ix++;
-				break;
-
-			case 'b':
-				result.push_back('\b');
-				ix++;
-				break;
-
-			case '0':
-				result.push_back('\0');
-				// Since a string is always 0 terminated, we return.
-				return result;
-
-			case 'f':
-				result.push_back('\f');
-				ix++;
-				break;
-
-			default:
-			{
-				char typeStr[] = { type, '\0' };
-				OT_LOG_EAS("Unknown escape character \"" + std::string(typeStr) + "\"");
-			}
-				break;
-			}
-		}
-		else {
-			result.push_back(_string[ix]);
-		}
-	}
-
-	return result;
 }
 
 std::list<std::string> ot::String::split(const std::string& _str, char _splitBy, bool _skipEmpty) {
@@ -215,142 +149,6 @@ std::list<std::wstring> ot::String::split(const std::wstring& _str, const std::w
 		ret.push_back(std::move(temp));
 	}
 	return ret;
-}
-
-std::list<std::string> ot::String::smartSplit(const std::string& _str, const std::string& _splitBy, bool _evaluateEscapeCharacters, bool _skipEmpty) {
-	std::list<std::string> result;
-	// Avoid empty strings
-	if (_str.empty()) {
-		return result;
-	}
-	// Avoid not required split
-	if (_splitBy.empty()) {
-		result.push_back(_str);
-		return result;
-	}
-
-	std::string currentSegment;
-	bool insideQuotes = false;
-	bool isEscape = false;
-	bool evaluateEscape = (_splitBy[0] != '\\');
-	if (!_evaluateEscapeCharacters) {
-		evaluateEscape = false;
-	}
-	bool evaluateQuotation = (_splitBy[0] != '\"');
-
-	size_t splitLen = _splitBy.length();
-	size_t i = 0;
-	while (i < _str.length()) {
-		// Check for escape character
-		if (evaluateEscape) {
-			if (_str[i] == '\\') {
-				i++;
-				if (isEscape) {
-					currentSegment.push_back('\\');
-					isEscape = false;
-					continue;
-				}
-				else {
-					isEscape = true;
-					continue;
-				}
-			}
-			else if (isEscape) {
-				char type = _str[i];
-				switch (type) {
-				case 'n': currentSegment.push_back('\n'); break;
-				case 't': currentSegment.push_back('\t'); break;
-				case 'r': currentSegment.push_back('\r'); break;
-				case 'b': currentSegment.push_back('\b'); break;
-				case 'f': currentSegment.push_back('\f'); break;
-				case '\\': currentSegment.push_back('\\'); break;
-				case '\"': currentSegment.push_back('\"'); break;
-				case '\'': currentSegment.push_back('\''); break;
-				case '0':
-					// Since a string is always 0 terminated, we return.
-					if (!currentSegment.empty() || !_skipEmpty) {
-						result.push_back(std::move(currentSegment));
-					}
-					return result;
-
-				default:
-				{
-					char typeStr[] = { type, '\0' };
-					OT_LOG_EAS("Unknown escape character \"" + std::string(typeStr) + "\"");
-				}
-					break;
-				}
-
-				isEscape = false;
-				i++;
-				continue;
-			}
-		}
-
-		// Check for quote characters
-		if (evaluateQuotation) {
-			if ((_str[i] == '"')) {
-				i++;
-				// Closing quote
-				if (insideQuotes) {
-					insideQuotes = false;
-					currentSegment.push_back('\"');
-					continue;
-				}
-				// Opening quote
-				else {
-					insideQuotes = true;
-					currentSegment.push_back('\"');
-					continue;
-				}
-			}
-		}
-
-		// Check for the delimiter
-		if (!insideQuotes && _str.find(_splitBy, i) == i) {
-			if (!currentSegment.empty() || !_skipEmpty) {
-				result.push_back(std::move(currentSegment));
-			}
-			currentSegment.clear();
-			i += splitLen;
-		}
-		else {
-			currentSegment += _str[i];
-			i++;
-		}
-	}
-
-	if (isEscape) {
-		OT_LOG_EA("Escape character specifier missing");
-		currentSegment.push_back('\\');
-	}
-
-	if (!currentSegment.empty() || !_skipEmpty) {
-		result.push_back(std::move(currentSegment));
-	}
-
-	if (insideQuotes) {
-		OT_LOG_WA("Closing quote missing");
-	}
-
-	return result;
-}
-
-std::string ot::String::addEscapeCharacters(const std::string& _str) {
-	std::string result = _str;
-
-	const std::map<std::string, std::string> replacementData({
-		{ "\t", "\\t" },
-		{ "\\\\t", "\\t" },
-		{ "\n", "\\n" },
-		{ "\\\\n", "\\n" }
-		});
-
-	for (const auto& it : replacementData) {
-		String::replaced(result, it.first, it.second);
-	}
-
-	return result;
 }
 
 void ot::String::replaced(std::string& _str, const std::string& _what, const std::string& _with) {
@@ -534,4 +332,214 @@ wchar_t* ot::String::getCStringCopy(const std::wstring& _str) {
 	wchar_t* retval = new wchar_t[len];
 	wcscpy_s(retval, len, _str.c_str());
 	return retval;
+}
+
+std::string ot::String::evaluateEscapeCharacters(const std::string& _string) {
+	std::string result;
+	size_t len = _string.length() - 1;
+	result.reserve(len + 1);
+
+	for (size_t ix = 0; ix < (len + 1); ix++) {
+		if (_string[ix] == '\\') {
+			if (ix == len) {
+				result.push_back(_string[ix]);
+				return result;
+			}
+			char type = _string[ix + 1];
+			switch (type) {
+			case 'n':
+				result.push_back('\n');
+				ix++;
+				break;
+
+			case 't':
+				result.push_back('\t');
+				ix++;
+				break;
+
+			case 'r':
+				result.push_back('\r');
+				ix++;
+				break;
+
+			case '\\':
+				result.push_back('\\');
+				ix++;
+				break;
+
+			case '\"':
+				result.push_back('\"');
+				ix++;
+				break;
+
+			case '\'':
+				result.push_back('\'');
+				ix++;
+				break;
+
+			case 'b':
+				result.push_back('\b');
+				ix++;
+				break;
+
+			case '0':
+				result.push_back('\0');
+				// Since a string is always 0 terminated, we return.
+				return result;
+
+			case 'f':
+				result.push_back('\f');
+				ix++;
+				break;
+
+			default:
+			{
+				char typeStr[] = { type, '\0' };
+				OT_LOG_EAS("Unknown escape character \"" + std::string(typeStr) + "\"");
+			}
+			break;
+			}
+		}
+		else {
+			result.push_back(_string[ix]);
+		}
+	}
+
+	return result;
+}
+
+std::list<std::string> ot::String::smartSplit(const std::string& _str, const std::string& _splitBy, bool _evaluateEscapeCharacters, bool _skipEmpty) {
+	std::list<std::string> result;
+	// Avoid empty strings
+	if (_str.empty()) {
+		return result;
+	}
+	// Avoid not required split
+	if (_splitBy.empty()) {
+		result.push_back(_str);
+		return result;
+	}
+
+	std::string currentSegment;
+	bool insideQuotes = false;
+	bool isEscape = false;
+	bool evaluateEscape = (_splitBy[0] != '\\');
+	if (!_evaluateEscapeCharacters) {
+		evaluateEscape = false;
+	}
+	bool evaluateQuotation = (_splitBy[0] != '\"');
+
+	size_t splitLen = _splitBy.length();
+	size_t i = 0;
+	while (i < _str.length()) {
+		// Check for escape character
+		if (evaluateEscape) {
+			if (_str[i] == '\\') {
+				i++;
+				if (isEscape) {
+					currentSegment.push_back('\\');
+					isEscape = false;
+					continue;
+				}
+				else {
+					isEscape = true;
+					continue;
+				}
+			}
+			else if (isEscape) {
+				char type = _str[i];
+				switch (type) {
+				case 'n': currentSegment.push_back('\n'); break;
+				case 't': currentSegment.push_back('\t'); break;
+				case 'r': currentSegment.push_back('\r'); break;
+				case 'b': currentSegment.push_back('\b'); break;
+				case 'f': currentSegment.push_back('\f'); break;
+				case '\\': currentSegment.push_back('\\'); break;
+				case '\"': currentSegment.push_back('\"'); break;
+				case '\'': currentSegment.push_back('\''); break;
+				case '0':
+					// Since a string is always 0 terminated, we return.
+					if (!currentSegment.empty() || !_skipEmpty) {
+						result.push_back(std::move(currentSegment));
+					}
+					return result;
+
+				default:
+				{
+					char typeStr[] = { type, '\0' };
+					OT_LOG_EAS("Unknown escape character \"" + std::string(typeStr) + "\"");
+				}
+				break;
+				}
+
+				isEscape = false;
+				i++;
+				continue;
+			}
+		}
+
+		// Check for quote characters
+		if (evaluateQuotation) {
+			if ((_str[i] == '"')) {
+				i++;
+				// Closing quote
+				if (insideQuotes) {
+					insideQuotes = false;
+					currentSegment.push_back('\"');
+					continue;
+				}
+				// Opening quote
+				else {
+					insideQuotes = true;
+					currentSegment.push_back('\"');
+					continue;
+				}
+			}
+		}
+
+		// Check for the delimiter
+		if (!insideQuotes && _str.find(_splitBy, i) == i) {
+			if (!currentSegment.empty() || !_skipEmpty) {
+				result.push_back(std::move(currentSegment));
+			}
+			currentSegment.clear();
+			i += splitLen;
+		}
+		else {
+			currentSegment += _str[i];
+			i++;
+		}
+	}
+
+	if (isEscape) {
+		OT_LOG_EA("Escape character specifier missing");
+		currentSegment.push_back('\\');
+	}
+
+	if (!currentSegment.empty() || !_skipEmpty) {
+		result.push_back(std::move(currentSegment));
+	}
+
+	if (insideQuotes) {
+		OT_LOG_WA("Closing quote missing");
+	}
+
+	return result;
+}
+
+std::string ot::String::addEscapeCharacters(const std::string& _str) {
+	std::string result = _str;
+
+	const std::map<std::string, std::string> replacementData({
+		{ "\t", "\\t" },
+		{ "\\\\t", "\\t" },
+		{ "\n", "\\n" },
+		{ "\\\\n", "\\n" }
+		});
+
+	for (const auto& it : replacementData) {
+		String::replaced(result, it.first, it.second);
+	}
+
+	return result;
 }
