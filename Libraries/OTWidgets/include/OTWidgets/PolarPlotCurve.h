933118dd91bde9d5ec6d63cbcd600ea5e0c30c04
Alexander Küster <alexk95@users.noreply.github.com>
2025-06-26 01:34:57 +0200

    Added polar plot curve class

---
+++ Libraries/OTWidgets/include/OTWidgets/PolarPlotCurve.h
@@ -0,0 +1,43 @@
+//! @file PolarPlotCurve.h
+//! @author Alexander Kuester (alexk95)
+//! @date June 2025
+// ###########################################################################################################################################################################################################################################################################################################################
+
+#pragma once
+
+// OpenTwin header
+#include "OTWidgets/OTWidgetsAPIExport.h"
+
+// Qwt header
+#include <qwt_polar_curve.h>
+
+// Qt header
+#include <QtGui/qpen.h>
+
+namespace ot {
+
+	class OT_WIDGETS_API_EXPORT PolarPlotCurve : public QwtPolarCurve {
+	public:
+
+		// ###########################################################################################################################################################################################################################################################################################################################
+
+		// Constructor
+
+		PolarPlotCurve(const QString& _title = QString());
+
+		// ###########################################################################################################################################################################################################################################################################################################################
+
+		// Setter / Getter
+
+		void setOutlinePen(const QPen& _pen) { m_outlinePen = _pen; };
+		const QPen& getOutlinePen() const { return m_outlinePen; };
+
+		void setPointInterval(int _interval) { m_pointInterval = _interval; };
+		int getPointInterval() const { return m_pointInterval; };
+
+	private:
+		QPen m_outlinePen;
+		int m_pointInterval;
+	};
+
+}
---
+++ Libraries/OTWidgets/src/PolarPlotCurve.cpp
@@ -0,0 +1,13 @@
+//! @file PolarPlotCurve.cpp
+//! @author Alexander Kuester (alexk95)
+//! @date June 2025
+// ###########################################################################################################################################################################################################################################################################################################################
+
+// OpenTwin header
+#include "OTWidgets/PolarPlotCurve.h"
+
+ot::PolarPlotCurve::PolarPlotCurve(const QString& _title) :
+	QwtPolarCurve(_title), m_outlinePen(Qt::NoPen), m_pointInterval(1)
+{
+
+}
