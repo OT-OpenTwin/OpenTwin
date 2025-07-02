4f39ace077ada1d4c55d43e2513c9ce1e623dc94
Alexander Küster <alexk95@users.noreply.github.com>
2025-07-02 14:30:34 +0200

    Minor cleanup in StateStack

--- Libraries/OTCore/include/OTCore/StateStack.h
+++ Libraries/OTCore/include/OTCore/StateStack.h
@@ -14,32 +14,30 @@
 namespace ot {
 
 	template <typename T> class StateStack {
+		OT_DECL_NODEFAULT(StateStack)
+		OT_DECL_DEFCOPY(StateStack)
+		OT_DECL_DEFMOVE(StateStack)
 	public:
 		StateStack(const T& _initialValue);
-		StateStack(const StateStack&) = default;
-		StateStack(StateStack&&) = default;
-		virtual ~StateStack() = default;
+		~StateStack() = default;
 
-		StateStack& operator = (const StateStack&) = default;
-		StateStack& operator = (StateStack&&) = default;
+		T* operator -> ();
+		T* operator *();
+		const T* operator *() const;
 
-		T* operator -> (void);
-		T* operator *(void);
-		const T* operator *(void) const;
-
-		T& getCurrent(void);
-		const T& getCurrent(void) const;
+		T& getCurrent();
+		const T& getCurrent() const;
 
 		//! @brief Adds a stack entry.
 		//! The added value is a copy of the current value.
-		inline void push(void);
+		inline void push();
 
 		//! @brief Adds the provided value to the stack.
 		inline void push(const T& _value);
 
 		//! @brief Removes the current entry from the stack.
 		//! The default "bottom" entry should not be removed.
-		inline void pop(void);
+		inline void pop();
 
 	private:
 		std::stack<T> m_stack;
--- Libraries/OTCore/include/OTCore/StateStack.hpp
+++ Libraries/OTCore/include/OTCore/StateStack.hpp
@@ -16,35 +16,35 @@ inline ot::StateStack<T>::StateStack(const T& _initialValue) {
 }
 
 template<typename T>
-inline T* ot::StateStack<T>::operator->(void) {
+inline T* ot::StateStack<T>::operator->() {
 	OTAssert(!m_stack.empty(), "Stack is empty");
 	return &m_stack.top();
 }
 
 template<typename T>
-inline T* ot::StateStack<T>::operator*(void) {
+inline T* ot::StateStack<T>::operator*() {
 	OTAssert(!m_stack.empty(), "Stack is empty");
 	return &m_stack.top();
 }
 
 template<typename T>
-inline const T* ot::StateStack<T>::operator*(void) const {
+inline const T* ot::StateStack<T>::operator*() const {
 	OTAssert(!m_stack.empty(), "Stack is empty");
 	return &m_stack.top();
 }
 
 template<typename T>
-inline T& ot::StateStack<T>::getCurrent(void) {
+inline T& ot::StateStack<T>::getCurrent() {
 	return m_stack.top();
 }
 
 template<typename T>
-inline const T& ot::StateStack<T>::getCurrent(void) const {
+inline const T& ot::StateStack<T>::getCurrent() const {
 	return m_stack.top();
 }
 
 template <typename T>
-inline void ot::StateStack<T>::push(void) {
+inline void ot::StateStack<T>::push() {
 	m_stack.push(m_stack.top());
 }
 
@@ -54,7 +54,7 @@ inline void ot::StateStack<T>::push(const T& _value) {
 }
 
 template <typename T>
-inline void ot::StateStack<T>::pop(void) {
+inline void ot::StateStack<T>::pop() {
 	m_stack.pop();
 	OTAssert(!m_stack.empty(), "Default entry was removed");
 }
