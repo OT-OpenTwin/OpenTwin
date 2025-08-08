e3afb9f3998e53aac47f18094bc10a1ff150b9d9
JWagner <jan.wagner@fb2.fra-uas.de>
2025-08-08 15:07:14 +0200

    Adjusted script

--- Testdata/PythonScripts/TestScript_UpdateEntity.py
+++ Testdata/PythonScripts/TestScript_UpdateEntity.py
@@ -1,7 +1,8 @@
+#@ Port : {"type": "out", "name": "Dummy", "label" : "Dummy"}
 import OpenTwin
 
 def __main__(x):
-    factor = OpenTwin.GetPropertyValue("Materials/material1","Permittivity (relative)")
-    newValue = x * factor
-    setSuccess = OpenTwin.SetPropertyValue("Materials/material1","Permittivity (relative)",newValue)
-    flushSuccess = OpenTwin.FlushEntity("Materials/material1")
+    factor = OpenTwin.GetPropertyValue("Units","Time")
+    newValue = "ms"
+    setSuccess = OpenTwin.SetPropertyValue("Units","Time",newValue)
+    flushSuccess = OpenTwin.FlushEntity("Units")
