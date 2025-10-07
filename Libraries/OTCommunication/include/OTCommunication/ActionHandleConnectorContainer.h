937a4957d4cbc75ee107eae8a31fb6b1ae25cde1
Alexander Küster <alexk95@users.noreply.github.com>
2025-10-07 02:14:47 +0200

    Added ActionHandlerConnectorContainer helper

---
+++ Libraries/OTCommunication/include/OTCommunication/ActionHandleConnectorContainer.h
@@ -0,0 +1,42 @@
+//! @file ActionHandleConnectorContainer.h
+//! @author Alexander Kuester (alexk95)
+//! @date October 2025
+// ###########################################################################################################################################################################################################################################################################################################################
+
+#pragma once
+
+// OpenTwin header
+#include "OTCommunication/ActionHandleConnector.h"
+
+// std header
+#include <list>
+
+namespace ot {
+
+	//! @brief The ActionHandleConnectorContainer class is a simple container to store ActionHandleConnector instances and to ensure that they are removed from the dispatcher when the container is destroyed.
+	class OT_COMMUNICATION_API_EXPORT ActionHandleConnectorContainer {
+		OT_DECL_NOCOPY(ActionHandleConnectorContainer)
+	public:
+		ActionHandleConnectorContainer() = default;
+		ActionHandleConnectorContainer(ActionHandleConnectorContainer&& _other) noexcept;
+		~ActionHandleConnectorContainer();
+
+		ActionHandleConnectorContainer& operator=(ActionHandleConnectorContainer&& _other) noexcept;
+
+		//! @brief Stores the given connector in the container. The container takes ownership of the connector.
+		//! @param _connector The connector to store.
+		void store(std::shared_ptr<ActionHandleConnector>&& _connector);
+
+		//! @brief Stores the given connector in the container. The container takes shared ownership of the connector.
+		//! @param _connector The connector to store.
+		void store(const std::shared_ptr<ActionHandleConnector>& _connector);
+
+		//! @brief Removes all connectors from the dispatcher and clears the container.
+		void clear();
+
+	private:
+
+		std::list<std::shared_ptr<ActionHandleConnector>> m_connectors;
+	};
+
+}
---
+++ Libraries/OTCommunication/src/ActionHandleConnectorContainer.cpp
@@ -0,0 +1,41 @@
+//! @file ActionHandleConnectorContainer.cpp
+//! @author Alexander Kuester (alexk95)
+//! @date October 2025
+// ###########################################################################################################################################################################################################################################################################################################################
+
+// OpenTwin header
+#include "OTCommunication/ActionHandleConnectorContainer.h"
+
+ot::ActionHandleConnectorContainer::ActionHandleConnectorContainer(ActionHandleConnectorContainer&& _other) noexcept {
+	this->operator=(std::move(_other));
+}
+
+ot::ActionHandleConnectorContainer::~ActionHandleConnectorContainer() {
+	this->clear();
+}
+
+ot::ActionHandleConnectorContainer& ot::ActionHandleConnectorContainer::operator=(ActionHandleConnectorContainer&& _other) noexcept {
+	if (this != &_other) {
+		this->clear();
+
+		m_connectors = std::move(_other.m_connectors);
+		_other.m_connectors.clear();
+	}
+
+	return *this;
+}
+
+void ot::ActionHandleConnectorContainer::store(std::shared_ptr<ActionHandleConnector>&& _connector) {
+	m_connectors.push_back(std::move(_connector));
+}
+
+void ot::ActionHandleConnectorContainer::store(const std::shared_ptr<ActionHandleConnector>& _connector) {
+	m_connectors.push_back(_connector);
+}
+
+void ot::ActionHandleConnectorContainer::clear() {
+	for (auto& connector : m_connectors) {
+		connector->removeFromDispatcher();
+	}
+	m_connectors.clear();
+}
