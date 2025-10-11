58cadde5f3e558eadc117288dce8491fc25ad4f6
Alexander Küster <alexk95@users.noreply.github.com>
2025-10-11 23:09:25 +0200

    Added missing libraries and services to cleanAll batch

--- Scripts/BuildAndTest/CleanAll.bat
+++ Scripts/BuildAndTest/CleanAll.bat
@@ -32,19 +32,39 @@ REM ############################################################################
 REM LIBRARIES
 
 ECHO ===============================================================
-ECHO Clean Library: OpenTwinSystem
+ECHO Clean Library: BlockEntities
 ECHO ===============================================================
-CALL "%OT_SYSTEM_ROOT%\Clean.bat" 
+CALL "%OT_BLOCKENTITIES_ROOT%\Clean.bat"
+
+ECHO ===============================================================
+ECHO Clean Library: CADModelEntities
+ECHO ===============================================================
+CALL "%OT_CADMODELENTITIES_ROOT%\Clean.bat"
+
+ECHO ===============================================================
+ECHO Clean Library: DataStorage
+ECHO ===============================================================
+CALL "%OT_DATASTORAGE_ROOT%\Clean.bat" 
+
+ECHO ===============================================================
+ECHO Clean Library: LTSpiceConnector
+ECHO ===============================================================
+CALL "%OT_LTSPICE_CONNECTOR_ROOT%\Clean.bat" 
+
+ECHO ===============================================================
+ECHO Clean Library: ModelEntities
+ECHO ===============================================================
+CALL "%OT_MODELENTITIES_ROOT%\Clean.bat" 
+
+ECHO ===============================================================
+ECHO Clean Library: OpenTwinCommunications
+ECHO ===============================================================
+CALL "%OT_COMMUNICATION_ROOT%\Clean.bat" 
 
 ECHO ===============================================================
 ECHO Clean Library: OpenTwinCore
 ECHO ===============================================================
 CALL "%OT_CORE_ROOT%\Clean.bat" 
-
-ECHO ===============================================================
-ECHO Clean Library: OTRandom
-ECHO ===============================================================
-CALL "%OT_RANDOM_ROOT%\Clean.bat" 
 
 ECHO ===============================================================
 ECHO Clean Library: Frontend Connector API
@@ -62,39 +82,14 @@ ECHO ===============================================================
 CALL "%OT_GUIAPI_ROOT%\Clean.bat" 
 
 ECHO ===============================================================
-ECHO Clean Library: OTWidgets
-ECHO ===============================================================
-CALL "%OT_WIDGETS_ROOT%\Clean.bat" 
-
-ECHO ===============================================================
-ECHO Clean Library: OpenTwinCommunications
-ECHO ===============================================================
-CALL "%OT_COMMUNICATION_ROOT%\Clean.bat" 
-
-ECHO ===============================================================
-ECHO Clean Library: DataStorage
-ECHO ===============================================================
-CALL "%OT_DATASTORAGE_ROOT%\Clean.bat" 
-
-ECHO ===============================================================
-ECHO Clean Library: ModelEntities
-ECHO ===============================================================
-CALL "%OT_MODELENTITIES_ROOT%\Clean.bat" 
-
-ECHO ===============================================================
-ECHO Clean Library: CADModelEntities
-ECHO ===============================================================
-CALL "%OT_CADMODELENTITIES_ROOT%\Clean.bat" 
-
-ECHO ===============================================================
-ECHO Clean Library: BlockEntities
-ECHO ===============================================================
-CALL "%OT_BLOCKENTITIES_ROOT%\Clean.bat" 
-
-ECHO ===============================================================
 ECHO Clean Library: ModelAPI
 ECHO ===============================================================
 CALL "%OT_MODELAPI_ROOT%\Clean.bat" 
+
+ECHO ===============================================================
+ECHO Clean Library: OTRandom
+ECHO ===============================================================
+CALL "%OT_RANDOM_ROOT%\Clean.bat" 
 
 ECHO ===============================================================
 ECHO Clean Library: ServiceFoundation
@@ -102,14 +97,19 @@ ECHO ===============================================================
 CALL "%OT_FOUNDATION_ROOT%\Clean.bat" 
 
 ECHO ===============================================================
-ECHO Clean Library: StudioSuiteConnector
+ECHO Clean Library: OpenTwinSystem
 ECHO ===============================================================
-CALL "%OT_STUDIO_SUITE_CONNECTOR_ROOT%\Clean.bat" 
+CALL "%OT_SYSTEM_ROOT%\Clean.bat" 
 
 ECHO ===============================================================
-ECHO Clean Library: uiCore
+ECHO Clean Library: OTWidgets
 ECHO ===============================================================
-CALL "%OT_UICORE_ROOT%\Clean.bat" 
+CALL "%OT_WIDGETS_ROOT%\Clean.bat" 
+
+ECHO ===============================================================
+ECHO Clean Library: Result Data Access
+ECHO ===============================================================
+CALL "%OT_RESULT_DATA_ACCESS_ROOT%\Clean.bat" 
 
 ECHO ===============================================================
 ECHO Clean Library: RubberbandEngine: Core
@@ -122,57 +122,22 @@ ECHO ===============================================================
 CALL "%OT_RUBBERBAND_ROOT%\Clean.bat" 
 
 ECHO ===============================================================
-ECHO Clean Library: QWT Wrapper
+ECHO Clean Library: StudioSuiteConnector
 ECHO ===============================================================
-CALL "%OT_QWTWRAPPER_ROOT%\Clean.bat" 
+CALL "%OT_STUDIO_SUITE_CONNECTOR_ROOT%\Clean.bat" 
+
+ECHO ===============================================================
+ECHO Clean Library: uiCore
+ECHO ===============================================================
+CALL "%OT_UICORE_ROOT%\Clean.bat" 
 
 ECHO ===============================================================
 ECHO Clean Library: Viewer
 ECHO ===============================================================
 CALL "%OT_VIEWER_ROOT%\Clean.bat" 
 
-ECHO ===============================================================
-ECHO Clean Library: Result Data Access
-ECHO ===============================================================
-CALL "%OT_RESULT_DATA_ACCESS_ROOT%\Clean.bat" 
-
 REM ################################################################################################################################
 REM SERVICES
-
-ECHO ===============================================================
-ECHO Clean Service: Model
-ECHO ===============================================================
-CALL "%OT_MODEL_SERVICE_ROOT%\Clean.bat" 
-
-ECHO ===============================================================
-ECHO Clean Service: GlobalSessionService
-ECHO ===============================================================
-CALL "%OT_GLOBAL_SESSION_SERVICE_ROOT%\Clean.bat" 
-
-ECHO ===============================================================
-ECHO Clean Service: LocalSessionService
-ECHO ===============================================================
-CALL "%OT_LOCAL_SESSION_SERVICE_ROOT%\Clean.bat" 
-
-ECHO ===============================================================
-ECHO Clean Service: GlobalDirectoryService
-ECHO ===============================================================
-CALL "%OT_GLOBAL_DIRECTORY_SERVICE_ROOT%\Clean.bat" 
-
-ECHO ===============================================================
-ECHO Clean Service: LocalDirectoryService
-ECHO ===============================================================
-CALL "%OT_LOCAL_DIRECTORY_SERVICE_ROOT%\Clean.bat" 
-
-ECHO ===============================================================
-ECHO Clean Service: RelayService
-ECHO ===============================================================
-CALL "%OT_RELAY_SERVICE_ROOT%\Clean.bat" 
-
-ECHO ===============================================================
-ECHO Clean Service: LoggerService
-ECHO ===============================================================
-CALL "%OT_LOGGER_SERVICE_ROOT%\Clean.bat" 
 
 ECHO ===============================================================
 ECHO Clean Service: AuthorisationService
@@ -180,84 +145,9 @@ ECHO ===============================================================
 CALL "%OT_AUTHORISATION_SERVICE_ROOT%\Clean.bat" 
 
 ECHO ===============================================================
-ECHO Clean Service: PHREECService
-ECHO ===============================================================
-CALL "%OT_PHREEC_SERVICE_ROOT%\Clean.bat" 
-
-REM ECHO ===============================================================
-REM ECHO Clean Service: KrigingService
-REM ECHO ===============================================================
-REM CALL "%OT_KRIGING_SERVICE_ROOT%\Clean.bat" 
-
-ECHO ===============================================================
-ECHO Clean Service: ModelingService
-ECHO ===============================================================
-CALL "%OT_MODELING_SERVICE_ROOT%\Clean.bat" 
-
-ECHO ===============================================================
-ECHO Clean Service: VisualizationService
-ECHO ===============================================================
-CALL "%OT_VISUALIZATION_SERVICE_ROOT%\Clean.bat" 
-
-ECHO ===============================================================
-ECHO Clean Service: FITTDService
-ECHO ===============================================================
-CALL "%OT_FITTD_SERVICE_ROOT%\Clean.bat" 
-
-ECHO ===============================================================
 ECHO Clean Service: CartesianMeshService
 ECHO ===============================================================
 CALL  "%OT_CARTESIAN_MESH_SERVICE_ROOT%\Clean.bat" 
-
-ECHO ===============================================================
-ECHO Clean Service: TetMeshService
-ECHO ===============================================================
-CALL  "%OT_TET_MESH_SERVICE_ROOT%\Clean.bat" 
-
-ECHO ===============================================================
-ECHO Clean Service: ImportParameterizedData
-ECHO ===============================================================
-CALL "%OT_IMPORT_PARAMETERIZED_DATA_SERVICE_ROOT%\Clean.bat" 
-
-ECHO ===============================================================
-ECHO Clean Service: GetDPService
-ECHO ===============================================================
-CALL "%OT_GETDP_SERVICE_ROOT%\Clean.bat" 
-
-ECHO ===============================================================
-ECHO Clean Service: ElmerFEMService
-ECHO ===============================================================
-CALL "%OT_ELMERFEM_SERVICE_ROOT%\Clean.bat" 
-
-ECHO ===============================================================
-ECHO Clean Service: StudioSuiteService
-ECHO ===============================================================
-CALL "%OT_STUDIOSUITE_SERVICE_ROOT%\Clean.bat" 
-
-ECHO ===============================================================
-ECHO Clean Service: PythonExecutionService
-ECHO ===============================================================
-CALL "%OT_PYTHON_EXECUTION_SERVICE_ROOT%\Clean.bat" 
-
-ECHO ===============================================================
-ECHO Clean Service: PythonExecution
-ECHO ===============================================================
-CALL "%OT_PYTHON_EXECUTION_ROOT%\Clean.bat" 
-
-ECHO ===============================================================
-ECHO Clean Service: BlockEditorService
-ECHO ===============================================================
-CALL "%OT_BLOCKEDITORSERVICE_ROOT%\Clean.bat" 
-
-ECHO ===============================================================
-ECHO Clean Service: DataProcessingService
-ECHO ===============================================================
-CALL "%OT_DATA_PROCESSING_SERVICE_ROOT%\Clean.bat" 
-
-ECHO ===============================================================
-ECHO Clean Service: CircuitSimulatorService
-ECHO ===============================================================
-CALL "%OT_CIRCUIT_SIMULATOR_SERVICE_ROOT%\Clean.bat" 
 
 ECHO ===============================================================
 ECHO Clean Service: CircuitExecution
@@ -265,9 +155,129 @@ ECHO ===============================================================
 CALL "%OT_CIRCUIT_EXECUTION_ROOT%\Clean.bat" 
 
 ECHO ===============================================================
+ECHO Clean Service: CircuitSimulatorService
+ECHO ===============================================================
+CALL "%OT_CIRCUIT_SIMULATOR_SERVICE_ROOT%\Clean.bat" 
+
+ECHO ===============================================================
+ECHO Clean Service: DataProcessingService
+ECHO ===============================================================
+CALL "%OT_DATA_PROCESSING_SERVICE_ROOT%\Clean.bat" 
+
+ECHO ===============================================================
+ECHO Clean Service: DebugService
+ECHO ===============================================================
+CALL "%OT_DEBUGSERVICE_ROOT%\Clean.bat"
+
+ECHO ===============================================================
+ECHO Clean Service: ElmerFEMService
+ECHO ===============================================================
+CALL "%OT_ELMERFEM_SERVICE_ROOT%\Clean.bat" 
+
+ECHO ===============================================================
+ECHO Clean Service: FITTDService
+ECHO ===============================================================
+CALL "%OT_FITTD_SERVICE_ROOT%\Clean.bat" 
+
+ECHO ===============================================================
+ECHO Clean Service: GetDPService
+ECHO ===============================================================
+CALL "%OT_GETDP_SERVICE_ROOT%\Clean.bat" 
+
+ECHO ===============================================================
+ECHO Clean Service: GlobalDirectoryService
+ECHO ===============================================================
+CALL "%OT_GLOBAL_DIRECTORY_SERVICE_ROOT%\Clean.bat" 
+
+ECHO ===============================================================
+ECHO Clean Service: GlobalSessionService
+ECHO ===============================================================
+CALL "%OT_GLOBAL_SESSION_SERVICE_ROOT%\Clean.bat" 
+
+ECHO ===============================================================
+ECHO Clean Service: ImportParameterizedData
+ECHO ===============================================================
+CALL "%OT_IMPORT_PARAMETERIZED_DATA_SERVICE_ROOT%\Clean.bat" 
+
+REM ECHO ===============================================================
+REM ECHO Clean Service: KrigingService
+REM ECHO ===============================================================
+REM CALL "%OT_KRIGING_SERVICE_ROOT%\Clean.bat" 
+
+ECHO ===============================================================
 ECHO Clean Service: LibraryMangementService
 ECHO ===============================================================
 CALL "%OT_LIBRARY_MANAGEMENT_SERVICE_ROOT%\Clean.bat" 
+
+ECHO ===============================================================
+ECHO Clean Service: LocalDirectoryService
+ECHO ===============================================================
+CALL "%OT_LOCAL_DIRECTORY_SERVICE_ROOT%\Clean.bat" 
+
+ECHO ===============================================================
+ECHO Clean Service: LocalSessionService
+ECHO ===============================================================
+CALL "%OT_LOCAL_SESSION_SERVICE_ROOT%\Clean.bat" 
+
+ECHO ===============================================================
+ECHO Clean Service: LoggerService
+ECHO ===============================================================
+CALL "%OT_LOGGER_SERVICE_ROOT%\Clean.bat" 
+
+ECHO ===============================================================
+ECHO Clean Service: LT Spice Service
+ECHO ===============================================================
+CALL "%OT_LTSPICE_SERVICE_ROOT%\Clean.bat" 
+
+ECHO ===============================================================
+ECHO Clean Service: Model
+ECHO ===============================================================
+CALL "%OT_MODEL_SERVICE_ROOT%\Clean.bat" 
+
+ECHO ===============================================================
+ECHO Clean Service: ModelingService
+ECHO ===============================================================
+CALL "%OT_MODELING_SERVICE_ROOT%\Clean.bat" 
+
+ECHO ===============================================================
+ECHO Clean Service: PHREECService
+ECHO ===============================================================
+CALL "%OT_PHREEC_SERVICE_ROOT%\Clean.bat" 
+
+ECHO ===============================================================
+ECHO Clean Service: PyritService
+ECHO ===============================================================
+CALL "%OT_PYRIT_SERVICE_ROOT%\Clean.bat" 
+
+ECHO ===============================================================
+ECHO Clean Service: PythonExecution
+ECHO ===============================================================
+CALL "%OT_PYTHON_EXECUTION_ROOT%\Clean.bat" 
+
+ECHO ===============================================================
+ECHO Clean Service: PythonExecutionService
+ECHO ===============================================================
+CALL "%OT_PYTHON_EXECUTION_SERVICE_ROOT%\Clean.bat" 
+
+ECHO ===============================================================
+ECHO Clean Service: RelayService
+ECHO ===============================================================
+CALL "%OT_RELAY_SERVICE_ROOT%\Clean.bat" 
+
+ECHO ===============================================================
+ECHO Clean Service: StudioSuiteService
+ECHO ===============================================================
+CALL "%OT_STUDIOSUITE_SERVICE_ROOT%\Clean.bat" 
+
+ECHO ===============================================================
+ECHO Clean Service: TetMeshService
+ECHO ===============================================================
+CALL  "%OT_TET_MESH_SERVICE_ROOT%\Clean.bat" 
+
+ECHO ===============================================================
+ECHO Clean Service: VisualizationService
+ECHO ===============================================================
+CALL "%OT_VISUALIZATION_SERVICE_ROOT%\Clean.bat" 
 
 REM ################################################################################################################################
 REM EXECUTABLES
