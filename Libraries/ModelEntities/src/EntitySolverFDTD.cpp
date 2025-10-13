9faba1bbcac15cfa831ab312aa2a0074aaec20f4
Alexandros McCray <223855625+alexm-dev@users.noreply.github.com>
2025-10-13 16:44:36 +0200

    Added FDTD solver end criteria and oversampling property

--- Libraries/ModelEntities/src/EntitySolverFDTD.cpp
+++ Libraries/ModelEntities/src/EntitySolverFDTD.cpp
@@ -25,6 +25,8 @@ void EntitySolverFDTD::createProperties(std::string& _meshFolderName, ot::UID& _
 	EntityPropertiesDouble::createProperty("Frequency", "End Frequency", 0, "FDTDSolver", getProperties());
 	EntityPropertiesInteger::createProperty("Simulation Settings", "Timesteps", 10000, "FDTDSolver", getProperties());
 	EntityPropertiesSelection::createProperty("Simulation Settings", "Excitation type", excitationTypes, "Gauss Excitation", "FDTDSolver", getProperties());
+	EntityPropertiesDouble::createProperty("Simulation Settings", "End Criteria", 1e-5, "FDTDSolver", getProperties());
+	EntityPropertiesInteger::createProperty("Simulation Settings", "Oversampling", 6, "FDTDSolver", getProperties());
 	EntityPropertiesSelection::createProperty("Boundary Conditions", "x-min", boundaryValues, "PEC", "FDTDSolver", getProperties());
 	EntityPropertiesSelection::createProperty("Boundary Conditions", "x-max", boundaryValues, "PEC", "FDTDSolver", getProperties());
 	EntityPropertiesSelection::createProperty("Boundary Conditions", "y-min", boundaryValues, "PEC", "FDTDSolver", getProperties());
