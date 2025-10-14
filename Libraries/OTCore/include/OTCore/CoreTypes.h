e9d5ee08e862fa3bca1c00e9e5377e5a3d4c2eb0
Alexander Küster <alexk95@users.noreply.github.com>
2025-10-14 21:43:04 +0200

    Added invalid UID to core types

--- Libraries/OTCore/include/OTCore/CoreTypes.h
+++ Libraries/OTCore/include/OTCore/CoreTypes.h
@@ -18,26 +18,30 @@
 #include <limits>
 #include <type_traits>
 
+#undef min
+#undef max
+
 namespace ot {
 
-	//! @brief ID type used to identify a service (16 bit unsigned integer).
+#ifndef OT_OS_64Bit
+	static_assert(false, "OpenTwin requires a 64-bit architecture!");
+#endif
+
+	//! @brief ID type used to identify a service (16 bit unsigned integer) inside a session.
 	typedef unsigned short serviceID_t;
 
-	//! @brief The invalid service ID, a Service must not have this ID assigned.
-	static const serviceID_t invalidServiceID = 0;
+	//! @brief Invalid service ID, a Service must not have this ID assigned at any point.
+	static constexpr serviceID_t invalidServiceID = 0U;
 
-	//! @brief The maximum number a service ID may reach.
-	static const serviceID_t maxServiceID = UINT16_MAX;
+	//! @brief Maximum number a service ID may reach.
+	static constexpr serviceID_t maxServiceID = std::numeric_limits<serviceID_t>::max();
 
-#ifdef OT_OS_64Bit
-	//! @brief Unique identifier (64 bit unsigned integer)
+	//! @brief Unique identifier (64 bit unsigned integer).
+	//! Mainly used for entities.
 	typedef uint64_t UID;
 
-#else
-	//! @brief Unique identifier (32 bit unsigned integer)
-	typedef uint32_t UID;
-
-#endif // DEBUG
+	//! @brief Invalid unique identifier.
+	static constexpr UID invalidUID = std::numeric_limits<UID>::max();
 
 	//! @brief Unique identifier list
 	typedef std::list<UID> UIDList;
