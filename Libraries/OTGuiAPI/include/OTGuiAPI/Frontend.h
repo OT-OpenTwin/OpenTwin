91e33ca6d848c06ab5a5333e806420291553dd9a
Alexander Küster <alexk95@users.noreply.github.com>
2025-10-18 21:47:27 +0200

    Added file operations to Frontend class in GuiAPI

--- Libraries/OTGuiAPI/include/OTGuiAPI/Frontend.h
+++ Libraries/OTGuiAPI/include/OTGuiAPI/Frontend.h
@@ -20,68 +20,27 @@ namespace ot {
 	//! @class Frontend
 	//! @brief Frontend request layer.
 	//! The Frontend prvoides a set methods that may be used to send requests to the frontend.
-	class Frontend {
+	class OT_GUIAPI_API_EXPORT Frontend {
 		OT_DECL_NOCOPY(Frontend)
+		OT_DECL_NOMOVE(Frontend)
 	public:
-		// ###########################################################################################################################################################################################################################################################################################################################
-
-		// General
-
-		//! @brief Sets the control enabled state.
-		//! @param _controlName Name of the control.
-		//! @param _isEnabled If true the control should be enabled.
-		//! @param _sendRequest If true the enabled state request will be send to the frontend, otherwise the state will be buffered to be send later.
-		bool setControlEnabled(const std::string& _controlName, bool _isEnabled = true, bool _sendRequest = true);
-
-		//! @brief Sets the control enabled state.
-		//! @param _toolBarPageName Name of the ToolBar page where the controls group is located at.
-		//! @param _toolBarGroupName Name of the ToolBar group where the control is located at.
-		//! @param _controlName Name of the control.
-		//! @param _isEnabled If true the control should be enabled.
-		//! @param _sendRequest If true the enabled state request will be send to the frontend, otherwise the state will be buffered to be send later.
-		bool setControlEnabled(const std::string& _toolBarPageName, const std::string& _toolBarGroupName, const std::string& _controlName, bool _isEnabled = true, bool _sendRequest = true);
-
-		//! @brief Sets the control enabled state.
-		//! @param _toolBarPageName Name of the ToolBar page where the controls group is located at.
-		//! @param _toolBarGroupName Name of the ToolBar group where the control is located at.
-		//! @param _controlName Name of the control.
-		//! @param _isEnabled If true the control should be enabled.
-		//! @param _sendRequest If true the enabled state request will be send to the frontend, otherwise the state will be buffered to be send later.
-		bool setControlEnabled(const std::string& _toolBarPageName, const std::string& _toolBarGroupName, const std::string& _toolBarSubGroupName, const std::string& _controlName, bool _isEnabled = true, bool _sendRequest = true);
-
-		//! @brief Will send the currently buffered control enabled states to the frontend.
-		bool sendCurrentControlEnabledStates(void);
 
 		// ###########################################################################################################################################################################################################################################################################################################################
 
-		// ToolBar
+		// File operations
 
-		
+		static bool requestFileForReading(const std::string& _callbackAction, const std::string& _dialogTitle, const std::string& _fileMask, bool _loadContent, bool _loadMultiple, const std::string& _additionalInfo = std::string());
+
+		static bool requestFileForWriting(const std::string& _callbackAction, const std::string& _dialogTitle, const std::string& _fileMask, const std::string& _additionalInfo = std::string());
+
+		static bool writeDataToFile(const std::string& _dialogTitle, const std::string& _filePath, const std::string& _content, unsigned long long _uncompressedContentLength);
 
 		// ###########################################################################################################################################################################################################################################################################################################################
 
 		// Private
 
 	private:
-		//! @brief Returns the control name according to the provided data.
-		//! The string will have the following format: "_pageName + ':' + _groupName + ':' + _controlName".
-		//! @param _pageName The ToolBar page name where the group is located at.
-		//! @param _groupName The ToolBar group inside the page where the control is located at.
-		//! @param _controlName The name of the control.
-		inline std::string toolBarControlName(const std::string& _pageName, const std::string& _groupName, const std::string& _controlName) const { return _pageName + ":" + _groupName + ":" + _controlName; };
-		//! @brief Returns the control name according to the provided data.
-		//! The string will have the following format: "_pageName + ':' + _groupName + ':' + _subgroupName + ':' + _controlName".
-		//! @param _pageName The ToolBar page name where the group is located at.
-		//! @param _groupName The ToolBar group inside the page where the subgroup is located at.
-		//! @param _sugroupName The ToolBar subgroup inside the group where the control is located at.
-		//! @param _controlName The name of the control.
-		inline std::string toolBarControlName(const std::string& _pageName, const std::string& _groupName, const std::string& _subgroupName, const std::string& _controlName) const { return _pageName + ":" + _groupName + ":" + _subgroupName + ":" + _controlName; };
-		//! @brief Returns true if a control under the given name exists.
-		inline bool controlExists(const std::string& _controlName) const { return m_controlsEnabledState.find(_controlName) != m_controlsEnabledState.end(); };
-
-		std::map<std::string, bool> m_controlsEnabledState; //! @brief Contains the currently requested enabled state
-		std::map<std::string, bool> m_controlsEnabledStateInFrontend; //! @brief Contains the current enabled state in the frontend
-
+		
 		Frontend() {};
 		~Frontend() {};
 	};
--- Libraries/OTGuiAPI/src/Frontend.cpp
+++ Libraries/OTGuiAPI/src/Frontend.cpp
@@ -5,58 +5,50 @@
 // ###########################################################################################################################################################################################################################################################################################################################
 
 // OpenTwin header
+#include "OTCore/ThisService.h"
 #include "OTCore/LogDispatcher.h"
 #include "OTCommunication/ActionTypes.h"
 #include "OTGuiAPI/Frontend.h"
 #include "OTGuiAPI/GuiAPIManager.h"
 
-bool ot::Frontend::setControlEnabled(const std::string& _controlName, bool _isEnabled, bool _sendRequest) {
-	// Ensure the control was actually created
-	if (!this->controlExists(_controlName)) {
-		OT_LOG_W("A control with the name \"" + _controlName + "\" does not exist");
-		return false;
-	}
+// ###########################################################################################################################################################################################################################################################################################################################
 
-	// Avoid further processing if the enabled state did not change
-	if (m_controlsEnabledState[_controlName] == _isEnabled) return true;
+// File operations
 
-	m_controlsEnabledState[_controlName] = _isEnabled;
+bool ot::Frontend::requestFileForReading(const std::string& _callbackAction, const std::string& _dialogTitle, const std::string& _fileMask, bool _loadContent, bool _loadMultiple, const std::string& _additionalInfo) {
+	JsonDocument request;
+	request.AddMember(OT_ACTION_MEMBER, JsonString(OT_ACTION_CMD_UI_RequestFileForReading, request.GetAllocator()), request.GetAllocator());
+	request.AddMember(OT_ACTION_PARAM_SENDER_URL, JsonString(ThisService::instance().getServiceURL(), request.GetAllocator()), request.GetAllocator());
+	request.AddMember(OT_ACTION_PARAM_CallbackAction, JsonString(_callbackAction, request.GetAllocator()), request.GetAllocator());
+	request.AddMember(OT_ACTION_PARAM_UI_DIALOG_TITLE, JsonString(_dialogTitle, request.GetAllocator()), request.GetAllocator());
+	request.AddMember(OT_ACTION_PARAM_FILE_Mask, JsonString(_fileMask, request.GetAllocator()), request.GetAllocator());
+	request.AddMember(OT_ACTION_PARAM_FILE_LoadMultiple, _loadMultiple, request.GetAllocator());
+	request.AddMember(OT_ACTION_PARAM_FILE_LoadContent, _loadContent, request.GetAllocator());
+	request.AddMember(OT_ACTION_PARAM_Info, JsonString(_additionalInfo, request.GetAllocator()), request.GetAllocator());
 
-	if (_sendRequest) {
-		m_controlsEnabledStateInFrontend[_controlName] = _isEnabled;
-
-		std::list<std::string> elst;
-		std::list<std::string> dlst;
-
-		if (_isEnabled) {
-			elst.push_back(_controlName);
-		}
-		else {
-			dlst.push_back(_controlName);
-		}
-
-		JsonDocument cmdDoc;
-		cmdDoc.AddMember(OT_ACTION_MEMBER, JsonString(OT_ACTION_CMD_UI_EnableDisableControls, cmdDoc.GetAllocator()), cmdDoc.GetAllocator());
-		cmdDoc.AddMember(OT_ACTION_PARAM_UI_EnabledControlsList, JsonArray(elst, cmdDoc.GetAllocator()), cmdDoc.GetAllocator());
-		cmdDoc.AddMember(OT_ACTION_PARAM_UI_DisabledControlsList, JsonArray(dlst, cmdDoc.GetAllocator()), cmdDoc.GetAllocator());
-		//cmdDoc.AddMember(OT_ACTION_PARAM_SERVICE_ID, m_application->serviceID(), cmdDoc.GetAllocator());
-
-		return GuiAPIManager::instance().sendQueuedRequestToFrontend(cmdDoc.toJson());
-	}
-	else {
-		return true;
-	}
+	return GuiAPIManager::instance().sendQueuedRequestToFrontend(request.toJson());
 }
 
-bool ot::Frontend::setControlEnabled(const std::string& _toolBarPageName, const std::string& _toolBarGroupName, const std::string& _controlName, bool _isEnabled, bool _sendRequest) {
-	return this->setControlEnabled(this->toolBarControlName(_toolBarPageName, _toolBarGroupName, _controlName), _isEnabled, _sendRequest);
+bool ot::Frontend::requestFileForWriting(const std::string& _callbackAction, const std::string& _dialogTitle, const std::string& _fileMask, const std::string& _additionalInfo) {
+	JsonDocument request;
+	request.AddMember(OT_ACTION_MEMBER, JsonString(OT_ACTION_CMD_UI_SelectFileForStoring, request.GetAllocator()), request.GetAllocator());
+	request.AddMember(OT_ACTION_PARAM_SENDER_URL, JsonString(ThisService::instance().getServiceURL(), request.GetAllocator()), request.GetAllocator());
+	request.AddMember(OT_ACTION_PARAM_CallbackAction, JsonString(_callbackAction, request.GetAllocator()), request.GetAllocator());
+	request.AddMember(OT_ACTION_PARAM_UI_DIALOG_TITLE, JsonString(_dialogTitle, request.GetAllocator()), request.GetAllocator());
+	request.AddMember(OT_ACTION_PARAM_FILE_Mask, JsonString(_fileMask, request.GetAllocator()), request.GetAllocator());
+	request.AddMember(OT_ACTION_PARAM_Info, JsonString(_additionalInfo, request.GetAllocator()), request.GetAllocator());
+
+	return GuiAPIManager::instance().sendQueuedRequestToFrontend(request.toJson());
 }
 
-bool ot::Frontend::setControlEnabled(const std::string& _toolBarPageName, const std::string& _toolBarGroupName, const std::string& _toolBarSubGroupName, const std::string& _controlName, bool _isEnabled, bool _sendRequest) {
-	return this->setControlEnabled(this->toolBarControlName(_toolBarPageName, _toolBarGroupName, _toolBarSubGroupName, _controlName), _isEnabled, _sendRequest);
-}
+bool ot::Frontend::writeDataToFile(const std::string& _dialogTitle, const std::string& _filePath, const std::string& _content, unsigned long long _uncompressedContentLength) {
+	JsonDocument request;
+	request.AddMember(OT_ACTION_MEMBER, JsonString(OT_ACTION_CMD_UI_SaveFileContent, request.GetAllocator()), request.GetAllocator());
+	request.AddMember(OT_ACTION_PARAM_SENDER_URL, JsonString(ThisService::instance().getServiceURL(), request.GetAllocator()), request.GetAllocator());
+	request.AddMember(OT_ACTION_PARAM_UI_DIALOG_TITLE, JsonString(_dialogTitle, request.GetAllocator()), request.GetAllocator());
+	request.AddMember(OT_ACTION_PARAM_FILE_OriginalName, JsonString(_filePath, request.GetAllocator()), request.GetAllocator());
+	request.AddMember(OT_ACTION_PARAM_FILE_Content, JsonString(_content, request.GetAllocator()), request.GetAllocator());
+	request.AddMember(OT_ACTION_PARAM_FILE_Content_UncompressedDataLength, _uncompressedContentLength, request.GetAllocator());
 
-bool ot::Frontend::sendCurrentControlEnabledStates(void) {
-	return false;
+	return GuiAPIManager::instance().sendQueuedRequestToFrontend(request.toJson());
 }
-
