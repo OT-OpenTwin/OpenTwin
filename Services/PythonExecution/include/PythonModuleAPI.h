6280784df6a0b087fe50f491a8237a5c3cd87403
Alexander Küster <alexk95@users.noreply.github.com>
2025-10-30 19:12:48 +0100

    Update file headers

--- Services/PythonExecution/include/PythonModuleAPI.h
+++ Services/PythonExecution/include/PythonModuleAPI.h
@@ -1,4 +1,21 @@
 // @otlicense
+// File: PythonModuleAPI.h
+// 
+// License:
+// Copyright 2025 by OpenTwin
+//  
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//  
+//     http://www.apache.org/licenses/LICENSE-2.0
+//  
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// @otlicense-end
 
 //! @brief  Functions that investigate an existing module for the starting function. If the module contains only one funtion, it will be the starting function of the module. 
 //!			If the module holds multiple function definitions it requires one function called "__main__", which will be the starting function of the module.
