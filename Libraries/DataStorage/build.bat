00910124f9f5844785cc6c14d1073b656a6383dd
Peter Thoma <pth68@users.noreply.github.com>
2025-11-21 15:44:30 +0100

    converted DataStorage library to cmake.

--- Libraries/DataStorage/build.bat
+++ Libraries/DataStorage/build.bat
@@ -11,7 +11,7 @@ REM Setup eviroment
 CALL "%OPENTWIN_DEV_ROOT%\Scripts\SetupEnvironment.bat"
 
 REM Call the build shell
-CALL "%OPENTWIN_DEV_ROOT%\Scripts\BuildAndTest\BuildSingleProject.bat" "%OT_DATASTORAGE_ROOT%\DataStorage.sln" %1 %2 
+CALL "%OPENTWIN_DEV_ROOT%\Scripts\BuildAndTest\CMakeBuildSingleProject.bat" "%OT_DATASTORAGE_ROOT%" %1 %2 
 
 GOTO END
 
@@ -20,3 +20,4 @@ pause
 GOTO END
 
 :END
+
