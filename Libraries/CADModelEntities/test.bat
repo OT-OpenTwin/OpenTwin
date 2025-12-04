cbcf0ae345890fbefb7373ef373e3a1cf9f8444f
Peter Thoma <pth68@users.noreply.github.com>
2025-12-04 18:13:40 +0100

    converted CADModelEntities library to cmake.

--- Libraries/CADModelEntities/test.bat
+++ Libraries/CADModelEntities/test.bat
@@ -3,7 +3,7 @@
 REM This script requires the following environment variables to be set:
 REM 1. OPENTWIN_DEV_ROOT
 REM 2. OPENTWIN_THIRDPARTY_ROOT
-REM 3. DEVENV_ROOT_2022
+REM 2. DEVENV_ROOT_2022
 IF "%OPENTWIN_DEV_ROOT%" == "" (
 	ECHO Please specify the following environment variables: OPENTWIN_DEV_ROOT
 	goto PAUSE_END
@@ -27,7 +27,7 @@ IF NOT "%OPENTWIN_DEV_ENV_DEFINED%" == "1" (
 	goto END
 )
 
-ECHO Testing Project : CADModelEntities
+SETLOCAL enabledelayedexpansion
 
 REM Open project
 
@@ -44,32 +44,20 @@ IF "%1"=="DEBUG" (
   SET DEBUG=1
 )
 
-SET TYPE=/Rebuild
-SET TYPE_NAME=REBUILD
-
-IF "%2"=="BUILD" (
-	SET TYPE=/Build
-	SET TYPE_NAME=BUILD
-)
+SET "OLDPATH=%PATH%"
 
 IF %DEBUG%==1 (
-	ECHO %TYPE% DEBUGTEST
-	"%DEVENV_ROOT_2022%\devenv.exe" "%OT_CADMODELENTITIES_ROOT%\CADModelEntities.vcxproj" %TYPE% "DebugTest|x64"  
-	ECHO %TYPE% DEBUG
-	"%OT_CADMODELENTITIES_ROOT%\x64\Debug\CADModelEntitiesTest.exe" /Out --gtest_output="xml:%OPENTWIN_DEV_ROOT%\Scripts\BuildAndTest\Reports\CADModelEntitiesDebugReport.xml"
-	CALL "%OPENTWIN_THIRDPARTY_ROOT%\Python\set_paths_dev.bat"
-	python "%OPENTWIN_DEV_ROOT%\Scripts\BuildAndTest\modifyXML.py" "%OPENTWIN_DEV_ROOT%\Scripts\BuildAndTest\Reports\CADModelEntitiesDebugReport.xml" "CADModelEntities" "%OPENTWIN_DEV_ROOT%\Scripts\BuildAndTest\EditReports\CADModelEntitiesDebugReport.xml"
+	SET "PATH=%OT_ALL_DLLD%;%ZLIB_DLLPATHD%;%OPENTWIN_DEV_ROOT%\Deployment;%OLDPATH%"
+	CALL "%OPENTWIN_DEV_ROOT%\Scripts\BuildAndTest\UnitTestSingleProject.bat" "%OT_CADMODELENTITIES_ROOT%" DEBUG
 )
 
 IF %RELEASE%==1 (
-	ECHO %TYPE% RELEASETEST
-	"%DEVENV_ROOT_2022%\devenv.exe" "%OT_CADMODELENTITIES_ROOT%\CADModelEntities.vcxproj" %TYPE% "ReleaseTest|x64"
-	ECHO %TYPE% RELEASE
-	"%OT_CADMODELENTITIES_ROOT%\x64\Release\CADModelEntitiesTest.exe" /Out --gtest_output="xml:%OPENTWIN_DEV_ROOT%\Scripts\BuildAndTest\Reports\CADModelEntitiesReleaseReport.xml"
-	CALL "%OPENTWIN_THIRDPARTY_ROOT%\Python\set_paths_dev.bat"
-	python "%OPENTWIN_DEV_ROOT%\Scripts\BuildAndTest\modifyXML.py" "%OPENTWIN_DEV_ROOT%\Scripts\BuildAndTest\Reports\CADModelEntitiesReleaseReport.xml" "CADModelEntities" "%OPENTWIN_DEV_ROOT%\Scripts\BuildAndTest\EditReports\CADModelEntitiesReleaseReport.xml"
+	SET "PATH=%OT_ALL_DLLR%;%OPENTWIN_DEV_ROOT%\Deployment;%OLDPATH%"
+	CALL "%OPENTWIN_DEV_ROOT%\Scripts\BuildAndTest\UnitTestSingleProject.bat" "%OT_CADMODELENTITIES_ROOT%" RELEASE
 ) 
-  
+
+SET "PATH=%OLDPATH%"
+
 GOTO END
 
 :PAUSE_END
