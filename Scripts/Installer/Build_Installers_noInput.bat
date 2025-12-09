a2c21f21f42a8cbd3b2351c688c9a9a73c0868d5
Alexander Küster <alexk95@users.noreply.github.com>
2025-12-09 16:14:03 +0100

    zlib is now copied to installer path (required by OTCore lib)

--- Scripts/Installer/Build_Installers_noInput.bat
+++ Scripts/Installer/Build_Installers_noInput.bat
@@ -110,6 +110,7 @@ REM	"!SEVENZIP_REG_DATA!\7z.exe" x !PLUGIN_ZIPFILE! -o!NSIS_REG_VALUE! -y -aos
 	copy %OPENTWIN_THIRDPARTY_ROOT%\boost\boost_1_86_0\lib64-msvc-14.3\boost_filesystem-vc143-mt-x64-1_86.dll .\Upgrader_Exe\boost_filesystem-vc143-mt-x64-1_86.dll
 	copy %OPENTWIN_THIRDPARTY_ROOT%\MongoDb\mongo-cxx-driver-r3.10.0\x64\Release\bin .\Upgrader_Exe
 	copy %OPENTWIN_THIRDPARTY_ROOT%\MongoDb\mongo-c-driver-1.27.3\x64\Release\bin .\Upgrader_Exe
+	copy %OPENTWIN_THIRDPARTY_ROOT%\zlib\zlib-1.2.11\x64\Release\bin\zlib.dll .\Upgrader_Exe\zlib.dll
 	copy %OPENTWIN_DEV_ROOT%\Libraries\OTSystem\x64\Release\OTSystem.dll .\Upgrader_Exe\OTSystem.dll
 	copy %OPENTWIN_DEV_ROOT%\Libraries\OTCore\x64\Release\OTCore.dll .\Upgrader_Exe\OTCore.dll
 	
