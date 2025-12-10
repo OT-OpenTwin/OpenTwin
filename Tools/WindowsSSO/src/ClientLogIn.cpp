323efcd73ab68bb8c9a1055e75a8d70858b71fb5
Alexander Küster <alexk95@users.noreply.github.com>
2025-12-10 21:08:23 +0100

    Added missing file headers...

--- Tools/WindowsSSO/src/ClientLogIn.cpp
+++ Tools/WindowsSSO/src/ClientLogIn.cpp
@@ -1,3 +1,22 @@
+// @otlicense
+// File: ClientLogIn.cpp
+// 
+// License:
+// Copyright 2025 by OpenTwin
+//  
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//  
+//     http://www.apache.org/licenses/LICENSE-2.0
+//  
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// @otlicense-end
+
 #include "ClientLogIn.h"
 
 ClientLogIn::ClientLogIn(const std::string& _targetName)
@@ -57,7 +76,7 @@ std::vector<unsigned char> ClientLogIn::generateClientToken(const std::vector<un
     SECURITY_STATUS securityStatus = InitializeSecurityContextW(
         &m_credHandle,
         m_partialContext,
-        m_targetName,    // target name (SPN) – can be empty if NTLM/Negotiate
+        m_targetName,    // target name (SPN) - can be empty if NTLM/Negotiate
         ISC_REQ_CONFIDENTIALITY | ISC_REQ_MUTUAL_AUTH,
         0,
         SECURITY_NATIVE_DREP,
