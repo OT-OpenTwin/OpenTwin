1ff8c17d5c1509408a9c5fd01f8099b5247f8cae
Alexander Küster <alexk95@users.noreply.github.com>
2026-01-10 08:55:36 +0100

    Added auto project open frontend script

---
+++ Testdata/UiScripts/AutoProjectOpen10Times.js
@@ -0,0 +1,28 @@
+var PROJECT_INDEX = 10;
+var MAX_SWITCHES = 10;
+
+// Auto Login
+Engine.logInDialogAvailable.connect(function (dialog) {
+    dialog.dialogShown.connect(function () {
+        dialog.slotLogIn();
+    });
+});
+
+// After login open the first project
+AppBase.loginSuccessful.connect(function () {
+    AppBase.slotOpenProjectFromIndex(0);
+});
+
+// After project open completed switch to other project
+var projectSwitchCount = 0;
+
+AppBase.servicesUiSetupComplete.connect(function () {
+    if (projectSwitchCount >= MAX_SWITCHES) {
+        return;
+    }
+
+    AppBase.slotRefreshProjectOverivew();
+
+    projectSwitchCount++;
+    AppBase.slotOpenProjectFromIndex(10); 
+});
