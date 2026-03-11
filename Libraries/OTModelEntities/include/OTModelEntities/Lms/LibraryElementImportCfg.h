2603cb80552f6ea5631364300c412409ad408e96
Sebastian Urmann <sebastian.urmann@stud.fra-uas.de>
2026-03-11 10:33:07 +0100

    Added config class to represent importable library element

---
+++ Libraries/OTModelEntities/include/OTModelEntities/Lms/LibraryElementImportCfg.h
@@ -0,0 +1,189 @@
+// @otlicense
+// File: LibraryModelImportCfg.h
+// 
+// License:
+// Copyright 2025 by OpenTwin
+//  
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//  
+//     http://www.apache.org/licenses/LICENSE-2.0
+//  
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// @otlicense-end
+
+#pragma once
+
+// OpenTwin header
+#include "OTCore/Serializable.h"
+#include "OTCore/CoreTypes.h"
+#include "OTModelEntities/ModelEntitiesAPIExport.h"
+#include "OTModelEntities/Lms/LibraryElementSelectionCfg.h"
+// std header
+#include <string>
+#include <map>
+namespace ot{
+    //! @brief Configuration class representing a complete library model to be imported
+    //! This class holds all information about a model loaded from the database including
+    //! metadata, additional infos, origin information and the actual model data
+    class OT_MODELENTITIES_API_EXPORT LibraryElementImportCfg : public ot::Serializable {
+    public:
+        LibraryElementImportCfg() = default;
+        virtual ~LibraryElementImportCfg() = default;
+
+        // ###########################################################################################################################################################################################################################################################################################################################
+
+        // Setter / Getter
+
+        //! @brief Set the model name
+        void setName(const std::string& _name) { m_name = _name; }
+        const std::string& getName() const { return m_name; }
+
+        //! @brief Set the model version
+        void setVersion(int64_t _version) { m_version = _version; }
+        int64_t getVersion() const { return m_version; }
+
+        //! @brief Set the original file name
+        void setFileName(const std::string& _fileName) { m_fileName = _fileName; }
+        const std::string& getFileName() const { return m_fileName; }
+
+        //! @brief Set the file hash
+        void setHash(const std::string& _hash) { m_hash = _hash; }
+        const std::string& getHash() const { return m_hash; }
+
+        //! @brief Set the actual model data (e.g., SPICE model code)
+        void setData(const std::string& _data) { m_data = _data; }
+        const std::string& getData() const { return m_data; }
+
+        //! @brief Set the requesting entity ID (entity that initiated the import request)
+        void setRequestingEntityID(ot::UID _entityID) { m_requestingEntityID = _entityID; }
+        ot::UID getRequestingEntityID() const { return m_requestingEntityID; }
+
+        //! @brief Set the collection name from which the model was loaded
+        void setCollectionName(const std::string& _collectionName) { m_collectionName = _collectionName; }
+        const std::string& getCollectionName() const { return m_collectionName; }
+
+        //! @brief Set the callback service URL (Service that will handle the selection result)
+        void setCallBackService(const std::string& _service) { m_callBackService = _service; };
+        const std::string& getCallBackService() const { return m_callBackService; };
+
+        //! @brief Set the folder path where the new entity should be created after selection
+        void setNewEntityFolder(const std::string& _folder) { m_newEntityFolder = _folder; };
+        const std::string& getNewEntityFolder() const { return m_newEntityFolder; };
+
+        //! @brief Set the type of the new entity to create based on the selection
+        void setEntityType(LmsNewEntityType _type) { m_newEntityType = _type; };
+        LmsNewEntityType getEntityType() const { return m_newEntityType; };
+
+        // Metadata management
+
+        //! @brief Add a metadata key-value pair (e.g., "IS": "27.5")
+        void addMetaData(const std::string& _key, const std::string& _value) {
+            m_metaData[_key] = _value;
+        }
+
+        //! @brief Check if metadata key exists
+        bool hasMetaData(const std::string& _key) const {
+            return m_metaData.find(_key) != m_metaData.end();
+        }
+
+        //! @brief Get metadata value by key
+        std::string getMetaDataValue(const std::string& _key) const {
+            auto it = m_metaData.find(_key);
+            if (it != m_metaData.end()) {
+                return it->second;
+            }
+            return "";
+        }
+
+        //! @brief Get all metadata
+        const std::map<std::string, std::string>& getMetaData() const {
+            return m_metaData;
+        }
+
+        //! @brief Clear all metadata
+        void clearMetaData() {
+            m_metaData.clear();
+        }
+
+        // Additional info management
+
+        //! @brief Add an additional info key-value pair
+        void addAdditionalInfo(const std::string& _key, const std::string& _value) {
+            m_additionalInfos[_key] = _value;
+        }
+
+        //! @brief Check if additional info key exists
+        bool hasAdditionalInfo(const std::string& _key) const {
+            return m_additionalInfos.find(_key) != m_additionalInfos.end();
+        }
+
+        //! @brief Get additional info value by key
+        std::string getAdditionalInfoValue(const std::string& _key) const {
+            auto it = m_additionalInfos.find(_key);
+            if (it != m_additionalInfos.end()) {
+                return it->second;
+            }
+            return "";
+        }
+
+        //! @brief Get all additional infos
+        const std::map<std::string, std::string>& getAdditionalInfos() const {
+            return m_additionalInfos;
+        }
+
+        //! @brief Clear all additional infos
+        void clearAdditionalInfos() {
+            m_additionalInfos.clear();
+        }
+
+        // ###########################################################################################################################################################################################################################################################################################################################
+
+        // Serialization
+
+        //! @brief Add the object contents to the provided JSON object
+        //! @param _object Json object reference
+        //! @param _allocator Allocator
+        virtual void addToJsonObject(ot::JsonValue& _object, ot::JsonAllocator& _allocator) const override;
+
+        //! @brief Will set the object contents from the provided JSON object
+        //! @param _object The JSON object containing the information
+        //! @throw Will throw an exception if the provided object is not valid (members missing or invalid types)
+        virtual void setFromJsonObject(const ot::ConstJsonObject& _object) override;
+
+        //! @brief Deserialize callback service, entity type and new entity folder from m_additionalInfo JSON string
+        //! @return true if deserialization was successful, false otherwise
+        bool deserializeCallbackInfoFromAdditionalInfo(const std::string& _additionalInfo);
+
+    private:
+        // Basic information
+        std::string m_name;
+        int64_t m_version = 0;
+
+        // Origin information
+        std::string m_fileName;
+        std::string m_hash;
+
+        // Model data
+        std::string m_data;
+
+        // Import context
+        ot::UID m_requestingEntityID = 0;
+        std::string m_collectionName;
+
+        // Additional data
+        std::map<std::string, std::string> m_metaData;
+        std::map<std::string, std::string> m_additionalInfos;
+
+        // Additional info fields for callback handling (serialized into m_additionalInfo as JSON string)
+        std::string m_callBackService;
+        std::string m_newEntityFolder;
+        LmsNewEntityType m_newEntityType;
+
+    };
+}
---
+++ Libraries/OTModelEntities/src/Lms/LibraryElementImportCfg.cpp
@@ -0,0 +1,129 @@
+// @otlicense
+// File: LibraryModelImportCfg.cpp
+// 
+// License:
+// Copyright 2025 by OpenTwin
+//  
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//  
+//     http://www.apache.org/licenses/LICENSE-2.0
+//  
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// @otlicense-end
+
+#include "OTModelEntities/Lms/LibraryElementImportCfg.h"
+
+void ot::LibraryElementImportCfg::addToJsonObject(ot::JsonValue& _object, ot::JsonAllocator& _allocator) const {
+    // Basic information
+    _object.AddMember("Name", ot::JsonString(m_name, _allocator), _allocator);
+    _object.AddMember("Version", m_version, _allocator);
+
+    // Origin information
+    _object.AddMember("FileName", ot::JsonString(m_fileName, _allocator), _allocator);
+    _object.AddMember("Hash", ot::JsonString(m_hash, _allocator), _allocator);
+
+    // Model data
+    _object.AddMember("Data", ot::JsonString(m_data, _allocator), _allocator);
+
+    _object.AddMember("RequestingEntityID", m_requestingEntityID, _allocator);
+    _object.AddMember("CollectionName", ot::JsonString(m_collectionName, _allocator), _allocator);
+
+    _object.AddMember("EntityType", ot::JsonString(LibraryElementSelectionCfg::entityTypeToString(m_newEntityType), _allocator), _allocator);
+    _object.AddMember("NewEntityFolder", ot::JsonString(m_newEntityFolder, _allocator), _allocator);
+    _object.AddMember("CallbackService", ot::JsonString(m_callBackService, _allocator), _allocator);
+
+    // Serialize metadata
+    ot::JsonObject metaDataObj;
+    for (const auto& pair : m_metaData) {
+        metaDataObj.AddMember(
+            ot::JsonString(pair.first, _allocator),
+            ot::JsonString(pair.second, _allocator),
+            _allocator
+        );
+    }
+    _object.AddMember("MetaData", metaDataObj, _allocator);
+
+    // Serialize additional infos
+    ot::JsonObject additionalInfosObj;
+    for (const auto& pair : m_additionalInfos) {
+        additionalInfosObj.AddMember(
+            ot::JsonString(pair.first, _allocator),
+            ot::JsonString(pair.second, _allocator),
+            _allocator
+        );
+    }
+    _object.AddMember("AdditionalInfos", additionalInfosObj, _allocator);
+}
+
+void ot::LibraryElementImportCfg::setFromJsonObject(const ot::ConstJsonObject& _object) {
+    // Basic information
+    m_name = ot::json::getString(_object, "Name");
+    m_version = ot::json::getInt64(_object, "Version");
+
+    // Origin information
+    m_fileName = ot::json::getString(_object, "FileName");
+    m_hash = ot::json::getString(_object, "Hash");
+
+    // Model data
+    m_data = ot::json::getString(_object, "Data");
+
+    // Import context
+    m_requestingEntityID = ot::json::getUInt64(_object, "RequestingEntityID");
+    m_collectionName = ot::json::getString(_object, "CollectionName");
+
+    m_callBackService = ot::json::getString(_object, "CallbackService");
+	m_newEntityType = LibraryElementSelectionCfg::stringToEntityType(ot::json::getString(_object, "EntityType"));
+	m_newEntityFolder = ot::json::getString(_object, "NewEntityFolder");
+
+    // Deserialize metadata
+    m_metaData.clear();
+    if (_object.HasMember("MetaData")) {
+        ot::ConstJsonObject metaDataObj = ot::json::getObject(_object, "MetaData");
+        for (auto it = metaDataObj.MemberBegin(); it != metaDataObj.MemberEnd(); ++it) {
+            std::string key = it->name.GetString();
+            std::string value = it->value.IsString() ? it->value.GetString() : "";
+            m_metaData[key] = value;
+        }
+    }
+
+    // Deserialize additional infos
+    m_additionalInfos.clear();
+    if (_object.HasMember("AdditionalInfos")) {
+        ot::ConstJsonObject additionalInfosObj = ot::json::getObject(_object, "AdditionalInfos");
+        for (auto it = additionalInfosObj.MemberBegin(); it != additionalInfosObj.MemberEnd(); ++it) {
+            std::string key = it->name.GetString();
+            std::string value = it->value.IsString() ? it->value.GetString() : "";
+            m_additionalInfos[key] = value;
+        }
+    }
+}
+
+bool ot::LibraryElementImportCfg::deserializeCallbackInfoFromAdditionalInfo(const std::string& _additionalInfo) {
+ 
+
+    if (_additionalInfo.empty()) {
+        return false;
+    }
+
+    ot::JsonDocument doc;
+    if (!doc.fromJson(_additionalInfo)) {
+        return false;
+    }
+
+    if (!doc.HasMember("CallbackService") || !doc.HasMember("EntityType") || !doc.HasMember("NewEntityFolder")) {
+        return false;
+    }
+
+    m_callBackService = ot::json::getString(doc, "CallbackService");
+    m_newEntityType = LibraryElementSelectionCfg::stringToEntityType(ot::json::getString(doc, "EntityType"));
+    m_newEntityFolder = ot::json::getString(doc, "NewEntityFolder");
+
+    return true;
+   
+}
