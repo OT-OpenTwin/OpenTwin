685d6ff3a091d435f86f5b9a9c8ca57acfbd9a94
JWagner <jan.wagner@fb2.fra-uas.de>
2026-04-02 10:02:14 +0200

    Added missing header

---
+++ Libraries/OTDataStorage/include/OTDataStorage/CumulativeLogMessage.h
@@ -0,0 +1,13 @@
+﻿//@ otlicense
+#pragma once
+#include <string>
+class CumulativeLogMessage
+{
+public:
+	void addLineToMessage(const std::string& line) { m_message += m_message; }
+	const std::string& getMessage() const { return m_message; }
+	void clearMessage() { m_message.clear(); }
+private:
+	std::string m_message;
+
+};
