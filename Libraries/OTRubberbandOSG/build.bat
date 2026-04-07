9a5af4c3564b264422280a72e95844cfdac60310
Alexandros McCray <223855625+alexm-dev@users.noreply.github.com>
2026-04-07 19:41:02 +0200

    OTRubberbandOSG: Convert to cmake

--- Libraries/OTRubberbandOSG/build.bat
+++ Libraries/OTRubberbandOSG/build.bat
@@ -11,7 +11,7 @@ REM Setup eviroment
 CALL "%OPENTWIN_DEV_ROOT%\Scripts\SetupEnvironment.bat"
 
 REM Call the build shell
-CALL "%OPENTWIN_DEV_ROOT%\Scripts\BuildAndTest\BuildSingleProject.bat" "%OT_RUBBERBAND_OSG_ROOT%\OTRubberbandOSG.sln" %1 %2 
+CALL "%OPENTWIN_DEV_ROOT%\Scripts\BuildAndTest\CMakeBuildSingleProject.bat" "%OT_RUBBERBAND_OSG_ROOT%" %1 %2 
 
 GOTO END
 
