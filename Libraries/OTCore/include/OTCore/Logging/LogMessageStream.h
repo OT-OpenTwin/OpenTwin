596be38819e0ae76152f866fdd8dfe3cfc89f7d8
Alexander Küster <alexk95@users.noreply.github.com>
2026-04-14 18:28:24 +0200

    Added container overloads to log message stream class

--- Libraries/OTCore/include/OTCore/Logging/LogMessageStream.h
+++ Libraries/OTCore/include/OTCore/Logging/LogMessageStream.h
@@ -6,7 +6,13 @@
 #include "OTCore/Logging/LogMessage.h"
 
 // std header
+#include <set>
+#include <map>
+#include <list>
+#include <vector>
 #include <sstream>
+#include <unordered_set>
+#include <unordered_map>
 
 namespace ot {
 
@@ -15,6 +21,14 @@ namespace ot {
 		OT_DECL_DEFMOVE(LogMessageStream)
 		OT_DECL_NODEFAULT(LogMessageStream)
 	public:
+		template <typename T, typename = void>
+		struct is_log_streamable
+			: std::false_type {};
+
+		template <typename T>
+		struct is_log_streamable<T, std::void_t<decltype(std::declval<ot::LogMessageStream&>() << std::declval<T>())>>
+			: std::true_type {};
+
 		struct Pointer
 		{
 			OT_DECL_DEFCOPY(Pointer)
@@ -43,11 +57,36 @@ namespace ot {
 		LogMessageStream& operator << (const Serializable* const _serializableObject);
 		LogMessageStream& operator << (const Pointer& _pointer);
 
+		template <typename T, typename = std::enable_if_t<is_log_streamable<T>::value>>
+		LogMessageStream& operator<<(const std::set<T>& _set);
+
+		template <typename T, typename = std::enable_if_t<is_log_streamable<T>::value>>
+		LogMessageStream& operator<<(const std::unordered_set<T>& _set);
+
+		template <typename T, typename = std::enable_if_t<is_log_streamable<T>::value>>
+		LogMessageStream& operator<<(const std::list<T>& _list);
+
+		template <typename T, typename = std::enable_if_t<is_log_streamable<T>::value>>
+		LogMessageStream& operator<<(const std::vector<T>& _vector);
+
+		template <typename K, typename V, 
+			typename = std::enable_if_t<is_log_streamable<K>::value>, 
+			typename = std::enable_if_t<is_log_streamable<V>::value>>
+		LogMessageStream& operator<<(const std::map<K, V>& _map);
+
+		template <typename K, typename V,
+			typename = std::enable_if_t<is_log_streamable<K>::value>,
+			typename = std::enable_if_t<is_log_streamable<V>::value>>
+			LogMessageStream & operator<<(const std::unordered_map<K, V>& _map);
+
 		std::string getText() const;
 		const std::string& getFunctionName() const { return m_functionName; };
 		const LogFlags& getFlags() const { return m_logFlags; };
 
 	private:
+		template <typename T> void appendList(const T& _list);
+		template <typename T> void appendMap(const T& _map);
+
 		std::stringstream m_stream;
 		std::string m_functionName;
 		LogFlags m_logFlags;
@@ -55,3 +94,5 @@ namespace ot {
 	};
 
 }
+
+#include "OTCore/Logging/LogMessageStream.hpp"
---
+++ Libraries/OTCore/include/OTCore/Logging/LogMessageStream.hpp
@@ -0,0 +1,95 @@
+// @otlicense
+
+#pragma once
+
+// OpenTwin header
+#include "OTCore/Logging/LogMessageStream.h"
+
+template <typename T, typename>
+inline ot::LogMessageStream& ot::LogMessageStream::operator<<(const std::set<T>& _set)
+{
+	appendList(_set);
+    return *this;
+}
+
+template <typename T, typename>
+inline ot::LogMessageStream& ot::LogMessageStream::operator<<(const std::unordered_set<T>& _set)
+{
+    appendList(_set);
+    return *this;
+}
+
+template <typename T, typename>
+inline ot::LogMessageStream& ot::LogMessageStream::operator<<(const std::list<T>& _list)
+{
+    appendList(_list);
+    return *this;
+}
+
+template <typename T, typename>
+inline ot::LogMessageStream& ot::LogMessageStream::operator<<(const std::vector<T>& _vector)
+{
+    appendList(_vector);
+    return *this;
+}
+
+template <typename K, typename V, typename, typename>
+inline ot::LogMessageStream& ot::LogMessageStream::operator<<(const std::map<K, V>& _map)
+{
+    appendMap(_map);
+    return *this;
+}
+
+template <typename K, typename V, typename, typename>
+inline ot::LogMessageStream& ot::LogMessageStream::operator<<(const std::unordered_map<K, V>& _map)
+{
+    appendMap(_map);
+    return *this;
+}
+
+template <typename T>
+void ot::LogMessageStream::appendList(const T& _list)
+{
+    m_stream << "[ ";
+    bool first = true;
+
+    for (const auto& item : _list)
+    {
+        if (!first)
+        {
+            m_stream << ", ";
+        }
+
+        *this << item;
+        first = false;
+    }
+
+    m_stream << " ]";
+}
+
+template <typename T>
+void ot::LogMessageStream::appendMap(const T& _list)
+{
+    m_stream << "[ ";
+    bool first = true;
+
+    for (const auto& item : _list)
+    {
+        if (!first)
+        {
+            m_stream << ", ";
+        }
+
+		m_stream << "{ ";
+		
+        *this << item.first;
+		m_stream << ", ";
+		*this << item.second;
+
+		m_stream << " }";
+
+        first = false;
+    }
+
+    m_stream << " ]";
+}
