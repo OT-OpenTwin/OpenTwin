0e04e0e78c52de70bab7e8a49d389b5b9efd37ac
Alexandros McCray <223855625+alexm-dev@users.noreply.github.com>
2026-05-30 20:45:02 +0200

    AuthorisationService: Convert to CMake

--- Services/AuthorisationService/edit.bat
+++ Services/AuthorisationService/edit.bat
@@ -30,7 +30,7 @@ IF NOT "%OPENTWIN_DEV_ENV_DEFINED%" == "1" (
 ECHO Launching development enviroment
 
 REM Open project
-START "" "%DEVENV_ROOT_2022%\devenv.exe" "%OT_AUTHORISATION_SERVICE_ROOT%\AuthorisationService.vcxproj"
+START "" "%DEVENV_ROOT_2022%\devenv.exe" "%OT_AUTHORISATION_SERVICE_ROOT%"
 
 GOTO END
 
