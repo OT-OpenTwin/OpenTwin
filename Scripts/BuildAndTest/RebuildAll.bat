2e9388b0965def2b299e2d9e229a23d7100df24c
Alexandros McCray <223855625+alexm-dev@users.noreply.github.com>
2026-05-30 22:14:34 +0200

    Migrate remaining converted lib paths to new CMake DLL paths

--- Scripts/BuildAndTest/RebuildAll.bat
+++ Scripts/BuildAndTest/RebuildAll.bat
@@ -117,9 +117,9 @@ IF NOT EXIST "%OT_ENCRYPTIONKEY_ROOT%\OTEncryptionKey.h" (
 	DEL "%OPENTWIN_DEV_ROOT%\Tools\KeyGenerator\x64\Release\OTRandom.dll"
 	
 	REM Copy libraries
-	COPY "%OT_SYSTEM_ROOT%\%OT_DLLR%\OTSystem.dll" "%OPENTWIN_DEV_ROOT%\Tools\KeyGenerator\x64\Release"
-	COPY "%OT_CORE_ROOT%\%OT_DLLR%\OTCore.dll" "%OPENTWIN_DEV_ROOT%\Tools\KeyGenerator\x64\Release"
-	COPY "%OT_RANDOM_ROOT%\%OT_DLLR%\OTRandom.dll" "%OPENTWIN_DEV_ROOT%\Tools\KeyGenerator\x64\Release"
+	COPY "%OT_SYSTEM_ROOT%\%OT_CDLLR%\OTSystem.dll" "%OPENTWIN_DEV_ROOT%\Tools\KeyGenerator\x64\Release"
+	COPY "%OT_CORE_ROOT%\%OT_CDLLR%\OTCore.dll" "%OPENTWIN_DEV_ROOT%\Tools\KeyGenerator\x64\Release"
+	COPY "%OT_RANDOM_ROOT%\%OT_CDLLR%\OTRandom.dll" "%OPENTWIN_DEV_ROOT%\Tools\KeyGenerator\x64\Release"
 
 	CALL "%OPENTWIN_DEV_ROOT%\Tools\KeyGenerator\x64\Release\KeyGenerator.exe" 2048 "%OT_ENCRYPTIONKEY_ROOT%\OTEncryptionKey.h"
 )
