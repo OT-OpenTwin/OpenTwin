8a1e48d9b62ae0e23ea2c022bafc4109c5481990
Alexandros McCray <223855625+alexm-dev@users.noreply.github.com>
2026-06-07 22:55:04 +0200

    CircuitExectution: Convert to CMake

--- Services/CircuitExecution/build.bat
+++ Services/CircuitExecution/build.bat
@@ -11,7 +11,7 @@ REM Setup eviroment
 CALL "%OPENTWIN_DEV_ROOT%\Scripts\SetupEnvironment.bat"
 
 REM Call the build shell
-CALL "%OPENTWIN_DEV_ROOT%\Scripts\BuildAndTest\BuildSingleProject.bat" "%OT_CIRCUIT_EXECUTION_ROOT%\CircuitExecution.sln" %1 %2 
+CALL "%OPENTWIN_DEV_ROOT%\Scripts\BuildAndTest\CMakeBuildSingleProject.bat" "%OT_CIRCUIT_EXECUTION_ROOT%" %1 %2
 
 GOTO END
 
--- Services/CircuitExecution/edit.bat
+++ Services/CircuitExecution/edit.bat
@@ -30,7 +30,7 @@ IF NOT "%OPENTWIN_DEV_ENV_DEFINED%" == "1" (
 ECHO Launching development enviroment
 
 REM Open project
-START "" "%DEVENV_ROOT_2022%\devenv.exe" "%OT_CIRCUIT_EXECUTION_ROOT%\CircuitExecution.vcxproj"
+START "" "%DEVENV_ROOT_2022%\devenv.exe" "%OT_CIRCUIT_EXECUTION_ROOT%"
 
 GOTO END
 
