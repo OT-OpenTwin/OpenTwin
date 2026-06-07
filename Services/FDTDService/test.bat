fc3b8c3a70e5d37c870be74836a5d1d3ea25863b
Alexandros McCray <223855625+alexm-dev@users.noreply.github.com>
2026-06-07 22:56:45 +0200

    FDTDService: Convert to CMake

--- Services/FDTDService/test.bat
+++ Services/FDTDService/test.bat
@@ -53,22 +53,12 @@ IF "%2"=="BUILD" (
 )
 
 IF %DEBUG%==1 (
-	ECHO %TYPE% DEBUGTEST
-	"%DEVENV_ROOT_2022%\devenv.exe" "%OPENTWIN_DEV_ROOT%\Services\FDTDService\FDTDService.vcxproj" %TYPE% "DebugTest|x64"  
-	ECHO %TYPE% DEBUG
-	"%OPENTWIN_DEV_ROOT%\Services\FDTDService\x64\Debug\FDTDServiceTest.exe" /Out --gtest_output="xml:%OPENTWIN_DEV_ROOT%\Scripts\Reports\FDTDServiceDebugReport.xml"
-	CALL "%OPENTWIN_THIRDPARTY_ROOT%\Python\set_paths_dev.bat"
-	python "%OPENTWIN_DEV_ROOT%\Scripts\modifyXML.py" "%OPENTWIN_DEV_ROOT%\Scripts\Reports\FDTDServiceDebugReport.xml" "FDTDService" "%OPENTWIN_DEV_ROOT%\Scripts\EditReports\FDTDServiceDebugReport.xml"
+	CALL "%OPENTWIN_DEV_ROOT%\Scripts\BuildAndTest\UnitTestSingleProject.bat" "%OT_FDTD_SERVICE_ROOT%" DEBUG
 )
 
 IF %RELEASE%==1 (
-	ECHO %TYPE% RELEASETEST
-	"%DEVENV_ROOT_2022%\devenv.exe" "%OPENTWIN_DEV_ROOT%\Services\FDTDService\FDTDService.vcxproj" %TYPE% "ReleaseTest|x64"
-	ECHO %TYPE% RELEASE
-	"%OPENTWIN_DEV_ROOT%\Services\FDTDService\x64\Release\FDTDServiceTest.exe" /Out --gtest_output="xml:%OPENTWIN_DEV_ROOT%\Scripts\Reports\FDTDServiceReleaseReport.xml"
-	CALL "%OPENTWIN_THIRDPARTY_ROOT%\Python\set_paths_dev.bat"
-	python "%OPENTWIN_DEV_ROOT%\Scripts\modifyXML.py" "%OPENTWIN_DEV_ROOT%\Scripts\Reports\FDTDServiceReleaseReport.xml" "FDTDService" "%OPENTWIN_DEV_ROOT%\Scripts\EditReports\FDTDServiceReleaseReport.xml"
-) 
+	CALL "%OPENTWIN_DEV_ROOT%\Scripts\BuildAndTest\UnitTestSingleProject.bat" "%OT_FDTD_SERVICE_ROOT%" RELEASE
+)
 
 GOTO END
 
