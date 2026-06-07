f3f2d1912bab342ef2de56057b6b91cbf3089505
Alexandros McCray <223855625+alexm-dev@users.noreply.github.com>
2026-06-07 23:07:09 +0200

    PythonExecution: Convert to CMake

--- Services/PythonExecution/tests/src/test.cpp
+++ Services/PythonExecution/tests/src/test.cpp
@@ -1,15 +1,15 @@
 // @otlicense
-// File: FixturePropertyPythonObjectInterface.h
-// 
+// File: test.cpp
+//
 // License:
 // Copyright 2025 by OpenTwin
-//  
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//  
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
-//  
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -17,14 +17,10 @@
 // limitations under the License.
 // @otlicense-end
 
-#pragma once
-#include <gtest/gtest.h>
-#include "PythonWrapper.h"
+#include <iostream>
+#include "gtest/gtest.h"
 
-class FixturePropertyPythonObjectInterface : public testing::Test
-{
-public:
-	FixturePropertyPythonObjectInterface();
-private:
-	PythonWrapper _wrapper;
-};
+TEST(PythonExecution, TestName) {
+  EXPECT_EQ(1, 1);
+  EXPECT_TRUE(true);
+}
