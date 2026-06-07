96f2f01710a5263a13a05aefe1d9aaff01620b79
Alexandros McCray <223855625+alexm-dev@users.noreply.github.com>
2026-06-07 22:57:08 +0200

    FileManagementProjectService: Convert to CMake

--- Services/FileManagementProjectService/build.bat
+++ Services/FileManagementProjectService/build.bat
@@ -11,7 +11,7 @@ REM Setup eviroment
 CALL "%OPENTWIN_DEV_ROOT%\Scripts\SetupEnvironment.bat"
 
 REM Call the build shell
-CALL "%OPENTWIN_DEV_ROOT%\Scripts\BuildAndTest\BuildSingleProject.bat" "%OT_FILEMANAGEMENT_PROJECT_SERVICE_ROOT%\FileManagementProjectService.sln" %1 %2 
+CALL "%OPENTWIN_DEV_ROOT%\Scripts\BuildAndTest\CMakeBuildSingleProject.bat" "%OT_FILEMANAGEMENT_PROJECT_SERVICE_ROOT%" %1 %2
 
 GOTO END
 
