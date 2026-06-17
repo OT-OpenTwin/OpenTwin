3fdaf69fd7f942239b33535004dd641efb33b510
JWagner <jan.wagner@fb2.fra-uas.de>
2026-06-17 13:59:13 +0200

    Added a lazy iterator for entities.

---
+++ Services/ImportParameterizedData/src/EntityLazyIterator.cpp
@@ -0,0 +1,40 @@
+﻿#include "EntityLazyIterator.h"
+#include "Application.h"
+#include "OTModelEntities/EntityAPI.h"
+inline EntityLazyIterator::EntityLazyIterator(const std::list<ot::EntityInformation>& _entityInfos, uint32_t _batchSize)
+	:m_entityInfosForLoading(_entityInfos.begin(), _entityInfos.end()) , m_batchSize(_batchSize)
+{}
+
+inline bool EntityLazyIterator::loadBatch()
+{
+    std::size_t start = m_batchIndex * m_batchSize;
+    if (start >= m_entityInfosForLoading.size())
+    {
+        return false;
+    }
+
+    std::size_t end = std::min(start + m_batchSize,m_entityInfosForLoading.size());
+    std::list<ot::EntityInformation> batchEntities;
+    for (std::size_t i = start; i < end; ++i)
+    {
+        batchEntities.push_back(m_entityInfosForLoading[i]);
+    }
+    
+    Application::instance()->prefetchDocumentsFromStorage(batchEntities);
+    for (ot::EntityInformation& batchEntityInfo : batchEntities)
+    {
+        m_buffer.push_back(ot::EntityAPI::readEntityFromEntityIDandVersion(batchEntityInfo));
+    }
+
+    return true;
+}
+
+void EntityLazyIterator::clearBuffer()
+{
+    for (EntityBase* bufferedEntity : m_buffer)
+    {
+        delete bufferedEntity;
+        bufferedEntity = nullptr;
+    }
+    m_buffer.clear();
+}
