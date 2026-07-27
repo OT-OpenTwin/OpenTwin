24a48a7ab0114c2f03dde92650a0b57397c3847e
Alexandros McCray <223855625+alexm-dev@users.noreply.github.com>
2026-07-27 17:04:29 +0200

    Remove bash wrapper scripts

--- Scripts/set_python.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-export OT_PYTHON="$OPENTWIN_THIRDPARTY_ROOT/Python/Python3_11_9/bin/python3"
--- Services/CircuitExecution/edit_py.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-. "$OPENTWIN_DEV_ROOT/Scripts/set_python.sh"
-"$OT_PYTHON" "$OPENTWIN_DEV_ROOT/Scripts/edit.py" CIRCUIT_EXECUTION || read -rp "Press Enter to continue... "
