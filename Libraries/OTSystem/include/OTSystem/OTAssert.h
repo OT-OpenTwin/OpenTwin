a5cffc728e29c4cb5f0d2d0a8c26d95155b263c7
Alexander Küster <alexk95@users.noreply.github.com>
2026-08-21 18:28:20 +0200

    Made text optional for OTAssert

--- Libraries/OTSystem/include/OTSystem/OTAssert.h
+++ Libraries/OTSystem/include/OTSystem/OTAssert.h
@@ -23,18 +23,23 @@
 #include <cassert>
 
 #ifdef _DEBUG
-//! @brief Adds the ability to provide a custom text to the cassert macro (assertion only in debug mode)
-#define OTAssert(___expression, ___message) (void)((!!(___expression)) || (_wassert(_CRT_WIDE(#___expression) L"\n\n" _CRT_WIDE(___message), _CRT_WIDE(__FILE__), (unsigned)__LINE__), 0))
 
-//! @brief Check if the provided pointer is not a nullptr
+//! @brief Adds the ability to provide a custom text to the cassert macro (assertion only in debug mode).
+//! @param ___expression Expression to check. If false, the assertion will be triggered.
+//! @param ... Custom text to display in the assertion message.
+#define OTAssert(___expression, ...) (void)((!!(___expression)) || (_wassert(_CRT_WIDE(#___expression) L"\n\n" _CRT_WIDE("" __VA_ARGS__), _CRT_WIDE(__FILE__), (unsigned)__LINE__), 0))
+
+//! @brief Check if the provided pointer is not a nullptr.
 #define OTAssertNullptr(___ptr) (void)((!!(___ptr != nullptr)) || (_wassert(_CRT_WIDE(#___ptr) _CRT_WIDE("\n\n[Caution]\nnullptr for \"") _CRT_WIDE(#___ptr) _CRT_WIDE("\"provided"), _CRT_WIDE(__FILE__), (unsigned)__LINE__), 0))
 
 #else
 
-//! @brief Adds the ability to provide a custom text to the cassert macro (assertion only in debug mode)
-#define OTAssert(___expression, ___message)
+//! @brief Adds the ability to provide a custom text to the cassert macro (assertion only in debug mode).
+//! @param ___expression Expression to check. If false, the assertion will be triggered.
+//! @param ... Custom text to display in the assertion message.
+#define OTAssert(___expression, ...)
 
-//! @brief Check if the provided pointer is not a nullptr
+//! @brief Check if the provided pointer is not a nullptr.
 #define OTAssertNullptr(___ptr)
 
 #endif // _DEBUG
