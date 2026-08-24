16efc87dde8d4d52378dd3f24f04917db0fcc4e0
Alexandros McCray <223855625+alexm-dev@users.noreply.github.com>
2026-08-24 14:08:36 +0200

    Add new testing batch wrapper for the new python testing system to each project

---
+++ Services/AuthorisationService/test_py.bat
@@ -0,0 +1,12 @@
+@ECHO OFF
+
+IF "%OPENTWIN_DEV_ROOT%" == "" (
+	ECHO Please specify the following environment variables: OPENTWIN_DEV_ROOT
+	PAUSE
+	EXIT /B 1
+)
+
+CALL "%OPENTWIN_DEV_ROOT%\Scripts\Python\set_python.bat"
+
+"%OT_PYTHON%" "%OPENTWIN_DEV_ROOT%\Scripts\Python\test.py" AUTHORISATION_SERVICE %1
+IF ERRORLEVEL 1 PAUSE
