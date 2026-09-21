f837f56b83086d7a4a7656bb299e9cc0c2939a40
Alexandros McCray <223855625+alexm-dev@users.noreply.github.com>
2026-09-21 13:39:05 +0200

    Change framework edit bat wrapper to directory

--- Framework/OpenTwin/edit.bat
+++ Framework/OpenTwin/edit.bat
@@ -8,5 +8,5 @@ IF "%OPENTWIN_DEV_ROOT%" == "" (
 
 CALL "%OPENTWIN_DEV_ROOT%\Scripts\Python\set_python.bat"
 
-"%OT_PYTHON%" "%OPENTWIN_DEV_ROOT%\Scripts\Python\edit.py" FRAMEWORK CODE
+"%OT_PYTHON%" "%OPENTWIN_DEV_ROOT%\Scripts\Python\edit.py" "%OPENTWIN_DEV_ROOT%\Framework\OpenTwin" CODE
 IF ERRORLEVEL 1 PAUSE
